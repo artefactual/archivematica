59ab74231e3ca3def4d069e302e9a90f4140aa38
Mike Cantelon <mcantelon@gmail.com>
2012-10-09 17:32:03 -0700

    Moved rights URLs and view logic into separate module.

--- src/dashboard/src/urls.py
+++ src/dashboard/src/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls.defaults import *
 
+UUID_REGEX = '[\w]{8}(-[\w]{4}){3}-[\w]{12}'
+
 urlpatterns = patterns('',
     (r'^mcp/', include('mcp.urls')),
     (r'^installer/', include('installer.urls')),
     (r'^administration/accounts/', include('components.accounts.urls')),
     (r'^archival-storage/', include('components.archival_storage.urls')),
     (r'^preservation-planning/', include('components.preservation_planning.urls')),
+    (r'transfer/(?P<uuid>' + UUID_REGEX + ')/rights/', include('components.rights.transfer_urls')),
+    (r'ingest/(?P<uuid>' + UUID_REGEX + ')/rights/', include('components.rights.ingest_urls')),
     (r'', include('main.urls'))
 )
