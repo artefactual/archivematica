072c1365d3368d69057102c36c11edd7f090f715
Mike Gale <mikeg@artefactual.com>
2012-11-01 10:30:33 -0700

    Added FPR search form

---
+++ src/dashboard/src/components/preservation_planning/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms.widgets import TextInput, Textarea
+
+INPUT_ATTRS = {'class': 'span11'}
+
+class FPRSearchForm(forms.Form):
+    query = forms.CharField(label='', required=False, widget=TextInput(attrs=INPUT_ATTRS))
+
