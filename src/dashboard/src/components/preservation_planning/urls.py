b315c269f38d8cee067b8dfa6c195f0a8e969002
Mike Cantelon <mcantelon@gmail.com>
2012-12-20 02:55:02 -0800

    Added a URL for temporary access to our old preservation planning page.

--- src/dashboard/src/components/preservation_planning/urls.py
+++ src/dashboard/src/components/preservation_planning/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('components.preservation_planning.views',
     #(r'fpr/search/$', 'preservation_planning_fpr_search'),
     #(r'fpr/search/(?P<current_page_number>\d+)/$', 'preservation_planning_fpr_search'),
     #(r'$', 'preservation_planning')
+    (r'old/$', 'preservation_planning'),
 
     (r'(?P<current_page_number>\d+)/$', 'preservation_planning_fpr_data'),
     (r'$', 'preservation_planning_fpr_data'),
