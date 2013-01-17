6d86d6add9fe516e8a806eeaf4eef2cb389e9447
Joseph Perry <joseph@artefactual.com>
2013-01-16 20:21:10 -0800

    Maildir removing rfc6266.
    Reverting to a modified version of Ian Lewis's code, for parsing content_disposion.
    http://www.ianlewis.org/en/parsing-email-attachments-python
    The rfc6266 package wasn't properly handling some attachment names.

--- src/archivematicaCommon/lib/externals/extractMaildirAttachments.py
+++ src/archivematicaCommon/lib/externals/extractMaildirAttachments.py
@@ -20,11 +20,6 @@ from email.utils import parseaddr
 from StringIO import StringIO
 import uuid
 import re
-import rfc6266 #TODO: add notes
-
-#http://tools.ietf.org/html/rfc2183
-#http://tools.ietf.org/html/rfc6266
-#http://en.wikipedia.org/wiki/MIME#Content-Disposition
 
 import sys
 sys.path.append("/usr/lib/archivematica/archivematicaCommon")
@@ -56,7 +51,7 @@ def parse_attachment(message_part, attachments=None):
         try:
             try:
                 content_disposition = tweakContentDisposition(content_disposition)
-                cd = rfc6266.parse_headers(content_disposition, relaxed=True)
+                dispositions = content_disposition.strip().split(";")
             except Exception as inst:
                 print type(inst)
                 print inst.args
@@ -68,7 +63,7 @@ def parse_attachment(message_part, attachments=None):
                         print >>sys.stderr, "Attempting extraction with random filename: %s" % (filename)
                         print >>sys.stderr
                         content_disposition = "attachment; filename=%s;" % (filename)
-                        cd = rfc6266.parse_headers(content_disposition, relaxed=True)
+                        dispositions = content_disposition.strip().split(";")
                     except Exception as inst:
                         print >>sys.stderr, type(inst)
                         print >>sys.stderr, inst.args
@@ -78,19 +73,7 @@ def parse_attachment(message_part, attachments=None):
                 else:
                     print >>sys.stderr
                     return None
-            if cd.disposition.lower() == "attachment":
-                filename = ""
-                if cd.assocs.has_key("filename"):
-                    filename = cd.assocs["filename"]
-                elif cd.assocs.has_key("filename*"):
-                    filename = cd.assocs["filename*"]
-                else:
-                    print >>sys.stderr, """Warning, no filename found in: [{%s}%s]%s""" % (sharedVariablesAcrossModules.sourceFileUUID, sharedVariablesAcrossModules.sourceFilePath, content_disposition)
-                    filename = uuid.uuid4().__str__()
-                    print >>sys.stderr, "Attempting extraction with random filename: %s" % (filename)
-                    print >>sys.stderr
-                if isinstance(filename, rfc6266.LangTagged):
-                    filename = filename.string
+            if content_disposition and dispositions[0].lower() == "attachment":
                 file_data = message_part.get_payload(decode=True)
                 if not file_data:
                     payload = message_part.get_payload()
@@ -100,22 +83,35 @@ def parse_attachment(message_part, attachments=None):
                         return None
                     print >>sys.stderr, message_part.get_payload()
                     print >>sys.stderr, message_part.get_content_charset()
+
                 attachment = StringIO(file_data)
                 attachment.content_type = message_part.get_content_type()
                 attachment.size = len(file_data)
-                attachment.name = filename
                 attachment.create_date = None
                 attachment.mod_date = None
-                attachment.read_date = None 
+                attachment.read_date = None
+                attachment.name = ""
                 
-                for name, value in cd.assocs.iteritems():
-                    if name == "create-date":
+                for param in dispositions[1:]:
+                    name,value = param.split("=", 1)
+                    name = name.lower().strip()
+                    
+                    if name == "filename":
+                        attachment.name = value
+                    if name == "filename*":
+                        attachment.name = value
+                    elif name == "create-date":
                         attachment.create_date = value  #TODO: datetime
                     elif name == "modification-date":
                         attachment.mod_date = value #TODO: datetime
                     elif name == "read-date":
                         attachment.read_date = value #TODO: datetime
                 
+                if not attachment.name: 
+                    print >>sys.stderr, """Warning, no filename found in: [{%s}%s]%s""" % (sharedVariablesAcrossModules.sourceFileUUID, sharedVariablesAcrossModules.sourceFilePath, content_disposition)
+                    filename = uuid.uuid4().__str__()
+                    print >>sys.stderr, "Attempting extraction with random filename: %s" % (filename)
+                    print >>sys.stderr
                 return attachment
                             
         except Exception as inst:
