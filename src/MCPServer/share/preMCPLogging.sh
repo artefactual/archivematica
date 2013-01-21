7d9ed4f424fa039cd7e5dc84974f208d4c22dd48
Joseph Perry <joseph@artefactual.com>
2013-01-21 10:02:31 -0800

    Adding root database password prompt to archivematica install.

--- src/MCPServer/share/preMCPLogging.sh
+++ src/MCPServer/share/preMCPLogging.sh
@@ -24,10 +24,21 @@ databaseName="MCP"
 username="demo"
 password="demo"
 
-mysql -hlocalhost -uroot --execute="DROP DATABASE IF EXISTS ${databaseName}"
-mysql -hlocalhost -uroot --execute="CREATE DATABASE ${databaseName} CHARACTER SET utf8 COLLATE utf8_unicode_ci"
-mysql -hlocalhost -uroot --execute="source ./mysql" "$databaseName"
-mysql -hlocalhost -uroot --execute="source ./mysql2Views" "$databaseName"
-mysql -hlocalhost -uroot --execute="DROP USER '${username}'@'localhost'"
-mysql -hlocalhost -uroot --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}'"
-mysql -hlocalhost -uroot --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost'"
+stty -echo
+echo -n "Enter the DATABASE root password (Hit enter if blank):"
+read dbpassword
+stty echo
+
+if [ ! -z "$dbpassword" ] ; then
+    dbpassword="-p${dbpassword}"
+else
+    dbpassword=""
+fi
+mysql -hlocalhost -uroot "${dbpassword}" --execute="DROP DATABASE IF EXISTS ${databaseName}"
+mysql -hlocalhost -uroot "${dbpassword}" --execute="CREATE DATABASE ${databaseName} CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+mysql -hlocalhost -uroot "${dbpassword}" --execute="source ./mysql" "$databaseName"
+mysql -hlocalhost -uroot "${dbpassword}" --execute="source ./mysql2Views" "$databaseName"
+mysql -hlocalhost -uroot "${dbpassword}" --execute="DROP USER '${username}'@'localhost'"
+mysql -hlocalhost -uroot "${dbpassword}" --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}'"
+mysql -hlocalhost -uroot "${dbpassword}" --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost'"
+dbpassword=""
