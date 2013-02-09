41d7b553bc7bb2d4a2215803040de9c3fb337787
Joseph Perry <joseph@artefactual.com>
2013-02-08 17:22:03 -0800

    Changing how directory permissions are set.
    refs #4647

--- src/MCPClient/lib/clientScripts/setDirectoryPermissionsForAppraisal.sh
+++ src/MCPClient/lib/clientScripts/setDirectoryPermissionsForAppraisal.sh
@@ -24,20 +24,22 @@
 
 #source /etc/archivematica/archivematicaConfig.conf
 
+set -e
+
 target="$1"
 if [ -e "${target}" ]; then
 	sudo chown -R archivematica:archivematica "${target}"  
 	echo `basename "${target}"` owned by "archivematica:archivematica" now 
 	sudo chmod -R 660 "${target}"
 	sudo chmod 640 "${target}"
-    sudo find "${target}" -type d | xargs -IF chmod u+rwx,g-w+rxt,o-rwx F
+    sudo find "${target}" -type d -execdir chmod 750 '{}' +
 	if [ -d "${target}objects" ]; then	
 		sudo chmod -R 660 "${target}objects"
-        sudo find "${target}objects" -type d | xargs -IF chmod u+rwx,g+rwxt,o-rwx F
+        sudo find "${target}objects" -type d -execdir chmod 770 '{}' +
 	fi
 	if [ -d "${target}metadata" ]; then	
 		sudo chmod -R 660 "${target}metadata"
-        sudo find "${target}metadata" -type d | xargs -IF chmod u+rwx,g+rwxt,o-rwx F
+        sudo find "${target}metadata" -type d -execdir chmod 770 '{}' +
 	fi
 else
   	echo $target does not exist\ 1>&2
