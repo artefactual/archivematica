215a4d490658ef2bed23badccda1a0e2d83c5f57
Mike Cantelon <mcantelon@gmail.com>
2013-02-12 14:24:57 -0800

    Added searchable fields in spec. of file ID API.
    refs #4658
    
    Specified, in the Tastypie resource definition, that we should be
    able to search on UUID and the lastmodified columns. They can be
    searched using URLs like the one below:
    
    http://localhost/api/fpr/file_id/?format=xml&lastmodified__gte=2012-10-08

--- src/dashboard/src/components/api/fpr/models.py
+++ src/dashboard/src/components/api/fpr/models.py
@@ -2,8 +2,14 @@ import sys
 sys.path.append("/usr/lib/archivematica/archivematicaCommon/externals")
 from tastypie.resources import ModelResource
 from main import models
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 
 class FPRFileIDResource(ModelResource):
     class Meta:
         queryset = models.FPRFileID.objects.all()
         resource_name = 'file_id'
+
+        filtering = {
+            "uuid": ALL,
+            "lastmodified": ALL
+        }
