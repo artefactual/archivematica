6e371a5f91bfea544ba3e1c0e749e58b9034d955
Mike Cantelon <mcantelon@gmail.com>
2013-02-14 15:35:02 -0800

    Re-removed AIPs model.
    refs #4569
    
    The initial removal of the AIPs model created an error. Resolved the error
    by tracking down an AIPs model code dependency I'd missed.

--- src/dashboard/src/components/api/models.py
+++ src/dashboard/src/components/api/models.py
@@ -20,11 +20,6 @@ sys.path.append("/usr/lib/archivematica/archivematicaCommon/externals")
 from tastypie.resources import ModelResource
 from main import models
 
-class AIPResource(ModelResource):
-    class Meta:
-        queryset = models.AIP.objects.all()
-        resource_name = 'aip'
-
 class SIPResource(ModelResource):
     class Meta:
         queryset = models.SIP.objects.all()
--- src/dashboard/src/components/api/urls.py
+++ src/dashboard/src/components/api/urls.py
@@ -16,13 +16,11 @@
 # along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf.urls.defaults import *
-from components.api.models import AIPResource
 from components.api.models import SIPResource
 from tastypie.api import Api
 
 # add version to non-FPR resources
 api = Api(api_name='v1')
-api.register(AIPResource())
 api.register(SIPResource())
 
 urlpatterns = patterns('components.archival_storage.views',
