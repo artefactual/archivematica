f55b8388ad76e4a535d7870c6c43d102f6fe19ee
Mike Cantelon <mcantelon@gmail.com>
2013-02-22 13:54:04 -0800

    Changed field order on setup form.
    refs #4684

--- src/dashboard/src/installer/forms.py
+++ src/dashboard/src/installer/forms.py
@@ -30,7 +30,7 @@ class SuperUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ['org_name', 'org_identifier', 'username', 'email', 'password1', 'password2']
 
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
