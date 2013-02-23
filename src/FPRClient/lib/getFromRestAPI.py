7a7f8f848075811cb857b6800f28784495338c96
Joseph Perry <joseph@artefactual.com>
2013-02-22 17:36:39 -0800

    Work on getting reply from server.

--- src/FPRClient/lib/getFromRestAPI.py
+++ src/FPRClient/lib/getFromRestAPI.py
@@ -21,23 +21,49 @@
 # @subpackage FPRClient
 # @author Joseph Perry <joseph@artefactual.com>
 from optparse import OptionParser
+from StringIO import StringIO    
 import pycurl
+import json
 import sys
 sys.path.append("/usr/lib/archivematica/archivematicaCommon")
 from databaseFunctions import insertIntoEvents
 
 def getFromRestAPI(url, postFields, verbose=False):
+    USER = "demo"
+    PASS = "demo"
+    storage = StringIO()
     c = pycurl.Curl()
     c.setopt(c.URL, url)
-    c.setopt(c.POSTFIELDS, postFields)
+    #c.setopt(c.POSTFIELDS, postFields)
     if verbose:
         c.setopt(c.VERBOSE, True)
+    #c.setopt(pycurl.USERPWD, "%s:%s" % (USER,PASS))
+    #c.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_BASIC)
+    
+    #c.setopt(pycurl.COOKIEJAR, 'cookies.txt')
+    #c.setopt(pycurl.COOKIEFILE, 'cookies.txt')
+    #c.setopt(pycurl.CAPATH,'FILE.pem')
+    #c.setopt(pycurl.SSLCERTPASSWD,'foobar')
+    #c.setopt(pycurl.SSLCERTTYPE, 'PEM')
+    #c.setopt(pycurl.SSLCERT,'FILE1.pem')
+    #c.setopt(pycurl.SSL_VERIFYPEER,0)#--insecure
+    
+    c.setopt(c.WRITEFUNCTION, storage.write)
     c.perform()
-
+    c.close()
+    
+    #todo
+    if not 200:
+        throw()
+    
+    content = storage.getvalue()
+    ret = json.loads(content)
+    for x in ret["objects"]:
+        print type(x), x
 
 if __name__ == '__main__':
     parser = OptionParser()
-    parser.add_option("-u",  "--url",          action="store", dest="url", default="http://localhost/api/fpr/v1/file_id/")
+    parser.add_option("-u",  "--url",          action="store", dest="url", default="http://fprserver/api/fpr/v1/file_id/")
     parser.add_option("-p",  "--postFields",        action="store", dest="postFields", default="format=json&order_by=lastmodified&lastmodified__gte=2012-10-10T10:00:00")
     parser.add_option("-v",  "--verbose",        action="store_true", dest="verbose", default=False)
 
