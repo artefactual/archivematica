cb25085ccea34d8656963a699654f40430e81d62
Mike Cantelon <mcantelon@gmail.com>
2013-04-01 14:40:17 -0700

    Added select disabling for non-boolean selects.

--- src/dashboard/src/media/js/administration/processing.js
+++ src/dashboard/src/media/js/administration/processing.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
 
-  var toggleableElementNames = [
+  var toggleableBooleanElementNames = [
     'backup_transfer',
     'quarantine_transfer',
     'normalize_transfer',
     'store_aip'
   ];
 
-  function disableApplicableRadioButtons() {
-    for(var index in toggleableElementNames) {
-      var elementName = toggleableElementNames[index]
-        , relatedRadioButtonName
+  function disableApplicableBooleanSelects() {
+    for(var index in toggleableBooleanElementNames) {
+      var elementName = toggleableBooleanElementNames[index]
+        , relatedBooleanSelectName
         , disabled;
 
       $('[name="' + elementName + '"]').each(function() {
         disabled = ($(this).attr('checked') != 'checked');
-        relatedRadioButtonName = elementName + '_toggle';
-        $('[name="' + relatedRadioButtonName + '"]').each(function() {
+        relatedBooleanSelectName = elementName + '_toggle';
+        $('[name="' + relatedBooleanSelectName + '"]').each(function() {
           if (disabled) {
             $(this).attr('disabled', 'true');
           } else {
@@ -27,11 +27,44 @@ $(document).ready(function() {
     }
   }
 
-  // disable applicable radion buttons on initial page load
-  disableApplicableRadioButtons();
+  // disable applicable boolean selects on initial page load
+  disableApplicableBooleanSelects();
+
+  var toggleableSelectElementNames = [
+    'create_sip_enabled',
+    'select_format_id_tool_enabled',
+    'normalize_enabled',
+    'compression_algo_enabled',
+    'compression_level_enabled',
+    'store_aip_location_enabled'
+  ];
+
+  function disableApplicableSelects() {
+    for(var index in toggleableSelectElementNames) {
+      var elementName = toggleableSelectElementNames[index]
+        , relatedSelectName
+        , disabled;
+
+     $('[name="' + elementName + '"]').each(function() {
+       disabled = ($(this).attr('checked') != 'checked');
+       relatedSelectName = elementName.replace('_enabled', '');
+       $('[name="' + relatedSelectName + '"]').each(function() {
+         if (disabled) {
+           $(this).attr('disabled', 'true');
+         } else {
+           $(this).removeAttr('disabled');
+         }
+       });
+     });
+    }
+  }
+
+  // disable applicable selects on initial page load
+  disableApplicableSelects();
 
   // update disabling when checkboxes are toggled
   $('[type="checkbox"]').change(function() {
-    disableApplicableRadioButtons();
+    disableApplicableBooleanSelects();
+    disableApplicableSelects();
   });
 });
