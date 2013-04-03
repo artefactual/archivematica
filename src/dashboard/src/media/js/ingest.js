e6e79b345447734dc02168e18020225e210c48de
Mike Cantelon <mcantelon@gmail.com>
2013-04-03 11:11:52 -0700

    Updating/adding copyright notices to source files.

--- src/dashboard/src/media/js/ingest.js
+++ src/dashboard/src/media/js/ingest.js
@@ -1,7 +1,7 @@
 /*
 This file is part of Archivematica.
 
-Copyright 2010-2012 Artefactual Systems Inc. <http://artefactual.com>
+Copyright 2010-2013 Artefactual Systems Inc. <http://artefactual.com>
 
 Archivematica is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
