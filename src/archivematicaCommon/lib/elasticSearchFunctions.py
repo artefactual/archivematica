0971bfe4e48add07c20e91b194555370aa4e359e
Joseph Perry <joseph@artefactual.com>
2013-04-18 17:29:07 -0700

    Revert "Revert "Added login check for ES connection/indexes.""
    This reverts commit 836688064d020b1738b5e4b894bac345d58adc43.

--- src/archivematicaCommon/lib/elasticSearchFunctions.py
+++ src/archivematicaCommon/lib/elasticSearchFunctions.py
@@ -49,13 +49,15 @@ def getElasticsearchServerHostAndPort():
     except:
         return '127.0.0.1:9200'
 
-def check_server_status():
+def check_server_status_and_create_indexes_if_needed():
     try:
-        # attempt simple connection
         conn = pyes.ES(getElasticsearchServerHostAndPort())
         conn._send_request('GET', '/')
     except:
         return 'Connection error'
+
+    connect_and_create_index('aips')
+    connect_and_create_index('transfers')
 
     # make sure the mapping for the transfer index types looks OK
     if not transfer_mapping_is_correct(conn):
@@ -64,6 +66,16 @@ def check_server_status():
     # make sure the mapping for the aip index types looks OK
     if not aip_mapping_is_correct(conn):
         return 'The AIP index mapping is incorrect. The "aips" index should be re-created.'
+
+    # no error!
+    return 'OK'
+
+def check_server_status():
+    try:
+        conn = pyes.ES(getElasticsearchServerHostAndPort())
+        conn._send_request('GET', '/')
+    except:
+        return 'Connection error'
 
     # no errors!
     return 'OK'
