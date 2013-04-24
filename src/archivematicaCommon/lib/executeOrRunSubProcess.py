8a932d99d42b51611766a12c1ec5aa7212c0b2e6
Joseph Perry <joseph@artefactual.com>
2013-04-24 11:58:58 -0700

    Setting LANG to en_US.UTF-8 if not defined.
    refs #4987

--- src/archivematicaCommon/lib/executeOrRunSubProcess.py
+++ src/archivematicaCommon/lib/executeOrRunSubProcess.py
@@ -33,6 +33,8 @@ def launchSubProcess(command, stdIn="", printing=True):
     try:
         my_env = os.environ
         my_env['PYTHONIOENCODING'] = 'utf-8'
+        if (not my_env.has_key('LANG')) or (not my_env['LANG']):
+             my_env['LANG'] = 'en_US.UTF-8'
         p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, env=my_env)
         stdOut, stdError = p.communicate(input=stdIn)
         #append the output to stderror and stdout
