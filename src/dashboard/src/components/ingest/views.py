3df1c0a6afffd1a7892e2765a11f33fee42b8bd4
Mike Cantelon <mcantelon@gmail.com>
2013-06-10 13:48:07 -0700

    Roughed out some logic for reading ATK collections/resource components.

--- src/dashboard/src/components/ingest/views.py
+++ src/dashboard/src/components/ingest/views.py
@@ -284,6 +284,62 @@ def ingest_upload_atk_get_dip_object_paths(uuid):
     ]
 
 def ingest_upload_atk_get_resource_children(resource_id):
+    resource_id = 63451;
+    return ingest_upload_atk_get_resource_component_and_children(resource_id, 'collection');
+
+def ingest_upload_atk_get_resource_component_and_children(resource_id, resource_type='collection'):
+    resource_data = {};
+
+    # TODO: where to store config?
+    db = MySQLdb.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        db="MCP"
+    )
+
+    cursor = db.cursor() 
+
+    if (resource_type == 'collection'):
+        # TODO: fix injection
+        cursor.execute("SELECT title, dateExpression FROM atk_collection WHERE resourceid=" + str(resource_id))
+
+        for row in cursor.fetchall():
+            resource_data['title']              = row[0]
+            resource_data['dates']              = row[1]
+            resource_data['levelOfDescription'] = 'Fonds'
+    else:
+        cursor.execute("SELECT title, dateExpression, persistentId, resourceLevel FROM atk_description WHERE resourceComponentId=" + str(resource_id))
+
+        for row in cursor.fetchall():
+            resource_data['title']              = row[0]
+            resource_data['dates']              = row[1]
+            resource_data['identifier']         = row[2]
+            resource_data['levelOfDescription'] = row[3]
+
+    resource_data['children'] = False
+
+    # TODO: fix injection issue
+    cursor.execute("SELECT resourceComponentId FROM atk_description WHERE parentResourceComponentId=" + str(resource_id))
+
+    rows = cursor.fetchall()
+
+    if len(rows):
+        resource_data['children'] = []
+
+        for row in rows:
+            resource_data['children'].append(
+                ingest_upload_atk_get_resource_component_and_children(
+                    row[0],
+                    'description'
+                )
+            )
+
+    return resource_data
+
+    """
+    Example data:
+
     return {
       'identifier': 'PR01',
       'title': 'Parent',
@@ -316,6 +372,7 @@ def ingest_upload_atk_get_resource_children(resource_id):
         'children': False
       }]
     }
+    """
 
 def ingest_normalization_report(request, uuid, current_page=None):
     jobs = models.Job.objects.filter(sipuuid=uuid, subjobof='')
