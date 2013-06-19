14141197ec5c874655c185de79edfdec7c6e868c
Mike Cantelon <mcantelon@gmail.com>
2013-06-18 22:32:11 -0700

    Move data fetching helpers in shared atk module.

--- src/dashboard/src/components/ingest/views_atk.py
+++ src/dashboard/src/components/ingest/views_atk.py
@@ -23,6 +23,7 @@ from django.utils import simplejson
 import os, sys, MySQLdb
 from components import helpers
 sys.path.append("/usr/lib/archivematica/archivematicaCommon")
+import archivistsToolkit.atk as atk
 import elasticSearchFunctions, databaseInterface, databaseFunctions
 
 # TODO: move into helpers module at some point
@@ -89,7 +90,7 @@ def ingest_upload_atk_resource(request, uuid, resource_id):
     db = ingest_upload_atk_db_connection()
     try:
         query = request.GET.get('query', '')
-        resource_data = ingest_upload_atk_get_resource_component_and_children(
+        resource_data = atk.get_resource_component_and_children(
             db,
             resource_id,
             'collection',
@@ -126,7 +127,7 @@ def ingest_upload_atk_resource_component(request, uuid, resource_component_id):
     db = ingest_upload_atk_db_connection()
     try:
         query = request.GET.get('query', '')
-        resource_component_data = ingest_upload_atk_get_resource_component_and_children(
+        resource_component_data = atk.get_resource_component_and_children(
             db,
             resource_component_id,
             'description',
@@ -161,7 +162,7 @@ def ingest_upload_atk_get_collections(search_pattern=''):
 
     for row in cursor.fetchall():
         collections.append(
-            ingest_upload_atk_get_resource_component_and_children(db, row[0])
+            atk.get_resource_component_and_children(db, row[0])
         )
 
     return collections
@@ -174,8 +175,9 @@ def ingest_upload_atk_match_dip_objects_to_resource_levels(request, uuid, resour
 
     try:
         # load resource and child data
+        db = ingest_upload_atk_db_connection()
         resource_data_json = simplejson.JSONEncoder().encode(
-            ingest_upload_atk_get_resource_children(resource_id)
+            atk.get_resource_children(db, resource_id)
         )
     except:
         return HttpResponse('Database error. Please contact an administrator.')
@@ -225,130 +227,11 @@ def ingest_upload_atk_match_dip_objects_to_resource_component_levels(request, uu
 
     try:
         # load resource and child data
+        db = ingest_upload_atk_db_connection()
         resource_data_json = simplejson.JSONEncoder().encode(
-            ingest_upload_atk_get_resource_component_children(resource_component_id)
+            atk.get_resource_component_children(db, resource_component_id)
         )
     except:
         return HttpResponse('Database error. Please contact an administrator.')
 
     return render(request, 'ingest/atk/match.html', locals())
-
-def ingest_upload_atk_get_resource_children(resource_id):
-    db = ingest_upload_atk_db_connection()
-    return ingest_upload_atk_get_resource_component_and_children(db, resource_id)
-
-def ingest_upload_atk_get_resource_component_children(resource_component_id):
-    db = ingest_upload_atk_db_connection()
-    return ingest_upload_atk_get_resource_component_and_children(db, resource_component_id, 'resource')
-
-def ingest_upload_atk_get_resource_component_and_children(db, resource_id, resource_type='collection', level=1, sort_data={}, **kwargs):
-    # we pass the sort position as a dict so it passes by reference and we
-    # can use it to share state during recursion
-
-    recurse_max_level = kwargs.get('recurse_max_level', False)
-    query             = kwargs.get('search_pattern', '')
-
-    # intialize sort position if this is the beginning of recursion
-    if level == 1:
-        sort_data['position'] = 0
-
-    sort_data['position'] = sort_data['position'] + 1
-
-    resource_data = {}
-
-    cursor = db.cursor() 
-
-    if resource_type == 'collection':
-        cursor.execute("SELECT title, dateExpression, persistentID FROM Resources WHERE resourceid=%s", (resource_id))
-
-        for row in cursor.fetchall():
-            resource_data['id']                 = resource_id
-            resource_data['sortPosition']       = sort_data['position']
-            resource_data['title']              = row[0]
-            resource_data['dates']              = row[1]
-            resource_data['identifier']         = row[2]
-            resource_data['levelOfDescription'] = 'collection'
-    else:
-        cursor.execute("SELECT title, dateExpression, persistentID, resourceLevel FROM ResourcesComponents WHERE resourceComponentId=%s", (resource_id))
-
-        for row in cursor.fetchall():
-            resource_data['id']                 = resource_id
-            resource_data['sortPosition']       = sort_data['position']
-            resource_data['title']              = row[0]
-            resource_data['dates']              = row[1]
-            resource_data['identifier']         = row[2]
-            resource_data['levelOfDescription'] = row[3]
-
-    resource_data['children'] = False
-
-    # fetch children if we haven't reached the maximum recursion level
-    if (not recurse_max_level) or level < recurse_max_level:
-        if resource_type == 'collection':
-            cursor.execute("SELECT resourceComponentId FROM ResourcesComponents WHERE parentResourceComponentId IS NULL AND resourceId=%s AND (title LIKE %s OR persistentID LIKE %s) ORDER BY FIND_IN_SET(resourceLevel, 'subseries,file'), title ASC", (resource_id, '%' + query + '%', '%' + query + '%'))
-        else:
-            cursor.execute("SELECT resourceComponentId FROM ResourcesComponents WHERE parentResourceComponentId=%s AND (title LIKE %s OR persistentID LIKE %s) ORDER BY FIND_IN_SET(resourceLevel, 'subseries,file'), title ASC", (resource_id, '%' + query + '%', '%' + query + '%'))
-
-        rows = cursor.fetchall()
-
-        if len(rows):
-            resource_data['children'] = []
-
-            for row in rows:
-                resource_data['children'].append(
-                    ingest_upload_atk_get_resource_component_and_children(
-                        db,
-                        row[0],
-                        'description',
-                        level + 1,
-                        sort_data
-                    )
-                 )
-
-    return resource_data
-
-    """
-    Example data:
-
-    return {
-      'id': '31',
-      'sortPosition': '1',
-      'identifier': 'PR01',
-      'title': 'Parent',
-      'levelOfDescription': 'collection',
-      'dates': '1880-1889',
-      'children': [{
-        'id': '23',
-        'sortPosition': '2',
-        'identifier': 'CH01',
-        'title': 'Child A',
-        'levelOfDescription': 'Sousfonds',
-        'dates': '1880-1888',
-        'children': [{
-          'id': '24',
-          'sortPosition': '3',
-          'identifier': 'GR01',
-          'title': 'Grandchild A',
-          'levelOfDescription': 'Item',
-          'dates': '1880-1888',
-          'children': False
-        },
-        {
-          'id': '25',
-          'sortPosition': '4',
-          'identifier': 'GR02',
-          'title': 'Grandchild B',
-          'levelOfDescription': 'Item',
-          'children': False
-        }]
-      },
-      {
-        'id': '26',
-        'sortPosition': '5',
-        'identifier': 'CH02',
-        'title': 'Child B',
-        'levelOfDescription': 'Sousfonds',
-        'dates': '1889',
-        'children': False
-      }]
-    }
-    """
