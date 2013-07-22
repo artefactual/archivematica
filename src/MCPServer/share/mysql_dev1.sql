e3c897551a4f84954b0a61932e6bca26a9dbb823
Mike Cantelon <mcantelon@gmail.com>
2013-07-21 22:04:01 -0700

    Fixed issue.

--- src/MCPServer/share/mysql_dev1.sql
+++ src/MCPServer/share/mysql_dev1.sql
@@ -39,7 +39,7 @@ UPDATE MicroServiceChoiceReplacementDic SET replacementDic='{\"%ContentdmServer%
 CREATE TABLE TransferMetadataSets (
   pk VARCHAR(50) NOT NULL,
   createdTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  createdByUserID INT(11) NOT NULL
+  createdByUserID INT(11) NOT NULL,
   transferType VARCHAR(50) NOT NULL,
   PRIMARY KEY (pk)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -60,7 +60,13 @@ INSERT INTO TransferMetadataFields (pk, createdTime, fieldLabel, fieldName, fiel
     VALUES ('fc69452c-ca57-448d-a46b-873afdd55e15', UNIX_TIMESTAMP(), 'Media number', 'media_number', 'text', 0);
 
 INSERT INTO TransferMetadataFields (pk, createdTime, fieldLabel, fieldName, fieldType, sortOrder)
-    VALUES ('a9a4efa8-d8ab-4b32-8875-b10da835621c', UNIX_TIMESTAMP(), 'Label text', 'label_text', 'text', 1);
+    VALUES ('a9a4efa8-d8ab-4b32-8875-b10da835621c', UNIX_TIMESTAMP(), 'Label text', 'label_text', 'textarea', 1);
+
+INSERT INTO TransferMetadataFields (pk, createdTime, fieldLabel, fieldName, fieldType, sortOrder)
+    VALUES ('367ef53b-49d6-4a4e-8b2f-10267d6a7db1', UNIX_TIMESTAMP(), 'Media manufacture', 'media_manufacture', 'text', 2);
+
+INSERT INTO TransferMetadataFields (pk, createdTime, fieldLabel, fieldName, fieldType, sortOrder)
+    VALUES ('277727e4-b621-4f68-acb4-5689f81f31cd', UNIX_TIMESTAMP(), 'Serial number', 'serial_number', 'text', 3);
 
 CREATE TABLE TransferMetadataFieldValues (
   pk varchar(50) NOT NULL,
