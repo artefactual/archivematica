c40953b2da2fe926ed6c5e63645b04d4c7cbd9c3
Holly Becker <hbecker@artefactual.com>
2013-09-04 15:35:31 -0700

    Upgrade to work with Django 1.5
    refs #5207
    
    breadcrumb_url is a wrapper around url, so it needs to have the ''
    around verbatim url destinations.  Enabled timezone support, which seems
    to fix a bug with the old version of tastypie bundled with
    Archivematica.

--- src/dashboard/src/settings/common.py
+++ src/dashboard/src/settings/common.py
@@ -62,6 +62,10 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+# Enable timezone support, for more info see:
+# https://docs.djangoproject.com/en/dev/topics/i18n/timezones/
+USE_TZ = True
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
