f53796bca2bd80f7c9da702406465ef98ed9bbce
Misty De Meo <mistydemeo@gmail.com>
2013-09-23 09:39:42 -0700

    executeOrRun: accept optional argument parameter
    This allows commands run via the executeOrRun functions to accept
    additional arguments. If a command is passed with arguments, the
    initial command should be passed as a string representing an
    executable command on its own, as shlex.split() will not be called on
    it.
    
    This also allows launchSubProcess() to accept an array as the first
    parameter, in which case it will be passed to subprocess.Popen
    unmodified.
    
    Refs #5625.

--- src/archivematicaCommon/lib/executeOrRunSubProcess.py
+++ src/archivematicaCommon/lib/executeOrRunSubProcess.py
@@ -26,18 +26,42 @@ import uuid
 import os
 import sys
 
-def launchSubProcess(command, stdIn="", printing=True):
+def launchSubProcess(command, stdIn="", printing=True, arguments=[]):
+    """
+    Launches a subprocess using ``command``, where ``command`` is either:
+    a) a single string containing a commandline statement, or
+    b) an array of commands and parameters.
+
+    In the former case, ``command`` is first split via shlex.split() before being
+    executed. No subshell will be used in either case; the commands are
+    directly execed.
+
+    Keyword arguments:
+    stdIn:      A string which will be fed as standard input to the executed process.
+                Default is empty.
+    printing:   Boolean which controls whether the subprocess's output is
+                printed to standard output. Default is True.
+    arguments:  An array of arguments to pass to ``command``. Note that this is only
+                honoured if ``command`` is an array, and will be ignored if ``command``
+                is a string.
+    """
     stdError = ""
     stdOut = ""
     #print  >>sys.stderr, command
     try:
+        # Split command strings but pass through arrays untouched
+        if isinstance(command, basestring):
+            command = shlex.split(command)
+        else:
+            command.extend(arguments)
+
         my_env = os.environ
         my_env['PYTHONIOENCODING'] = 'utf-8'
         if (not my_env.has_key('LANG')) or (not my_env['LANG']):
              my_env['LANG'] = 'en_US.UTF-8'
         if (not my_env.has_key('LANGUAGE')) or (not my_env['LANGUAGE']):
              my_env['LANGUAGE'] = my_env['LANG']
-        p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, env=my_env)
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, env=my_env)
         stdOut, stdError = p.communicate(input=stdIn)
         #append the output to stderror and stdout
         if printing:
@@ -56,15 +80,17 @@ def launchSubProcess(command, stdIn="", printing=True):
 
 
 
-def createAndRunScript(text, stdIn="", printing=True):
+def createAndRunScript(text, stdIn="", printing=True, arguments=[]):
     #output the text to a /tmp/ file
     scriptPath = "/tmp/" + uuid.uuid4().__str__()
     FILE = os.open(scriptPath, os.O_WRONLY | os.O_CREAT, 0770)
     os.write(FILE, text)
     os.close(FILE)
+    cmd = [scriptPath]
+    cmd.extend(arguments)
 
     #run it
-    ret = launchSubProcess(scriptPath, stdIn="", printing=True)
+    ret = launchSubProcess(cmd, stdIn="", printing=True)
 
     #remove the temp file
     os.remove(scriptPath)
@@ -73,14 +99,41 @@ def createAndRunScript(text, stdIn="", printing=True):
 
 
 
-def executeOrRun(type, text, stdIn="", printing=True):
+def executeOrRun(type, text, stdIn="", printing=True, arguments=[]):
+    """
+    Attempts to run the provided command on the shell, with the text of
+    "stdIn" passed as standard input if provided. The type parameter
+    should be one of the following:
+
+    command:    Runs the argument as a direct command line. In this usage,
+                "text" should be a complete commandline statement,
+                which will be split with shlex.split(), or an array.
+    bashScript, pythonScript:
+                Interprets the "text" argument as the source code to either
+                a bash or python script, as appropriate. The appropriate
+                shebang will be prepended, then the script will be written
+                to disk and executed. If the "arguments" parameter is passed,
+                they will be appended to the array that is built to be
+                passed to subprocess.Popen.
+    as_is:      Like the above, except that the provided script is executed
+                without modification.
+
+    Keyword arguments:
+    stdIn:      A string which will be fed as standard input to the executed process.
+                Default is empty.
+    printing:   Boolean which controls whether the subprocess's output is
+                printed to standard output. Default is True.
+    arguments:  An array of arguments to pass to ``command``. Note that this is only
+                honoured if ``command`` is an array, and will be ignored if ``command``
+                is a string.
+    """
     if type == "command":
-        return launchSubProcess(text, stdIn=stdIn, printing=printing)
+        return launchSubProcess(text, stdIn=stdIn, printing=printing, arguments=arguments)
     if type == "bashScript":
         text = "#!/bin/bash\n" + text
-        return createAndRunScript(text, stdIn=stdIn, printing=printing)
+        return createAndRunScript(text, stdIn=stdIn, printing=printing, arguments=arguments)
     if type == "pythonScript":
         text = "#!/usr/bin/python -OO\n" + text
-        return createAndRunScript(text, stdIn=stdIn, printing=printing)
+        return createAndRunScript(text, stdIn=stdIn, printing=printing, arguments=arguments)
     if type == "as_is":
-        return createAndRunScript(text, stdIn=stdIn, printing=printing)
+        return createAndRunScript(text, stdIn=stdIn, printing=printing, arguments=arguments)
