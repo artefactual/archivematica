34f00b0f5ca30043b81e70c12d0bbb963adbee98
Holly Becker <hbecker@artefactual.com>
2013-10-02 16:41:14 -0700

    Fixed Django filtering
    Related to removing the DeprecationWarning for Django's logging.

--- src/dashboard/src/settings/common.py
+++ src/dashboard/src/settings/common.py
@@ -186,6 +186,11 @@ EMAIL_FILE_PATH = '/tmp/dashboard-django'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
       'mail_admins': {
         'level': 'ERROR',
