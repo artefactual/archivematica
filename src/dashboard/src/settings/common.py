0def57232504e8264b3f172fda076cce991dd295
Misty De Meo <mistydemeo@gmail.com>
2013-10-02 14:04:55 -0700

    django: add mail_admins filter
    This filter will be required in the future, and results in a
    DeprecationWarning currently.

--- src/dashboard/src/settings/common.py
+++ src/dashboard/src/settings/common.py
@@ -189,6 +189,7 @@ LOGGING = {
     'handlers': {
       'mail_admins': {
         'level': 'ERROR',
+        'filters': ['require_debug_false'],
         'class': 'django.utils.log.AdminEmailHandler'
       }
     },
