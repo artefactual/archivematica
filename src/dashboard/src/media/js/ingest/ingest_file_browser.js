35f48b380c8e8bc9ad469c5ddead13076e07dff0
Mike Cantelon <mcantelon@gmail.com>
2013-12-11 15:25:28 -0800

    Refactored SIP arrange JS.
    Cleaned up SIP arrange JS, fixed minor issue, and moved JS out of
    HTML template.

--- src/dashboard/src/media/js/ingest/ingest_file_browser.js
+++ src/dashboard/src/media/js/ingest/ingest_file_browser.js
@@ -17,35 +17,36 @@ You should have received a copy of the GNU General Public License
 along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
 */
 function setupBacklogBrowser(originalsDirectory, arrangeDirectory) {
-
   var backlogBrowserEntryClickHandler = function(event) {
-    var explorer = event.data.self.explorer
-      , explorerId = explorer.id
-      , entryEl = this
-      , entryId = $(this).attr('id')
-      , borderCssSpec = '1px solid red';
+    if (typeof event.data != 'undefined') {
+      var explorer = event.data.self.explorer
+        , explorerId = explorer.id
 
-    if (explorer.selectedEntryId == entryId) {
-      // un-highlight selected entry
-      $(entryEl).css('border', '');
+        var entryEl = this
+        , entryId = $(this).attr('id')
+        , borderCssSpec = '1px solid red';
 
-      // remove selected entry
-      explorer.selectedEntryId = undefined;
-    } else {
-      // remove highlighting of existing entries
-      $('#' + explorerId).find('.backbone-file-explorer-entry').css('border', '');
+      if (explorer.selectedEntryId == entryId) {
+        // un-highlight selected entry
+        $(entryEl).css('border', '');
 
-      // highlight selected entry
-      $(entryEl).css('border', borderCssSpec);
+        // remove selected entry
+        explorer.selectedEntryId = undefined;
+      } else {
+        // remove highlighting of existing entries
+        $('#' + explorerId).find('.backbone-file-explorer-entry').css('border', '');
 
-      // change selected entry
-      explorer.selectedEntryId = entryId; // $(entryEl).attr('id')
+        // highlight selected entry
+        $(entryEl).css('border', borderCssSpec);
+
+        // change selected entry
+        explorer.selectedEntryId = entryId;
+      }
     }
   }
 
   function moveHandler(move) {
-      console.log(move);
-    // don't allow dragging stuff from originals directory?
+    // don't allow moving anything into the originals directory
     if (move.self.id != 'originals') {
       if (move.allowed) {
         move.self.busy();
@@ -81,10 +82,10 @@ function setupBacklogBrowser(originalsDirectory, arrangeDirectory) {
           }
         );
       } else {
-        alert("You can't move a directory into its subdirectory.");
+        move.self.alert('Error', "You can't move a directory into its subdirectory.");
       }
     } else {
-      alert("You can't copy into the originals directory.");
+      move.self.alert('Error', "You can't copy into the originals directory.");
     }
   }
 
@@ -117,3 +118,95 @@ function setupBacklogBrowser(originalsDirectory, arrangeDirectory) {
     'arrange': arrange
   };
 }
+
+// spawn browsers
+var originals_browser,
+    arrange_browser;
+
+$(document).ready(function() {
+  var arrange_directory = '/var/archivematica/sharedDirectory/arrange';
+  browsers = setupBacklogBrowser(
+    '/var/archivematica/sharedDirectory/www/AIPsStore/transferBacklog/originals',
+    arrange_directory
+  );
+  originals_browser = browsers['originals'];
+  arrange_browser = browsers['arrange'];
+
+  // delete button functionality
+  $('#arrange_delete_button').click(function() {
+    if (typeof arrange_browser.selectedEntryId === 'undefined') {
+      arrange_browser.alert('Delete', 'Please select a directory to delete.');
+    } else {
+      // only allow top-level directories to be deleted
+      var delete_path = arrange_browser.getPathForCssId(arrange_browser.selectedEntryId);
+      if (delete_path.split('/').length >= arrange_directory.split('/').length + 1) {
+        arrange_browser.alert('Delete', 'You can only delete top-level directories.');
+      } else {
+        var path = arrange_browser.getPathForCssId(arrange_browser.selectedEntryId)
+          , type = arrange_browser.getTypeForCssId(arrange_browser.selectedEntryId);
+
+        arrange_browser.confirm(
+          'Delete',
+          'Are you sure you want to delete this directory or file?',
+          function() {
+            arrange_browser.deleteEntry(path, type);
+          }
+        );
+      }
+    }
+  });
+
+  // create SIP button functionality
+  $('#arrange_create_sip_button').click(function() {
+    if (typeof arrange_browser.selectedEntryId === 'undefined') {
+      arrange_browser.alert('Create SIP', 'Please select a directory before creating a SIP.');
+    } else {
+      var entryDiv = $('#' + arrange_browser.selectedEntryId)
+        , path = arrange_browser.getPathForCssId(arrange_browser.selectedEntryId);
+
+      arrange_browser.confirm(
+        'Create SIP',
+        'Are you sure you want to create a SIP?',
+        function() {
+          $.post(
+            '/filesystem/copy_from_arrange/',
+            {filepath: path},
+            function(result) {
+              var title = (result.error) ? 'Error' : '';
+              arrange_browser.alert(
+                title,
+                result.message
+              );
+              if (!result.error) {
+                $(entryDiv).next().hide();
+                $(entryDiv).hide();
+              }
+            }
+          );
+        }
+      );
+    }
+  });
+
+  // open originals file button functionality
+  $('#open_originals_file_button').click(function() {
+    var entryDiv = $('#' + originals_browser.selectedEntryId)
+      , path = originals_browser.getPathForCssId(originals_browser.selectedEntryId);
+
+    window.open(
+      '/filesystem/download?filepath=' + encodeURIComponent(path),
+      '_blank'
+    );
+  });
+
+  // open arrange file button functionality
+  $('#open_arrange_file_button').click(function() {
+    var entryDiv = $('#' + arrange_browser.selectedEntryId)
+      , path = arrange_browser.getPathForCssId(arrange_browser.selectedEntryId);
+
+    window.open(
+      '/filesystem/download?filepath=' + encodeURIComponent(path),
+      '_blank'
+    );
+  });
+});
