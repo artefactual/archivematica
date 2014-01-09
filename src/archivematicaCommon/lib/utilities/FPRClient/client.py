dc3449f29ee8500041dda5796b352c3710b59a77
Holly Becker <hbecker@artefactual.com>
2014-01-08 17:24:20 -0800

    Update FPRClient to make more robust
    refs #5825
    
    Update FPR client to fix some bugs downloading rules, and add tests for
    those cases.
    
    When downloading a new rule (not replacing anything), always set it to
    active.  If not, the entire chain is set to disabled.
    
    When downloading a rule that has a local modification, insert the newly
    downloaded rule immediately after the one it replaces in the modification
    chain, updating the local modification to point at the newly downloaded
    rule.  If not, the new rule does not appear in the GUI.
    
    Rename main.py to client.py in the FPRClient because it conflicts with the
    main folder in the dashboard when importing.  Update the installer for the
    change.  Update fpradmin version.
    
    Add tests for the FPRClient in test_fprclient.py.  Can be run with pytest.

--- src/archivematicaCommon/lib/utilities/FPRClient/client.py
+++ src/archivematicaCommon/lib/utilities/FPRClient/client.py
@@ -1,48 +1,22 @@
-#!/usr/bin/python -OO
+#!/usr/bin/python2 -OO
 
-# This file is part of Archivematica.
-#
-# Copyright 2010-2013 Artefactual Systems Inc. <http://artefactual.com>
-#
-# Archivematica is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Archivematica is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
-
-# @package Archivematica
-# @subpackage FPRClient
-# @author Joseph Perry <joseph@artefactual.com>
 import os
 import sys
-import uuid
 
 import getFromRestAPI
 
-sys.path.append("/usr/lib/archivematica/archivematicaCommon")
-import databaseInterface
+from annoying.functions import get_object_or_None
 
 # Set up Django settings
-path = '/usr/share/archivematica/dashboard'
-if path not in sys.path:
-    sys.path.append(path)
+sys.path.append('/usr/share/archivematica/dashboard')
 if 'DJANGO_SETTINGS_MODULE' not in os.environ:
     os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.common'
 import django.db
 from django.conf import settings as django_settings
 
-from annoying.functions import get_object_or_None
-
 from fpr import models
+import main.models
 
-databaseInterface.printSQL = False
 
 class FPRClient(object):
     """FPR Client provides methods to download data from FPR Server"""
@@ -55,23 +29,17 @@ class FPRClient(object):
         self.retry = {}
 
     def getMaxLastUpdate(self):
-        sql = """SELECT pk, variableValue FROM UnitVariables WHERE unitType = 'FPR' AND unitUUID = 'Client' AND variable = 'maxLastUpdate' """
-        rows = databaseInterface.queryAllSQL(sql)
-        if rows:
-            self.maxLastUpdateUUID, self.maxLastUpdate = rows[0]
-        else:
-            self.maxLastUpdate = "2000-01-01T00:00:00"
-            self.maxLastUpdateUUID = str(uuid.uuid4())
-    
-    def setMaxLastUpdate(self):
-        sql = """INSERT INTO UnitVariables(pk, variableValue, variable, unitType, unitUUID) VALUES ('{pk}', '{variableValue}', 'maxLastUpdate', 'FPR', 'Client') ON DUPLICATE KEY UPDATE variableValue='{variableValue}';""".format(
-                pk=self.maxLastUpdateUUID,
-                variableValue=self.maxLastUpdate
-            )
-        databaseInterface.runSQL(sql)
+        (last_updated, _) = main.models.UnitVariable.objects.get_or_create(
+                unittype='FPR', unituuid='Client', variable='maxLastUpdate',
+                defaults={'variablevalue': "2000-01-01T00:00:00"})
+        self.maxLastUpdate = last_updated.variablevalue
+        self.maxLastUpdateUUID = last_updated.id
 
-    def addResource(self, fields, table):
-        """ Add an object with data `fields` to the model `table`.  Adds to
+    def setMaxLastUpdate(self):
+        main.models.UnitVariable.objects.filter(id=self.maxLastUpdateUUID).update(variablevalue=self.maxLastUpdate)
+
+    def addResource(self, fields, model):
+        """ Add an object with data `fields` to the model `model`.  Adds to
         retry dictionary if it fails to be inserted because of an integrity
         error so it can be retried. """
         # Update lastmodified if exists
@@ -79,15 +47,15 @@ class FPRClient(object):
             self.maxLastUpdate = fields['lastmodified']
 
         # If an fields with this UUID exists, update enabled
-        obj = get_object_or_None(table, uuid=fields['uuid'])
+        obj = get_object_or_None(model, uuid=fields['uuid'])
         if obj:
-            print 'Object already in DB:', get_object_or_None(table, uuid=fields['uuid'])
-            if not hasattr(table, 'replaces') and hasattr(table, 'enabled'):
+            print 'Object already in DB:', get_object_or_None(model, uuid=fields['uuid'])
+            if not hasattr(model, 'replaces') and hasattr(model, 'enabled'):
                 obj.enabled = fields['enabled']
                 obj.save()
             return
-        # Otherwise, new fields, need to add to table
-        # print 'fields', fields
+        # Otherwise, new fields, need to add to database
+
         # TastyPie doesn't like fields named format, so they're all fmt
         if 'fmt' in fields:
             fields['format'] = fields.pop('fmt')
@@ -95,7 +63,7 @@ class FPRClient(object):
         # Only keep fields that are in the model
         valid_fields = dict(
             [ (k, v) for k, v in fields.iteritems()
-                if k in table._meta.get_all_field_names()
+                if k in model._meta.get_all_field_names()
             ])
 
         # Convert foreign keys from URIs to just UUIDs
@@ -107,7 +75,7 @@ class FPRClient(object):
                 del valid_fields[field]
                 valid_fields[field+u"_id"] = uuid
         # Insert FormatGroup for Formats if not exist
-        if table == models.Format:
+        if model == models.Format:
             if not get_object_or_None(
                     models.FormatGroup,
                     uuid=fields['group']['uuid']):
@@ -117,29 +85,42 @@ class FPRClient(object):
 
         # Create
         try:
-            obj = table.objects.create(**valid_fields)
+            obj = model.objects.create(**valid_fields)
         except django.db.utils.IntegrityError:
-            self.retry[table].append(valid_fields)
+            self.retry[model].append(valid_fields)
             print 'Integrity error failed; will retry later'
             return
         self.count_rules_updated += 1
 
         # Update enabled on self and replaces
-        if hasattr(table, 'replaces') and obj.replaces:
-            if obj.replaces.enabled:
+        if hasattr(model, 'replaces'):
+            if obj.replaces is None:
+                # First rule in a chain is always enabled
+                obj.enabled = True
+            elif obj.replaces.enabled:
+                # If replacing an active rule, disable it and set self active
                 obj.replaces.enabled = False
                 obj.replaces.save()
+                obj.enabled = True
             else:  # obj.replaces is disabled
                 obj.enabled = False
-                obj.save()
+            obj.save()
+
+            # Check for a manual replacement
+            # Look for another rule that has the same replaces value
+            # Insert obj as that rule's parent
+            if obj.replaces is not None:
+                existing_rules = model.objects.filter(replaces=obj.replaces).exclude(uuid=valid_fields['uuid'])
+                if len(existing_rules) >= 1:
+                    existing_rules[0].replaces = obj
+                    existing_rules[0].save()
+
         print 'Added:', obj
 
-    
     def autoUpdateFPR(self):
         self.getMaxLastUpdate()
         maxLastUpdateAtStart = self.maxLastUpdate
         print 'maxLastUpdateAtStart', maxLastUpdateAtStart
-        databaseInterface.runSQL("SET foreign_key_checks = 0;")
         resources = [
             (models.Format, 'format'),
             (models.FormatVersion, 'format-version'),
@@ -189,11 +170,10 @@ class FPRClient(object):
                         obj.save()
                 print 'Added:', obj
 
-        databaseInterface.runSQL("SET foreign_key_checks = 1;")
         print 'maxLastUpdate at end', self.maxLastUpdate
         if self.maxLastUpdate != maxLastUpdateAtStart:
             self.setMaxLastUpdate()
-    
+
     def getUpdates(self):
         status = 'failed'
         exception = None
@@ -208,6 +188,7 @@ class FPRClient(object):
         except Exception as e:
             response = 'Error updating FPR'
             exception = e
+            raise
         else:
             status = 'success'
             if self.count_rules_updated == 0:
@@ -215,7 +196,8 @@ class FPRClient(object):
             elif self.count_rules_updated > 0:
                 response = "Successfully updated FPR: {} changes".format(self.count_rules_updated)
         return (status, response, exception)
-        
+
+
 if __name__ == '__main__':
     ret = FPRClient().getUpdates()
     print ret
