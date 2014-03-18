5e6a906e31df03425a3dc86585056e1bccf4ae5f
Misty De Meo <mdemeo@artefactual.com>
2014-03-18 11:32:05 -0700

    Start transfer: correctly handle archives
    This particularly affects zipped bag transfers.
    
    Fixes #6464.

--- src/dashboard/src/media/js/transfer/component_form.js
+++ src/dashboard/src/media/js/transfer/component_form.js
@@ -102,13 +102,14 @@ var TransferComponentFormView = Backbone.View.extend({
         }
 
         // move from temp directory to appropriate watchdir
+        var decoded_path = Base64.decode(path);
         var url = '/filesystem/ransfer/'
-          , isArchiveFile = path.toLowerCase().indexOf('.zip') != -1 || path.toLowerCase().indexOf('.tgz') != -1 || path.toLowerCase().indexOf('.tar.gz') != -1
+          , isArchiveFile = decoded_path.toLowerCase().indexOf('.zip') != -1 || decoded_path.toLowerCase().indexOf('.tgz') != -1 || decoded_path.toLowerCase().indexOf('.tar.gz') != -1
           , filepath;
 
         // if transfer is a ZIP file, then extract basename add to temporary directory
         if (isArchiveFile) {
-          filepath = tempDir + '/' + path.replace(/\\/g,'/').replace( /.*\//, '' );
+          filepath = tempDir + '/' + decoded_path.replace(/\\/g,'/').replace( /.*\//, '' );
         } else {
           filepath = tempDir + '/' + transfer.name;
         }
