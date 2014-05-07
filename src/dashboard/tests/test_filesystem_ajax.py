6f61344b9c60a7f988218f2f2447dbb8fbad8159
Holly Becker <hbecker@artefactual.com>
2014-05-07 12:43:41 -0700

    Update & add more tests
    refs #6131
    
    Update tests for code changes, including base64 and a default arrange
    directory.  Add tests for new functions.  Add new more valid fixture
    data.

--- src/dashboard/tests/test_filesystem_ajax.py
+++ src/dashboard/tests/test_filesystem_ajax.py
@@ -4,6 +4,7 @@
 # bash: PYTHONPATH='./src' py.test --ds 'settings.local' -v
 # fish: env PYTHONPATH='./src' py.test --ds 'settings.local' -v
 
+import base64
 import json
 
 from django.core.urlresolvers import reverse
@@ -11,6 +12,9 @@ from django.test import TestCase
 from django.test.client import Client
 
 from main import models
+
+# Run from src/dashboard:
+# env PYTHONPATH='./src' py.test --ds 'settings.local'
 
 class TestSIPArrange(TestCase):
 
@@ -25,29 +29,102 @@ class TestSIPArrange(TestCase):
         assert len(objs) > 0
 
     def test_arrange_contents_data(self):
-        response = self.client.get(reverse('components.filesystem_ajax.views.arrange_contents'), follow=True)
+        response = self.client.get(reverse('components.filesystem_ajax.views.arrange_contents'), {'path': None}, follow=True)
+        # Order of response lists not guaranteed - this is set to be the same as the response
         expected_json = {
             "directories": [
-                "folder",
-                "newsip"
+                base64.b64encode("newsip"),
+                base64.b64encode("toplevel"),
             ],
             "entries": [
-                "folder",
-                "newsip"
+                base64.b64encode("newsip"),
+                base64.b64encode("toplevel"),
             ]
         }
         assert response.status_code == 200
         assert json.loads(response.content) == expected_json
 
         # Folder, without /
-        response = self.client.get(reverse('components.filesystem_ajax.views.arrange_contents'), {'path': '/arrange/folder'}, follow=True)
+        response = self.client.get(reverse('components.filesystem_ajax.views.arrange_contents'), {'path': base64.b64encode('/arrange/newsip/objects/')}, follow=True)
         expected_json = {
             "directories": [
-                "objects"
+                base64.b64encode("evelyn_s_second_photo"),
             ],
             "entries": [
-                "item_main",
-                "objects"
+                base64.b64encode("evelyn_s_photo.jpg"),
+                base64.b64encode("evelyn_s_second_photo"),
+            ]
+        }
+        assert response.status_code == 200
+        assert json.loads(response.content) == expected_json
+
+    def test_delete_arranged_files(self):
+        # Delete files
+        response = self.client.post(reverse('components.filesystem_ajax.views.delete_arrange'), data={'filepath': base64.b64encode('/arrange/newsip/')}, follow=True)
+        assert response.status_code == 200
+        assert json.loads(response.content) == {'message': 'Delete successful.'}
+        # Check deleted
+        response = self.client.get(reverse('components.filesystem_ajax.views.arrange_contents'), {'path': base64.b64encode('/arrange')}, follow=True)
+        expected_json = {
+            "directories": [
+                base64.b64encode("toplevel"),
+            ],
+            "entries": [
+                base64.b64encode("toplevel"),
+            ]
+        }
+        assert response.status_code == 200
+        assert json.loads(response.content) == expected_json
+
+    def test_create_arranged_directory(self):
+        # Create directory
+        response = self.client.post(reverse('components.filesystem_ajax.views.create_directory_within_arrange'), data={'path': base64.b64encode('/arrange/new_dir')}, follow=True)
+        assert response.status_code == 200
+        assert json.loads(response.content) == {'message': 'Creation successful.'}
+        # Check created
+        response = self.client.get(reverse('components.filesystem_ajax.views.arrange_contents'), {'path': base64.b64encode('/arrange')}, follow=True)
+        expected_json = {
+            "directories": [
+                base64.b64encode("newsip"),
+                base64.b64encode("new_dir"),
+                base64.b64encode("toplevel"),
+            ],
+            "entries": [
+                base64.b64encode("newsip"),
+                base64.b64encode("new_dir"),
+                base64.b64encode("toplevel"),
+            ]
+        }
+        assert response.status_code == 200
+        assert json.loads(response.content) == expected_json
+
+    def test_move_within_arrange(self):
+        # Move directory
+        response = self.client.post(reverse('components.filesystem_ajax.views.move_within_arrange'), data={'filepath': base64.b64encode('/arrange/newsip/'), 'destination': base64.b64encode('/arrange/toplevel/')}, follow=True)
+        assert response.status_code == 200
+        assert json.loads(response.content) == {'message': 'SIP files successfully moved.'}
+        # Check gone from parent
+        response = self.client.get(reverse('components.filesystem_ajax.views.arrange_contents'), {'path': base64.b64encode('/arrange')}, follow=True)
+        expected_json = {
+            "directories": [
+                base64.b64encode("toplevel"),
+            ],
+            "entries": [
+                base64.b64encode("toplevel"),
+            ]
+        }
+        assert response.status_code == 200
+        assert json.loads(response.content) == expected_json
+        # Check now in subdirectory
+        response = self.client.get(reverse('components.filesystem_ajax.views.arrange_contents'), {'path': base64.b64encode('/arrange/toplevel')}, follow=True)
+        expected_json = {
+            "directories": [
+                base64.b64encode("newsip"),
+                base64.b64encode("subsip"),
+            ],
+            "entries": [
+                base64.b64encode("newsip"),
+                base64.b64encode("subsip"),
             ]
         }
         assert response.status_code == 200
