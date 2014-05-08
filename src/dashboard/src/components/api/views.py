c7a947199ffb12c11ecc1611a1eb9d4b4a049e65
Holly Becker <hbecker@artefactual.com>
2014-05-08 12:32:02 -0700

    Cleanup
    Add instruction to API whitelist template, updated code to match.
    Whitelisted IPs are now whitespace separated, instead of newline
    separated.  Add display functions for MicroServiceChain and
    MicroServiceChainChoice.  Cleanup imports.

--- src/dashboard/src/components/api/views.py
+++ src/dashboard/src/components/api/views.py
@@ -15,13 +15,21 @@
 # You should have received a copy of the GNU General Public License
 # along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, json
-from django.http import Http404, HttpResponse, HttpResponseForbidden, HttpResponseServerError
+# stdlib, alphabetical
+import json
+import os
+
+# Core Django, alphabetical
 from django.db.models import Q
+from django.http import Http404, HttpResponseForbidden, HttpResponseServerError
+
+# External dependencies, alphabetical
 from tastypie.authentication import ApiKeyAuthentication
+
+# This project, alphabetical
 from contrib.mcp.client import MCPClient
-from main import models
 from components import helpers
+from main import models
 
 def authenticate_request(request):
     error = None
@@ -31,11 +39,9 @@ def authenticate_request(request):
 
     if authorized == True:
         client_ip = request.META['REMOTE_ADDR']
-        whitelist = helpers.get_setting('api_whitelist', '127.0.0.1').split("\r\n")
-        try:
-            whitelist.index(client_ip)
-            return
-        except:
+        whitelist = helpers.get_setting('api_whitelist', '127.0.0.1').split()
+        # logging.debug('client IP: %s, whitelist: %s', client_ip, whitelist)
+        if client_ip not in whitelist:
             error = 'Host/IP ' + client_ip + ' not authorized.'
     else:
         error = 'API key not valid.'
@@ -52,7 +58,6 @@ def unapproved_transfers(request):
         response = {}
 
         if auth_error == None:
-            message    = ''
             error      = None
             unapproved = []
 
@@ -126,7 +131,6 @@ def approve_transfer(request):
         response = {}
 
         if auth_error == None:
-            message = ''
             error   = None
 
             directory = request.POST.get('directory', '')
@@ -220,8 +224,9 @@ def approve_transfer_via_mcp(directory, type, user_id):
                 else:
                     error = 'Error: could not find MCP choice to execute.'
 
-            except:
+            except Exception:
                 error = 'Unable to find unapproved transfer directory.'
+                # logging.exception(error)
 
     else:
         error = 'Please specify a transfer directory.'
