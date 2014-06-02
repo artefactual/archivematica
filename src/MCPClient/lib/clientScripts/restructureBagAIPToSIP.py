a9d77b3a439f11c2b779a724cd49e90ff15cc638
Holly Becker <hbecker@artefactual.com>
2014-06-02 16:44:48 -0700

    Restructure reingest AIP to resemble SIP
    refs #6488
    
    Reingested AIP restructuring makes it look more like a normal SIP, instead
    of putting most things in submissionDocumentation.

--- src/MCPClient/lib/clientScripts/restructureBagAIPToSIP.py
+++ src/MCPClient/lib/clientScripts/restructureBagAIPToSIP.py
@@ -29,24 +29,47 @@ import shutil
 from custom_handlers import get_script_logger
 import archivematicaFunctions
 
+def _move_file(src, dst, exit_on_error=True):
+    print 'Moving', src, 'to', dst
+    try:
+        shutil.move(src, dst)
+    except IOError:
+        print 'Could not move', src
+        if exit_on_error:
+            raise
+
 if __name__ == '__main__':
     logger = get_script_logger("archivematica.mcp.client.restructureBagAIPToSIP")
 
-    path = sys.argv[1]
-    os.makedirs(os.path.join(path, "metadata/submissionDocumentation"))
+    sip_path = sys.argv[1]
 
-    # Move everything to submission documentation
-    for item in os.listdir(path):
-        if item in archivematicaFunctions.OPTIONAL_FILES + ["metadata"]:
+    # Move everything out of data directory
+    for item in os.listdir(os.path.join(sip_path, 'data')):
+        src = os.path.join(sip_path, 'data', item)
+        dst = os.path.join(sip_path, item)
+        _move_file(src, dst)
+
+    os.rmdir(os.path.join(sip_path, 'data'))
+
+    # Move metadata and logs out of objects if they exist
+    src = os.path.join(sip_path, 'objects', 'metadata')
+    dst = os.path.join(sip_path, 'metadata')
+    _move_file(src, dst, exit_on_error=False)
+
+    src = os.path.join(sip_path, 'objects', 'logs')
+    dst = os.path.join(sip_path, 'logs')
+    _move_file(src, dst, exit_on_error=False)
+
+    # Move anything unexpected to submission documentation
+    # Leave objects, metadata, etc
+    # Original METS ends up in submissionDocumentation
+    os.makedirs(os.path.join(sip_path, 'metadata', 'submissionDocumentation'))
+    for item in os.listdir(sip_path):
+        # Leave SIP structure
+        if item in archivematicaFunctions.OPTIONAL_FILES + archivematicaFunctions.REQUIRED_DIRECTORIES:
             continue
-        src = os.path.join(path, item)
-        dst = os.path.join(path, "metadata/submissionDocumentation", item)
-        print 'Moving', src, 'to', dst
-        shutil.move(src, dst)
-    src = os.path.join(path, "metadata/submissionDocumentation", "data/objects")
-    dst = path
-    if os.path.exists(src):
-        print 'Moving', src, 'to', dst
-        shutil.move(src, dst)
+        src = os.path.join(sip_path, item)
+        dst = os.path.join(sip_path, 'metadata', 'submissionDocumentation', item)
+        _move_file(src, dst)
 
-    archivematicaFunctions.create_structured_directory(path, manual_normalization=True, printing=True)
+    archivematicaFunctions.create_structured_directory(sip_path, manual_normalization=True, printing=True)
