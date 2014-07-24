8dc27cf89bf544a5c4d8b49a00ec9d612b0a0443
Misty De Meo <mdemeo@artefactual.com>
2014-07-23 17:39:40 -0700

    get_transfer_file_info: only return precise matches
    Elasticsearch's top match may not actually be an exact match, even if
    an exact match is in the set.
    
    Fixes #7001

--- src/archivematicaCommon/lib/elasticSearchFunctions.py
+++ src/archivematicaCommon/lib/elasticSearchFunctions.py
@@ -696,12 +696,23 @@ def get_transfer_file_info(field, value):
         pyes.FieldQuery(pyes.FieldParameter(field, searchvalue)),
         indicies=indicies,
     )
-    if len(documents['hits']['hits']) > 0:
+    result_count = len(documents['hits']['hits'])
+    if result_count == 1:
         results = documents['hits']['hits'][0]['_source']
+    elif result_count > 1:
+        # Elasticsearch was sometimes ranking results for a different filename above
+        # the actual file being queried for; in that case only consider results
+        # where the value is an actual precise match.
+        filtered_results = [results for results in documents['hits']['hits']
+                            if results['_source'][field] == value]
+        results = filtered_results[0]['_source']
+
+        result_count = len(filtered_results)
+        if result_count > 1:
+            logging.warning('get_transfer_file_info returned %s results for query %s: %s (using first result)',
+                            result_count, field, value)
 
     logging.debug('get_transfer_file_info: results: %s', results)
-    if results.get(field) != value:
-        logging.warning('May not be correct transfer info: %s is %s instead of %s', field, results.get(field), value)
     return results
 
 
