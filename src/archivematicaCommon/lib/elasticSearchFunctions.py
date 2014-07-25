07660c3c4946887d7591038ca535c13e685b3c6d
Misty De Meo <mdemeo@artefactual.com>
2014-07-25 11:37:22 -0700

    get_transfer_file_info: raise if no precise match
    refs #7001

--- src/archivematicaCommon/lib/elasticSearchFunctions.py
+++ src/archivematicaCommon/lib/elasticSearchFunctions.py
@@ -47,6 +47,9 @@ logging.basicConfig(filename="/tmp/archivematicaDashboard.log",
 pathToElasticSearchServerConfigFile='/etc/elasticsearch/elasticsearch.yml'
 MAX_QUERY_SIZE = 50000  # TODO Check that this is a reasonable number
 
+class ElasticsearchError(Exception):
+    pass
+
 def getDashboardUUID():
     sql = "SELECT value FROM DashboardSettings WHERE name='%s'"
     sql = sql % (MySQLdb.escape_string('dashboard_uuid'))
@@ -705,12 +708,18 @@ def get_transfer_file_info(field, value):
         # where the value is an actual precise match.
         filtered_results = [results for results in documents['hits']['hits']
                             if results['_source'][field] == value]
-        results = filtered_results[0]['_source']
 
         result_count = len(filtered_results)
+        if result_count == 1:
+            results = filtered_results[0]['_source']
         if result_count > 1:
+            results = filtered_results[0]['_source']
             logging.warning('get_transfer_file_info returned %s results for query %s: %s (using first result)',
                             result_count, field, value)
+        elif result_count < 1:
+            logging.error('get_transfer_file_info returned no exact results for query %s: %s',
+                          field, value)
+            raise ElasticsearchError("get_transfer_file_info returned no exact results")
 
     logging.debug('get_transfer_file_info: results: %s', results)
     return results
