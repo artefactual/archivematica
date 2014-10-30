990139056185b5fb7e493755a3caa9462f304459
Misty De Meo <mdemeo@artefactual.com>
2014-10-30 12:16:56 -0700

    dicts: don't try to expand path if sipdir not defined

--- src/archivematicaCommon/lib/dicts.py
+++ src/archivematicaCommon/lib/dicts.py
@@ -105,6 +105,7 @@ class ReplacementDict(dict):
                 sip = file_.transfer
 
         rd = ReplacementDict()
+        sipdir = None
         if sip:
             if isinstance(sip, models.Transfer):
                 relative_location = sip.currentlocation
@@ -136,7 +137,7 @@ class ReplacementDict(dict):
             except:
                 base_location = file_.transfer.currentlocation
 
-            if expand_path:
+            if expand_path and sipdir is not None:
                 base_location = base_location.replace('%sharedPath%', shared_path)
                 # If the original location contains non-unicode characters,
                 # using base_location as retrieved from the DB will raise.
@@ -145,6 +146,7 @@ class ReplacementDict(dict):
                 current_location = current_location.replace('%SIPDirectory%', sipdir)
             else:
                 origin = file_.originallocation
+                current_location = file_.currentlocation
             rd['%originalLocation%'] = origin
             rd['%currentLocation%'] = current_location
             rd['%fileGrpUse%'] = file_.filegrpuse
