e1a362206150ed0a8199cf27b1bd6bb31aa8347c
Misty De Meo <mdemeo@artefactual.com>
2014-11-13 14:30:19 -0800

    Move Transfer/SIP name cleanup log to correct location
    Fixes #7478.

--- src/MCPServer/share/mysql_dev.sh
+++ src/MCPServer/share/mysql_dev.sh
@@ -7,4 +7,5 @@ currentDir="`dirname $0`"
 # Add post 1.0 development to the following files
 echo 'Running mysql_dev1'
 mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_7464_processing_directory.sql;"
+mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_7478_processing_directory.sql;"
 # ...
