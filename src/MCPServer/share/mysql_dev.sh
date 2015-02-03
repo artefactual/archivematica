f293bfb81d6291fc327eeb291b4055e84f143f8e
Justin Simpson <jsimpson@artefactual.com>
2015-02-03 00:38:07 -0800

    Updating database scripts for 1.3.1 release
    Refs #7911
    
    Update mysql scripts in MCPServer package
    include new 1.3.1 update script
    and update existing mysql script.

--- src/MCPServer/share/mysql_dev.sh
+++ src/MCPServer/share/mysql_dev.sh
@@ -4,10 +4,8 @@ databaseName=$1
 dbpassword=$2  # should be "" or "-p<password>", like in localDevSetup/recreateDB.sh
 currentDir="`dirname $0`"
 
-# Add post 1.0 development to the following files
+# Add post 1.3.1 development to the following files
 echo 'Running mysql_dev1'
-mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_7464_processing_directory.sql;"
-mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_7478_processing_directory.sql;"
-mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_7603_metadata_identification.sql;"
-mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_7606_atk_whitespace.sql;"
+#mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_XXXX_your_script.sql;"
+
 # ...
