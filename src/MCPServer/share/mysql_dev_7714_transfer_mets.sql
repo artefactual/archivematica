afdb0bca43ae6b46d3203a1bf63627aedbf0c4b5
Holly Becker <hbecker@artefactual.com>
2015-03-10 17:24:12 -0700

    Extraction: MSCL changes don't skip extraction

--- src/MCPServer/share/mysql_dev_7714_transfer_mets.sql
+++ src/MCPServer/share/mysql_dev_7714_transfer_mets.sql
@@ -10,6 +10,7 @@ INSERT INTO TasksConfigs (pk, taskType, taskTypePKReference, description) VALUES
 INSERT INTO MicroServiceChainLinks (pk, currentTask, defaultNextChainLink, microServiceGroup, defaultExitMessage) VALUES (@processedStructmapMSCL, @processedStructmapTC, '61c316a6-0a50-4f65-8767-1f44b1eeb6dd', 'Update METS.xml document', 'Failed');
 UPDATE MicroServiceChains SET startingLink=@processedStructmapMSCL WHERE startingLink='303a65f6-a16f-4a06-807b-cb3425a30201';
 UPDATE MicroServiceChainLinksExitCodes SET nextMicroServiceChainLink=@processedStructmapMSCL WHERE microServiceChainLink='b944ec7f-7f99-491f-986d-58914c9bb4fa' AND exitCode=1;
-UPDATE MicroServiceChainLinksExitCodes SET nextMicroServiceChainLink=@processedStructmapMSCL WHERE microServiceChainLink='1cb7e228-6e94-4c93-bf70-430af99b9264' AND exitCode=0;
+UPDATE MicroServiceChainLinksExitCodes SET nextMicroServiceChainLink=@processedStructmapMSCL WHERE microServiceChainLink='aaa929e4-5c35-447e-816a-033a66b9b90b';
+UPDATE MicroServiceChainLinks SET defaultNextChainLink=@processedStructmapMSCL WHERE pk='aaa929e4-5c35-447e-816a-033a66b9b90b';
 UPDATE MicroServiceChainLinks SET defaultNextChainLink=@processedStructmapMSCL WHERE pk='1cb7e228-6e94-4c93-bf70-430af99b9264';
 INSERT INTO MicroServiceChainLinksExitCodes (microServiceChainLink, exitCode, nextMicroServiceChainLink) VALUES (@processedStructmapMSCL, 0, @characterizeExtractMSCL);
