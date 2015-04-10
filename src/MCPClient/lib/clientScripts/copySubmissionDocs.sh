147851b9d3a1dcfc975ffbdce57b9f991c399479
Holly Becker <hbecker@artefactual.com>
2015-04-09 17:59:04 -0700

    Continue if submission docs don't exist to be copied out
    refs #8258

--- src/MCPClient/lib/clientScripts/copySubmissionDocs.sh
+++ src/MCPClient/lib/clientScripts/copySubmissionDocs.sh
@@ -7,4 +7,4 @@
 # Copy submission documentation from the AIP back into the SIP
 subdir="$1/submissionDocumentation"
 mkdir -p $subdir
-cp -R "$1/$2/data/objects/submissionDocumentation" $subdir
+cp -R "$1/$2/data/objects/submissionDocumentation" $subdir || true
