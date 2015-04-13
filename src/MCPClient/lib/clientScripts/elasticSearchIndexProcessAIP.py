1f353d76b29a584614038fcaffd1d76f79859c7d
Misty De Meo <mdemeo@artefactual.com>
2015-04-13 13:16:35 -0700

    elasticSearchIndexProcessAIP: correct argument order

--- src/MCPClient/lib/clientScripts/elasticSearchIndexProcessAIP.py
+++ src/MCPClient/lib/clientScripts/elasticSearchIndexProcessAIP.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
             'aipfile',
             uuid,
             pathToAIP,
-            sipName,
+            sipName=sipName,
             identifiers=identifiers
         )
 
