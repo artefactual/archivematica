0a091b0b54847cb8cf2f172a646c0ed53b84f44e
Misty De Meo <mdemeo@artefactual.com>
2015-05-08 16:34:40 -0700

    verifyMD5: pass proper groupType for Django
    Fixes #8415.

--- src/MCPClient/lib/clientScripts/verifyMD5.sh
+++ src/MCPClient/lib/clientScripts/verifyMD5.sh
@@ -42,7 +42,7 @@ ret=0
 MD5FILE="${target}metadata/${checksums}.md5"
 if [ -f "${MD5FILE}" ]; then
     "${checkMD5NoGui}" "${target}objects/" "${MD5FILE}" "${target}logs/`basename "${MD5FILE}"`-Check-`date`" "md5deep" && \
-    "`dirname $0`/createEventsForGroup.py" --groupUUID "${transferUUID}" --groupType "transferUUID" --eventType "fixity" --eventDateTime "$date" --eventOutcome "Pass" --eventDetail "`md5deep -v` md5deep ${target}"   
+    "`dirname $0`/createEventsForGroup.py" --groupUUID "${transferUUID}" --groupType "transfer_id" --eventType "fixity" --eventDateTime "$date" --eventOutcome "Pass" --eventDetail "`md5deep -v` md5deep ${target}"   
     ret+="$?"
 else
     echo "File Does not exist:" "${MD5FILE}"
@@ -51,7 +51,7 @@ fi
 SHA1FILE="${target}metadata/${checksums}.sha1"
 if [ -f "${SHA1FILE}" ]; then
     "${checkMD5NoGui}" "${target}objects/" "${SHA1FILE}" "${target}logs/`basename "${SHA1FILE}"`-Check-`date`" "sha1deep" && \
-    "`dirname $0`/createEventsForGroup.py" --groupUUID "${transferUUID}" --groupType "transferUUID" --eventType "fixity" --eventDateTime "$date" --eventOutcome "Pass" --eventDetail "`sha1deep -v` sha1deep ${target}"  
+    "`dirname $0`/createEventsForGroup.py" --groupUUID "${transferUUID}" --groupType "transfer_id" --eventType "fixity" --eventDateTime "$date" --eventOutcome "Pass" --eventDetail "`sha1deep -v` sha1deep ${target}"  
     ret+="$?"
 else
     echo "File Does not exist:" "${SHA1FILE}"
@@ -60,7 +60,7 @@ fi
 SHA256FILE="${target}metadata/${checksums}.sha256"
 if [ -f "${SHA256FILE}" ]; then
     "${checkMD5NoGui}" "${target}objects/" "${SHA256FILE}" "${target}logs/`basename "${SHA256FILE}"`-Check-`date`" "sha256deep" && \
-    "`dirname $0`/createEventsForGroup.py" --groupUUID "${transferUUID}" --groupType "transferUUID" --eventType "fixity" --eventDateTime "$date" --eventOutcome "Pass" --eventDetail "`sha256deep -v` sha256deep ${target}"  
+    "`dirname $0`/createEventsForGroup.py" --groupUUID "${transferUUID}" --groupType "transfer_id" --eventType "fixity" --eventDateTime "$date" --eventOutcome "Pass" --eventDetail "`sha256deep -v` sha256deep ${target}"  
     ret+="$?"
 else
     echo "File Does not exist:" "${SHA256FILE}"
