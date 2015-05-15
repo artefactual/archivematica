9a2a8648fe00b90b7416d16f2fc06e6b1a2b25b3
Holly Becker <hbecker@artefactual.com>
2015-05-14 21:16:56 -0700

    FPR: Update PRONOM for v82
    updating mysql script with new pronom id's
    
    refs #8252

--- src/MCPServer/share/mysql_dev.sh
+++ src/MCPServer/share/mysql_dev.sh
@@ -6,7 +6,6 @@ currentDir="`dirname $0`"
 
 # Add post 1.4.0 development to the following files
 echo 'Running mysql_dev1'
-#mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_XXXX_my_sql_script.sql;"
 # ...
 # optional delete unused MCSL's
 mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_delete_links.sql;"
