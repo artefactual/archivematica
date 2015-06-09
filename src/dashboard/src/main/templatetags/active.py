51551de8c88da228b8d9fe4a8a688f37d9faa4ba
Misty De Meo <mdemeo@artefactual.com>
2015-06-09 09:53:24 -0700

    templatetags: fix active tag when unhandled exception raised
    If Django calls handle_uncaught_exception, the passed "request"
    object will be an empty string instead of a Request object.

--- src/dashboard/src/main/templatetags/active.py
+++ src/dashboard/src/main/templatetags/active.py
@@ -22,7 +22,9 @@ register = Library()
 
 @register.simple_tag
 def active(request, pattern):
-    if request.path.startswith(pattern) and pattern != '/':
+    if not request:
+        return ''
+    elif request.path.startswith(pattern) and pattern != '/':
         return 'active'
     elif request.path == pattern == '/':
         return 'active'
