8a039573e26e4471228c1d35fa9e0f1d80a317a1
Holly Becker <hbecker@artefactual.com>
2015-06-15 14:40:50 -0700

    Test: in memory DB

---
+++ src/dashboard/src/settings/test.py
@@ -0,0 +1,13 @@
+from common import *
+
+########## IN-MEMORY TEST DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
