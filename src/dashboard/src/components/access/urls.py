91c2a37ade17196fe9e560ee6abe510fb94b7dc9
Misty De Meo <mdemeo@artefactual.com>
2015-09-16 17:32:34 -0700

    /access/: prioritize /levels over record

--- src/dashboard/src/components/access/urls.py
+++ src/dashboard/src/components/access/urls.py
@@ -3,8 +3,8 @@ from components.access import views
 
 urlpatterns = patterns('',
     url(r'(?P<system>archivesspace|atk)/$', views.all_records),
+    url(r'(?P<system>archivesspace|atk)/levels/$', views.get_levels_of_description),
     url(r'(?P<system>archivesspace|atk)/(?P<record_id>[A-Za-z0-9-_]+)/$', views.record),
     url(r'(?P<system>archivesspace|atk)/accession/(?P<accession>.+)/$', views.get_records_by_accession),
     url(r'(?P<system>archivesspace|atk)/(?P<record_id>.+)/children/$', views.record_children),
-    url(r'(?P<system>archivesspace|atk)/levels/$', views.get_levels_of_description),
 )
