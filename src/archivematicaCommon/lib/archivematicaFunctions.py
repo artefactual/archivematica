311868ed1dff0091d7db088981e01ec43d4e6db0
Holly Becker <hbecker@artefactual.com>
2015-11-10 13:29:08 -0800

    ArchivematicaFunctions: Remove unused functions, fix spacing

--- src/archivematicaCommon/lib/archivematicaFunctions.py
+++ src/archivematicaCommon/lib/archivematicaFunctions.py
@@ -22,7 +22,6 @@
 # @author Joseph Perry <joseph@artefactual.com>
 
 import collections
-import lxml.etree as etree
 import os
 import re
 import sys
@@ -83,65 +82,9 @@ def strToUnicode(string):
 def getTagged(root, tag):
     ret = []
     for element in root:
-        #print element.tag
-        #print tag
-        #print element.tag == tag
         if element.tag == tag:
             ret.append(element)
-            #return ret #only return the first encounter
     return ret
-
-
-def appendEventToFile(SIPLogsDirectory, fileUUID, eventXML):
-    xmlFile = SIPLogsDirectory + "fileMeta/" + fileUUID + ".xml"
-    appendEventToFile2(xmlFile, eventXML)
-
-def appendEventToFile2(xmlFile, eventXML):
-    tree = etree.parse( xmlFile )
-    root = tree.getroot()
-
-    events = getTagged(root, "events")[0]
-    events.append(eventXML)
-
-    tree = etree.ElementTree(root)
-    tree.write(xmlFile)
-
-def archivematicaRenameFile(SIPLogsDirectory, fileUUID, newName, eventXML):
-    xmlFile = SIPLogsDirectory + "fileMeta/" + fileUUID + ".xml"
-    newName = newName.decode('utf-8')
-    tree = etree.parse( xmlFile )
-    root = tree.getroot()
-    xmlFileName = getTagged(root, "currentFileName")[0]
-    xmlFileName.text = newName
-
-    events = getTagged(root, "events")[0]
-    events.append(eventXML)
-
-    #print etree.tostring(root, pretty_print=True)
-
-    tree = etree.ElementTree(root)
-    tree.write(xmlFile)
-
-
-def fileNoLongerExists(root, objectsDir):
-    """Returns 0 if not deleted, 1 if deleted, -1 if deleted, but already an event to indicated it has been removed"""
-    events = getTagged(root, "events")[0]
-
-    for event in getTagged(events, "event"):
-        #print >>sys.stderr , "event"
-        etype = getTagged(event, "eventType")
-        if len(etype) and etype[0].text == "fileRemoved":
-            #print >>sys.stderr , "file already removed"
-            return -1
-
-    currentName = getTagged(root, "currentFileName")[0].text
-
-    currentName2 = currentName.replace("objects", objectsDir, 1)
-    if os.path.isfile(currentName2.encode('utf8')):
-        return 0
-    else:
-        print currentName
-        return 1
 
 def escapeForCommand(string):
     ret = string
@@ -165,12 +108,12 @@ def escape(string):
 # Normalize non-DC CONTENTdm metadata element names to match those used
 # in transfer's metadata.csv files.
 def normalizeNonDcElementName(string):
-     # Convert non-alphanumerics to _, remove extra _ from ends of string.
-     normalizedString = re.sub(r"\W+", '_', string)
-     normalizedString = normalizedString.strip('_')
-     # Lower case string.
-     normalizedString = normalizedString.lower()
-     return normalizedString
+    # Convert non-alphanumerics to _, remove extra _ from ends of string.
+    normalizedString = re.sub(r"\W+", '_', string)
+    normalizedString = normalizedString.strip('_')
+    # Lower case string.
+    normalizedString = normalizedString.lower()
+    return normalizedString
 
 def find_metadata_files(sip_path, filename):
     """
