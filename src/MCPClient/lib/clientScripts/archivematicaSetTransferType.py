757785a468e6912575df11fd62dd41daf5153b6a
Jesús García Crespo <jesus@sevein.com>
2015-11-19 14:30:25 -0800

    WIP

--- src/MCPClient/lib/clientScripts/archivematicaSetTransferType.py
+++ src/MCPClient/lib/clientScripts/archivematicaSetTransferType.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python2
 
+"""
+mysql> SELECT pk, arguments FROM StandardTasksConfigs WHERE execute = 'archivematicaSetTransferType_v0.0';
++--------------------------------------+------------------------+
+| pk                                   | arguments              |
++--------------------------------------+------------------------+
+| 353f326a-c599-4e66-8e1c-6262316e3729 | "%SIPUUID%" "TRIM"     |
+| 36ad6300-5a2c-491b-867b-c202541749e8 | "%SIPUUID%" "Standard" |
+| 6c261f8f-17ce-4b58-86c2-ac3bfb0d2850 | "%SIPUUID%" "Dspace"   |
+| 7b455fc5-b201-4233-ba1c-e05be059b279 | "%SIPUUID%" "Maildir"  |
++--------------------------------------+------------------------+
+4 rows in set (0.00 sec)
+"""
+
 # This file is part of Archivematica.
 #
 # Copyright 2010-2013 Artefactual Systems Inc. <http://artefactual.com>
