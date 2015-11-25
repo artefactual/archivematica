769b2612fe90b08815fef170ebe1e2b783502056
Pieter De Praetere <pieter@packed.be>
2015-11-25 12:07:07 +0100

    Fixed a bug where using a different databasename from the default produced errors
    Added a new variable to the dbsettings-file "db" that contains the name of the database. Updated database interface code to support this. This allows users to choose a different database name from the default (MCP).
    
    Changed the value of db to the default of MCP

--- src/archivematicaCommon/lib/databaseInterface.py
+++ src/archivematicaCommon/lib/databaseInterface.py
@@ -32,7 +32,7 @@ printSQL = False
 printErrors = True
 
 #DB_CONNECTION_OPTS = dict(db="MCP", read_default_file="/etc/archivematica/archivematicaCommon/dbsettings")
-DB_CONNECTION_OPTS = dict(db="MCP", read_default_file="/etc/archivematica/archivematicaCommon/dbsettings", charset="utf8", use_unicode = True)
+DB_CONNECTION_OPTS = dict(read_default_file="/etc/archivematica/archivematicaCommon/dbsettings", charset="utf8", use_unicode = True)
 
 def reconnect():
     global database
