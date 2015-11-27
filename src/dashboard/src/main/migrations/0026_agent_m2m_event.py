11a0177217e55e6eed8766e7c3e83f90ad1f2ea7
Holly Becker <becker.holly@gmail.com>
2015-11-26 17:48:50 -0800

    Models: Events and Agents are many-to-many

---
+++ src/dashboard/src/main/migrations/0026_agent_m2m_event.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0025_copyrightstatus_controlled_vocab'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='event',
+            name='linking_agent',
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='agents',
+            field=models.ManyToManyField(to='main.Agent'),
+            preserve_default=True,
+        ),
+    ]
