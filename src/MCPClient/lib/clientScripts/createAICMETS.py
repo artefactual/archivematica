db42ee9be626d8d8d9352d8350825de198a739d1
Jesús García Crespo <jesus@sevein.com>
2015-12-01 15:17:20 -0800

    Fix shebangs

--- src/MCPClient/lib/clientScripts/createAICMETS.py
+++ src/MCPClient/lib/clientScripts/createAICMETS.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python2
 
 import argparse
 from lxml import etree
--- src/dashboard/tests/test_access.py
+++ src/dashboard/tests/test_access.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python2
 
 import base64
 import json
