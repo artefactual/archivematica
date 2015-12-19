7e58751a18aa92427b2d7a35933b405837f252b0
Holly Becker <becker.holly@gmail.com>
2015-12-18 17:02:05 -0800

    Release: Upgrade SQL script

--- src/MCPServer/share/mysql_dev.sh
+++ src/MCPServer/share/mysql_dev.sh
@@ -6,10 +6,8 @@ currentDir="`dirname $0`"
 
 # Add post 1.0 development to the following files
 echo 'Running mysql_dev1'
-mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_7595_archivesspace_upload.sql;"
-mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_6488_aip_reingest2.sql;"
-mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_hierarchical_dip_upload.sql;"
-mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_8609_automate_matching_gui.sql;"
+#mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_XXXX_your_new_update.sql;"
+
 # ...
 # optional delete unused MCSL's
 mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/mysql_dev_delete_links.sql;"
--- src/MCPServer/share/mysql_dev_delete_links.sql
+++ src/MCPServer/share/mysql_dev_delete_links.sql
@@ -4,8 +4,3 @@
 -- then you will not be able to delete those particular ChainLinks
 -- which is fine.
 
--- Don't use weird normalization node, remove unitVars for that
-SET @d1 = '29dece8e-55a4-4f2c-b4c2-365ab6376ceb' COLLATE utf8_unicode_ci;
-SET @d2 = '635ba89d-0ad6-4fc9-acc3-e6069dffdcd5' COLLATE utf8_unicode_ci;
-DELETE FROM MicroServiceChainLinksExitCodes WHERE microServiceChainLink IN (@d1, @d2);
-DELETE FROM MicroServiceChainLinks WHERE pk IN (@d1, @d2);
