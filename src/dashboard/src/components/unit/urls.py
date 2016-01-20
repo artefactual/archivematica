4c66bdac9eddf96fbf787d4d4bbffb086daf9715
Holly Becker <hbecker@artefactual.com>
2016-01-20 10:50:10 -0800

    Dashboard: Merge SIP/Transfer detail & hide endpoints
    Many endpoints are the same between Transfers and SIPs. Create a new 'unit'
    app for the common endpoints. Detail, microservice and hide endpoints moved
    there initially.

---
+++ src/dashboard/src/components/unit/urls.py
@@ -0,0 +1,28 @@
+# This file is part of Archivematica.
+#
+# Copyright 2010-2016 Artefactual Systems Inc. <http://artefactual.com>
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.conf.urls import url, patterns
+from django.conf import settings
+from components.unit import views
+
+# The first segment of these urls is '^(?P<unit_type>transfer|ingest)/'
+# All views should expect a first parameter of unit_type, with a value of 'transfer' or 'ingest'
+urlpatterns = patterns('',
+    url(r'(?P<unit_uuid>' + settings.UUID_REGEX + ')/$', views.detail),
+    url(r'(?P<unit_uuid>' + settings.UUID_REGEX + ')/microservices/$', views.microservices),
+    url(r'(?P<unit_uuid>' + settings.UUID_REGEX + ')/delete/$', views.mark_hidden),
+)
