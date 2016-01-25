96e9b4bce676ef450f621f97411564ed11097af2
Holly Becker <hbecker@artefactual.com>
2016-01-25 14:43:47 -0800

    Bags: Cleanup bag verification
    Remove duplicate verify bag function. Move script-level bag verification into
    function to be reused. Move parameters in bag_with_empty_directories to be
    explicit.

--- src/MCPClient/lib/clientScripts/verifyBAG.py
+++ src/MCPClient/lib/clientScripts/verifyBAG.py
@@ -17,9 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
 
-# @package Archivematica
-# @subpackage archivematicaClientScript
-# @author Joseph Perry <joseph@artefactual.com>
+from __future__ import print_function
+
 import os
 import sys
 # archivematicaCommon
@@ -28,34 +27,41 @@ from executeOrRunSubProcess import executeOrRun
 
 logger = get_script_logger("archivematica.mcp.client.verifyBAG")
 
-printSubProcessOutput=True
+def verify_bag(bag):
+    verificationCommands = [
+        ["/usr/share/bagit/bin/bag", "verifyvalid", bag],  # Validity
+        ["/usr/share/bagit/bin/bag", "verifycomplete", bag],  # Completness
+        ["/usr/share/bagit/bin/bag", "verifypayloadmanifests", bag],  # Checksums in manifests
+    ]
+    bagInfoPath = os.path.join(bag, "bag-info.txt")
+    if os.path.isfile(bagInfoPath):
+        for line in open(bagInfoPath, 'r'):
+            if line.startswith("Payload-Oxum"):
+                # Generate Payload-Oxum and check against Payload-Oxum in bag-info.txt.
+                verificationCommands.append(
+                    ["/usr/share/bagit/bin/bag", "checkpayloadoxum", bag]
+                )
+                break
 
-bag = sys.argv[1]
-verificationCommands = []
-verificationCommands.append("/usr/share/bagit/bin/bag verifyvalid \"%s\"" % (bag)) #Verifies the validity of a bag.
-verificationCommands.append("/usr/share/bagit/bin/bag verifycomplete \"%s\"" % (bag)) #Verifies the completeness of a bag.
-verificationCommands.append("/usr/share/bagit/bin/bag verifypayloadmanifests \"%s\"" % (bag)) #Verifies the checksums in all payload manifests.
-
-bagInfoPath = os.path.join(bag, "bag-info.txt")
-if os.path.isfile(bagInfoPath):
-    for line in open(bagInfoPath,'r'):
-        if line.startswith("Payload-Oxum"):
-            verificationCommands.append("/usr/share/bagit/bin/bag checkpayloadoxum \"%s\"" % (bag)) #Generates Payload-Oxum and checks against Payload-Oxum in bag-info.txt.
+    for item in os.listdir(bag):
+        if item.startswith("tagmanifest-") and item.endswith(".txt"):
+            # Verify the checksums in all tag manifests.
+            verificationCommands.append(
+                ["/usr/share/bagit/bin/bag", "verifytagmanifests", bag]
+            )
             break
 
-for item in os.listdir(bag):
-    if item.startswith("tagmanifest-") and item.endswith(".txt"):        
-        verificationCommands.append("/usr/share/bagit/bin/bag verifytagmanifests \"%s\"" % (bag)) #Verifies the checksums in all tag manifests.
-        break
-        
-exitCode = 0
-for command in verificationCommands:
-    ret = executeOrRun("command", command, printing=printSubProcessOutput)
-    exit, stdOut, stdErr = ret
-    if exit != 0:
-        print >>sys.stderr, "Failed test: ", command
-        exitCode=1
-    else:
-        print >>sys.stderr, "Passed test: ", command
-quit(exitCode)
+    exitCode = 0
+    for command in verificationCommands:
+        ret = executeOrRun("command", command)
+        rc, _, _ = ret
+        if rc != 0:
+            print("Failed test: %s", command, file=sys.stderr)
+            exitCode = 1
+        else:
+            print("Passed test: %s", command)
+    return exitCode
 
+if __name__ == '__main__':
+    bag = sys.argv[1]
+    sys.exit(verify_bag(bag))
