c890c75588fafb3ed59c42a464da8876e1cf538f
Holly Becker <hbecker@artefactual.com>
2016-01-28 10:15:39 -0800

    URLs: Fix Unit regex
    Fix transfer and ingest related URLs to prevent accidental greedy matching.
    All sub-url files required to match at the start of the string.
    
    Notably this fixes (transfer|ingest)/UUID accidentally grabbing URLs intended
    for transfer/component/UUID

--- src/dashboard/src/components/rights/ingest_urls.py
+++ src/dashboard/src/components/rights/ingest_urls.py
@@ -20,9 +20,9 @@ from components.rights import views
 
 urlpatterns = patterns('',
     url(r'^$', views.ingest_rights_list),
-    url(r'add/$', views.ingest_rights_edit),
-    url(r'delete/(?P<id>\d+)/$', views.ingest_rights_delete),
-    url(r'grants/(?P<id>\d+)/delete/$', views.ingest_rights_grant_delete),
-    url(r'grants/(?P<id>\d+)/$', views.ingest_rights_grants_edit),
-    url(r'(?P<id>\d+)/$', views.ingest_rights_edit)
+    url(r'^add/$', views.ingest_rights_edit),
+    url(r'^delete/(?P<id>\d+)/$', views.ingest_rights_delete),
+    url(r'^grants/(?P<id>\d+)/delete/$', views.ingest_rights_grant_delete),
+    url(r'^grants/(?P<id>\d+)/$', views.ingest_rights_grants_edit),
+    url(r'^(?P<id>\d+)/$', views.ingest_rights_edit)
 )
