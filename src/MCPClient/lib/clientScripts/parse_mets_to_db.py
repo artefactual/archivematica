c58d9630c61eeca96245e23c70bca0dd9a257ba2
Holly Becker <hbecker@artefactual.com>
2016-03-15 14:01:01 -0700

    Parse METS uses argparse

--- src/MCPClient/lib/clientScripts/parse_mets_to_db.py
+++ src/MCPClient/lib/clientScripts/parse_mets_to_db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 
 from __future__ import print_function
+import argparse
 import datetime
 from lxml import etree
 import sys
@@ -426,20 +427,12 @@ def parse_rights(sip_uuid, root):
             parsed_rights.append(rights)
     return parsed_rights
 
-def main():
-    # HACK Most of this file is a hack to parse the METS file into the DB.
-    # This should use the in-progress METS Reader/Writer
 
-    sip_uuid = sys.argv[1]
-    sip_path = sys.argv[2]
-
+def main(sip_uuid, sip_path):
     # Set reingest type
     # TODO also support AIC-REIN
     sip = models.SIP.objects.filter(uuid=sip_uuid)
     sip.update(sip_type='AIP-REIN')
-
-    # Stuff to delete
-    # The cascading delete of the SIP on approve reingest deleted most things
 
     # Parse METS to extract information needed by later microservices
     mets_path = os.path.join(sip_path, 'metadata', 'submissionDocumentation', 'METS.' + sip_uuid + '.xml')
@@ -455,4 +448,9 @@ def main():
 
 if __name__ == '__main__':
     print('METS Reader')
-    sys.exit(main())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sip_uuid', help='%SIPUUID%')
+    parser.add_argument('sip_path', help='%SIPDirectory%')
+    args = parser.parse_args()
+
+    sys.exit(main(args.sip_uuid, args.sip_path))
