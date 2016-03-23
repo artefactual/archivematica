682b99d519efc9b4df97548624e701bca54c0ddb
Jesús García Crespo <jesus@sevein.com>
2016-03-23 16:30:36 -0700

    Dashboard: set up ES client in wsgi.py

--- src/dashboard/src/wsgi.py
+++ src/dashboard/src/wsgi.py
@@ -2,5 +2,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import elasticSearchFunctions
+
 
 application = get_wsgi_application()
+
+# Set up Elasticsearch client
+elasticSearchFunctions.setup_reading_from_client_conf()
