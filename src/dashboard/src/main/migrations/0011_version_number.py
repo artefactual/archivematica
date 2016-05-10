3e7aba30128fd90e60ef03209b1edb48e3af69c9
Holly Becker <hbecker@artefactual.com>
2016-05-10 15:48:32 -0700

    Update Archivematica version

---
+++ src/dashboard/src/main/migrations/0011_version_number.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def data_migration(apps, schema_editor):
+    Agent = apps.get_model('main', 'Agent')
+    Agent.objects.filter(identifiertype='preservation system', name='Archivematica').update(identifiervalue='Archivematica-1.5.0')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0010_dip_upload_store'),
+    ]
+
+    operations = [
+        migrations.RunPython(data_migration)
+    ]
