b73bff14a92900ddf070c6560e001821ae12bf86
Justin Simpson <jsimpson@artefactual.com>
2016-06-03 13:24:28 -0700

    update MicroServiceChoiceReplacementDic for Siegfried 1.5.0
    refs #9830
    
    When a new IDCommand is created, the MicroServiceChoiceReplacementDic
    entries that linked to the old IDCommand need to be updated.

---
+++ src/dashboard/src/main/migrations/0017_update_seigfried.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+def data_migration(apps, schema_editor):
+    mscrd = apps.get_model('main', 'MicroServiceChoiceReplacementDic')
+    
+    #make the Seigfried replacementDics point at a new IDCommand for sf 1.5.0
+    #see archivematica-fpr-admin branch 1.1.x
+    
+    mscrd.objects.filter(id='bed4eeb1-d654-4d97-b98d-40eb51d3d4bb').update(replacementDic='{"%IDCommand%":"9d2cefc1-2bd2-44e4-8d55-6cf8151eecff"}')
+    
+    mscrd.objects.filter(id='664cbde3-e658-4288-87db-bd28266d83f5').update(replacementDic='{"%IDCommand%":"9d2cefc1-2bd2-44e4-8d55-6cf8151eecff"}')
+    
+    mscrd.objects.filter(id='25a91595-37f0-4373-a89a-56a757353fb8').update(replacementDic='{"%IDCommand%":"9d2cefc1-2bd2-44e4-8d55-6cf8151eecff"}')
+    
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0016_file_currentlocation_nullable'),
+    ]
+
+    operations = [
+        migrations.RunPython(data_migration),
+    ]
+    
