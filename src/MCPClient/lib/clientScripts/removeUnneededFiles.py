20c028d7b0ba97dc48efefd9e9976c6039a8c3f8
Mike Winter <mike.winter@usask.ca>
2016-08-19 10:43:31 -0600

    Modify removeUnneededFiles and removeCacheFiles to only run once per submission rather than once per file.

--- src/MCPClient/lib/clientScripts/removeUnneededFiles.py
+++ src/MCPClient/lib/clientScripts/removeUnneededFiles.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import ntpath
 
 # databaseFunctions requires Django to be set up
 import django
@@ -9,23 +10,33 @@ django.setup()
 # archivematicaCommon
 from custom_handlers import get_script_logger
 from databaseFunctions import fileWasRemoved
+from main.models import File
 
 REMOVEABLE_FILES = ["Thumbs.db", "Icon", u"Icon\u000D"]
 
-def remove_file(target_file, file_uuid):
-    basename = os.path.basename(target_file)
-    if basename in REMOVEABLE_FILES:
-        print "Removing {filename} (UUID: {uuid})".format(uuid=file_uuid, filename=basename)
-        os.remove(target_file)
-        # Gearman passes parameters as strings, so None (NoneType) becomes
-        # "None" (string)
-        if file_uuid and file_uuid != "None":
-            fileWasRemoved(file_uuid)
+def remove_file(sip_obj_dir, sip_UUID):
+
+    ### 1. Remove unneeded files from package.
+    for dir_path, dir_name, filenames in os.walk(sip_obj_dir):
+        for remove_name in REMOVEABLE_FILES:
+            for filename in filenames:
+                if remove_name == filename:
+                    del_file = dir_path + '/' + filename
+                    os.remove(del_file)
+                    logger.debug('removeUnneededFiles file deleted: %s', del_file)
+
+    ### 2. Insert removal notification into events database
+    sip_files = File.objects.filter(sip=sip_UUID).values_list('uuid', 'currentlocation')
+    remove_files = [file_UUID for file_UUID, current_location in sip_files if (ntpath.basename(current_location) in REMOVEABLE_FILES and file_UUID != 'None')]
+    logger.debug('removeUnneededFiles db results :  %s', remove_files)
+    for file_UUID in remove_files:
+        fileWasRemoved(file_UUID)
 
 if __name__ == '__main__':
     logger = get_script_logger("archivematica.mcp.client.removeUnneededFiles")
 
-    target = sys.argv[1]
-    file_uuid = sys.argv[2]
+    sip_obj_dir = sys.argv[1]
+    sip_UUID = sys.argv[2]
 
-    sys.exit(remove_file(target, file_uuid))
+    sys.exit(remove_file(sip_obj_dir, sip_UUID))
+
