dbe79f3f81196589d48e2f1598464c70a2e8d7e4
Joel Dunham <jrwdunham@gmail.com>
2016-09-13 14:17:47 -0700

    API can now approve AIP objects re-ingest.

--- src/dashboard/src/components/api/urls.py
+++ src/dashboard/src/components/api/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     url(r'ingest/waiting', views.waiting_for_user_input),
     url(r'^(?P<unit_type>transfer|ingest)/(?P<unit_uuid>' + settings.UUID_REGEX + ')/delete/', views.mark_hidden),
 
+    url(r'^ingest/reingest/approve', views.reingest_approve),
     url(r'^ingest/reingest', views.start_reingest),
 
     url(r'administration/dips/atom/levels/$', views.get_levels_of_description),
