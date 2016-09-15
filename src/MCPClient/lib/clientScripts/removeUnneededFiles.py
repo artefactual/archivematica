e90bc8c09b34be10a08163591147de468ed315e8
Mike Winter <mike.winter@usask.ca>
2016-09-15 16:19:44 -0600

    * Add migration for removeUnneededFile change. * Use correct platform-independent os. functions.

--- src/MCPClient/lib/clientScripts/removeUnneededFiles.py
+++ src/MCPClient/lib/clientScripts/removeUnneededFiles.py
@@ -2,7 +2,6 @@
 
 import os
 import sys
-import ntpath
 
 # databaseFunctions requires Django to be set up
 import django
@@ -18,16 +17,15 @@ def remove_file(sip_obj_dir, sip_UUID):
 
     ### 1. Remove unneeded files from package.
     for dir_path, dir_name, filenames in os.walk(sip_obj_dir):
-        for remove_name in REMOVEABLE_FILES:
-            for filename in filenames:
-                if remove_name == filename:
-                    del_file = dir_path + '/' + filename
-                    os.remove(del_file)
-                    logger.debug('removeUnneededFiles file deleted: %s', del_file)
+        for filename in filenames:
+            if filename in REMOVEABLE_FILES:
+                del_file = os.path.join(dir_path, filename)
+                os.remove(del_file)
+                logger.debug('removeUnneededFiles file deleted: %s', del_file)
 
     ### 2. Insert removal notification into events database
     sip_files = File.objects.filter(sip=sip_UUID).values_list('uuid', 'currentlocation')
-    remove_files = [file_UUID for file_UUID, current_location in sip_files if (ntpath.basename(current_location) in REMOVEABLE_FILES and file_UUID != 'None')]
+    remove_files = [file_UUID for file_UUID, current_location in sip_files if (os.path.basename(current_location) in REMOVEABLE_FILES and file_UUID != 'None')]
     logger.debug('removeUnneededFiles db results :  %s', remove_files)
     for file_UUID in remove_files:
         fileWasRemoved(file_UUID)
