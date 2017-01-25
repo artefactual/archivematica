artefactual/archivematica

commit 07ca4925560afeb09753b3cc9065c8f7521da0b9

Re-ingest: update transfer clientScripts
This commmit updates the client scripts being executed in the standardTransfer
chain in order to accept Archivematica AIPs. The METS file is used when
necessary to avoid extra work, e.g. archivematicaUpdateSizeAndChecksum.py will
use the checksum found in the METS instead of computing it again.

src/MCPClient/lib/clientScripts/restructureForCompliance.py
@@ -22,34 +22,112 @@
 
 from __future__ import print_function
 import os
+import re
 import sys
 import shutil
+
+import django
+django.setup()
+from main.models import Transfer
+
+from verifyBAG import verify_bag
 
 # archivematicaCommon
 import archivematicaFunctions
 from archivematicaFunctions import REQUIRED_DIRECTORIES, OPTIONAL_FILES
 from custom_handlers import get_script_logger
 
+logger = get_script_logger('archivematica.mcp.client.restructureForCompliance')
 
-def restructureDirectory(unitPath):
-    unitPath = str(unitPath)
+
+def _move_file(src, dst, exit_on_error=True):
+    logger.info('Moving %s to %s', src, dst)
+    try:
+        shutil.move(src, dst)
+    except IOError:
+        print('Could not move', src)
+        if exit_on_error:
+            raise
+
+
+def restructure_transfer(unit_path):
     # Create required directories
-    archivematicaFunctions.create_directories(
-        REQUIRED_DIRECTORIES, unitPath, printing=True)
+    archivematicaFunctions.create_structured_directory(unit_path, printing=True)
+
     # Move everything else to the objects directory
-    for item in os.listdir(unitPath):
-        dst = os.path.join(unitPath, "objects", '.')
-        itemPath =  os.path.join(unitPath, item)
-        if os.path.isdir(itemPath) and item not in REQUIRED_DIRECTORIES:
-            shutil.move(itemPath, dst)
-            print("moving directory to objects: ", item)
-        elif os.path.isfile(itemPath) and item not in OPTIONAL_FILES:
-            shutil.move(itemPath, dst)
-            print("moving file to objects: ", item)
+    for item in os.listdir(unit_path):
+        src = os.path.join(unit_path, item)
+        dst = os.path.join(unit_path, "objects", '.')
+        if os.path.isdir(src) and item not in REQUIRED_DIRECTORIES:
+            _move_file(src, dst)
+        elif os.path.isfile(src) and item not in OPTIONAL_FILES:
+            _move_file(src, dst)
+
+
+def restructure_transfer_aip(unit_path):
+    """
+    Restructure a transfer that comes from re-ingesting an Archivematica AIP.
+    """
+    old_bag = os.path.join(unit_path, 'old_bag', '')
+    os.makedirs(old_bag)
+
+    # Move everything to old_bag
+    for item in os.listdir(unit_path):
+        if item == 'old_bag':
+            continue
+        src = os.path.join(unit_path, item)
+        _move_file(src, old_bag)
+
+    # Create required directories
+    # - "/logs" and "/logs/fileMeta"
+    # - "/metadata" and "/metadata/submissionDocumentation"
+    # - "/objects"
+    archivematicaFunctions.create_structured_directory(unit_path, printing=True)
+
+    # Move /old_bag/data/METS.<UUID>.xml => /metadata/METS.<UUID>.xml
+    p = re.compile(r'^METS\..*\.xml$', re.IGNORECASE)
+    src = os.path.join(old_bag, 'data')
+    for item in os.listdir(src):
+        m = p.match(item)
+        if m:
+            break # Stop trying after the first match
+    src = os.path.join(src, m.group())
+    dst = os.path.join(unit_path, 'metadata')
+    _move_file(src, dst)
+
+    # Move /old_bag/data/objects/submissionDocumentation/* => /metadata/submissionDocumentation/
+    src = os.path.join(old_bag, 'data', 'objects', 'submissionDocumentation')
+    dst = os.path.join(unit_path, 'metadata', 'submissionDocumentation')
+    for item in os.listdir(src):
+        item_path = os.path.join(src, item)
+        _move_file(item_path, dst)
+    shutil.rmtree(src)
+
+    # Move /old_bag/data/objects/* => /objects/
+    src = os.path.join(old_bag, 'data', 'objects')
+    dst = os.path.join(unit_path, 'objects')
+    for item in os.listdir(src):
+        item_path = os.path.join(src, item)
+        _move_file(item_path, dst)
+
+    # Get rid of old_bag
+    shutil.rmtree(old_bag)
 
 if __name__ == '__main__':
-    logger = get_script_logger("archivematica.mcp.client.restructureForCompliance")
+    sip_path = sys.argv[1]
+    sip_uuid = sys.argv[2]
 
-    target = sys.argv[1]
-    restructureDirectory(target)
-    
+    transfer = Transfer.objects.get(uuid=sip_uuid)
+    logger.info('Transfer.type=%s', transfer.type)
+
+    if transfer.type == 'Archivematica AIP':
+        logger.info('Archivematica AIP detected, verifying bag...')
+        exit_code = verify_bag(sip_path)
+        if exit_code > 0:
+            logger.info('Archivematica AIP: bag verification failed!')
+            sys.exit(exit_code)
+        logger.info('Restructuring transfer (Archivematica AIP re-ingest)...')
+        restructure_transfer_aip(sip_path)
+    else:
+        logger.info('Restructuring transfer...')
+        restructure_transfer(sip_path)

src/MCPClient/lib/clientScripts/archivematicaAssignFileUUID.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 
 # This file is part of Archivematica.
 #
@@ -17,63 +18,110 @@
 # You should have received a copy of the GNU General Public License
 # along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
 
-# @package Archivematica
-# @subpackage archivematicaClientScript
-# @author Joseph Perry <joseph@artefactual.com>
 from __future__ import print_function
+
+import argparse
+import os
+import re
 import sys
 import uuid
-from optparse import OptionParser
 
 import django
 django.setup()
 # dashboard
-from main.models import File
+from main.models import File, Transfer
 # archivematicaCommon
 from custom_handlers import get_script_logger
 from fileOperations import addFileToTransfer
 from fileOperations import addFileToSIP
 
+import metsrw
+
+
+logger = get_script_logger('archivematica.mcp.client.assignFileUUID')
+
+
+def find_mets_file(unit_path):
+    """
+    Return the location of the original METS in a Archivematica AIP transfer.
+    """
+    p = re.compile(r'^METS\..*\.xml$', re.IGNORECASE)
+    src = os.path.join(unit_path, 'metadata')
+    for item in os.listdir(src):
+        m = p.match(item)
+        if m:
+            return os.path.join(src, m.group())
+
+
+def get_file_uuid_from_mets(sip_directory, file_path_relative_to_sip):
+    """
+    Look up the UUID of the file in the METS document using metsrw.
+    """
+    mets_file = find_mets_file(sip_directory)
+    if not mets_file:
+        logger.info('Archivematica AIP: METS file not found.')
+        return
+    logger.info('Archivematica AIP: reading METS file %s.', mets_file)
+    mets = metsrw.METSDocument.fromfile(mets_file)
+
+    file_path_relative_to_sip = file_path_relative_to_sip.replace('%transferDirectory%', '', 1)
+    file_path_relative_to_sip = file_path_relative_to_sip.replace('%SIPDirectory%', '', 1)
+
+    # Warning! This is not the fastest way to achieve this. But we will focus
+    # on optimizations later.
+    # TODO: is it ok to assume that the file structure is flat?
+    entry = mets.get_file(path=file_path_relative_to_sip)
+    if entry:
+        logger.info('Archivematica AIP: file UUID of has been found in the METS document (%s).', entry.path)
+        return entry.file_uuid
+    logger.info('Archivematica AIP: file UUID has not been found in the METS document: %s', file_path_relative_to_sip)
+
+
+def main(file_uuid=None, file_path='', date='', event_uuid=None, sip_directory='', sip_uuid=None, transfer_uuid=None, use='original', update_use=True):
+    if file_uuid == "None":
+        file_uuid = None
+    if file_uuid:
+        logger.error('File already has UUID: %s', file_uuid)
+        if update_use:
+            File.objects.filter(uuid=file_uuid).update(filegrpuse=use)
+        return 0
+
+    # Stop if both or neither of them are used
+    if all([sip_uuid, transfer_uuid]) or not any([sip_uuid, transfer_uuid]):
+        logger.error('SIP exclusive-or Transfer UUID must be defined')
+        return 2
+
+    # Transfer
+    if transfer_uuid:
+        file_path_relative_to_sip = file_path.replace(sip_directory, '%transferDirectory%', 1)
+        transfer = Transfer.objects.get(uuid=transfer_uuid)
+        if transfer.type == 'Archivematica AIP':
+            file_uuid = get_file_uuid_from_mets(sip_directory, file_path_relative_to_sip)
+        if not file_uuid:
+            file_uuid = str(uuid.uuid4())
+            logger.info('Generated UUID for this file: %s.', file_uuid)
+        addFileToTransfer(file_path_relative_to_sip, file_uuid, transfer_uuid, event_uuid, date, use=use)
+        return 0
+
+    # Ingest
+    if sip_uuid:
+        file_uuid = str(uuid.uuid4())
+        file_path_relative_to_sip = file_path.replace(sip_directory,"%SIPDirectory%", 1)
+        addFileToSIP(file_path_relative_to_sip, file_uuid, sip_uuid, event_uuid, date, use=use)
+        return 0
 
 
 if __name__ == '__main__':
-    logger = get_script_logger("archivematica.mcp.client.assignFileUUID")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--fileUUID', type=str, dest='file_uuid')
+    parser.add_argument('-p', '--filePath', action='store', dest='file_path', default='')
+    parser.add_argument('-d', '--date', action='store', dest='date', default='')
+    parser.add_argument('-u', '--eventIdentifierUUID', type=lambda x: str(uuid.UUID(x)), dest='event_uuid')
+    parser.add_argument('-s', '--sipDirectory', action='store', dest='sip_directory', default='')
+    parser.add_argument('-S', '--sipUUID', type=lambda x: str(uuid.UUID(x)), dest='sip_uuid')
+    parser.add_argument('-T', '--transferUUID', type=lambda x: str(uuid.UUID(x)), dest='transfer_uuid')
+    parser.add_argument('-e', '--use', action='store', dest="use", default='original')
+    parser.add_argument('--disable-update-filegrpuse', action='store_false', dest='update_use', default=True)
+    args = parser.parse_args()
 
-    parser = OptionParser()
-    parser.add_option("-i",  "--fileUUID",          action="store", dest="fileUUID", default="")
-    parser.add_option("-p",  "--filePath",          action="store", dest="filePath", default="")
-    parser.add_option("-d",  "--date",              action="store", dest="date", default="")
-    parser.add_option("-u",  "--eventIdentifierUUID", action="store", dest="eventIdentifierUUID", default="")
-    parser.add_option("-s",  "--sipDirectory", action="store", dest="sipDirectory", default="")
-    parser.add_option("-S",  "--sipUUID", action="store", dest="sipUUID", default="")
-    parser.add_option("-T",  "--transferUUID", action="store", dest="transferUUID", default="")
-    parser.add_option("-e",  "--use", action="store", dest="use", default="original")
-    parser.add_option("--disable-update-filegrpuse", action="store_false", dest="update_use", default=True)
-
-
-    (opts, args) = parser.parse_args()
-    opts2 = vars(opts)
-#    for key, value in opts2.iteritems():
-#        print type(key), key, type(value), value
-#        exec 'opts.' + key + ' = value.decode("utf-8")'
-    fileUUID = opts.fileUUID
-    if not fileUUID or fileUUID == "None":
-        fileUUID = uuid.uuid4().__str__()
-    else:
-        print("File already has UUID:", fileUUID, file=sys.stderr)
-        if opts.update_use:
-            File.objects.filter(uuid=fileUUID).update(filegrpuse=opts.use)
-        exit(0) 
-
-
-    if opts.sipUUID == "" and opts.transferUUID != "":
-        filePathRelativeToSIP = opts.filePath.replace(opts.sipDirectory,"%transferDirectory%", 1)
-        addFileToTransfer(filePathRelativeToSIP, fileUUID, opts.transferUUID, opts.eventIdentifierUUID, opts.date, use=opts.use)
-
-    elif opts.sipUUID != "" and opts.transferUUID == "":
-        filePathRelativeToSIP = opts.filePath.replace(opts.sipDirectory,"%SIPDirectory%", 1)
-        addFileToSIP(filePathRelativeToSIP, fileUUID, opts.sipUUID, opts.eventIdentifierUUID, opts.date, use=opts.use)
-
-    else:
-        print("SIP exclusive-or Transfer uuid must be defined", file=sys.stderr)
-        exit(2)
+    sys.exit(main(**vars(args)))

src/MCPClient/lib/clientScripts/archivematicaSetTransferType.py
@@ -36,4 +36,4 @@ if __name__ == '__main__':
     transferUUID = sys.argv[1]
     transferType = sys.argv[2]
 
-    Transfer.objects.filter(uuid=transferUUID).update(type=transferType)
+    Transfer.objects.filter(uuid=transferUUID, type__isnull=False).update(type=transferType)

src/MCPClient/lib/clientScripts/archivematicaUpdateSizeAndChecksum.py
@@ -17,26 +17,127 @@
 # You should have received a copy of the GNU General Public License
 # along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
 
-# @package Archivematica
-# @subpackage archivematicaClientScript
-# @author Joseph Perry <joseph@artefactual.com>
-from optparse import OptionParser
+import argparse
+import os
+import re
+import sys
+import uuid
 
 # fileOperations requires Django to be set up
 import django
 django.setup()
-# archivematicaCommon
+
+from main.models import File, Transfer
+
 from custom_handlers import get_script_logger
 from fileOperations import updateSizeAndChecksum
 
+import metsrw
+
+
+logger = get_script_logger('archivematica.mcp.client.updateSizeAndChecksum')
+
+
+def find_mets_file(unit_path):
+    """
+    Return the location of the original METS in a Archivematica AIP transfer.
+    """
+    p = re.compile(r'^METS\..*\.xml$', re.IGNORECASE)
+    src = os.path.join(unit_path, 'metadata')
+    for item in os.listdir(src):
+        m = p.match(item)
+        if m:
+            return os.path.join(src, m.group())
+
+
+def get_size_and_checksum_from_mets(shared_path, file_):
+    """
+    Given an instance of a File, return a tuple with three values: file_Size,
+    checksum and checksum_type, as they are described in the original METS
+    document of the transfer. The values will be None when not found.
+    """
+    transfer = file_.transfer
+    transfer_location = transfer.currentlocation.replace('%sharedPath%', shared_path, 1)
+
+    mets_file = find_mets_file(transfer_location)
+    if not mets_file:
+        logger.info('Archivematica AIP: METS file not found in %s.', transfer_location)
+        return
+
+    logger.info('Archivematica AIP: reading METS file %s.', mets_file)
+    mets = metsrw.METSDocument.fromfile(mets_file)
+
+    fsentry = mets.get_file(file_uuid=file_.uuid)
+    if not fsentry:
+        logger.error('Archivematica AIP: FSEntry with UUID %s not found', file_.uuid)
+        return None
+    if not len(fsentry.amdsecs):
+        logger.error('Archivematica AIP: FSEntry.amdsecs is empty')
+        return None
+    amdsec = fsentry.amdsecs[0]
+    for item in amdsec.subsections:
+        if item.subsection == 'techMD':
+            techmd = item
+    if not techmd:
+        logger.error('Archivematica AIP: techMD section could not be found')
+        return None
+    if techmd.contents.mdtype != 'PREMIS:OBJECT':
+        logger.error('Archivematica AIP: PREMIS:OBJECT could not be found')
+        return None
+    pobject = techmd.contents.document # Element
+
+    size = checksum = checksum_type = None
+    r = pobject.xpath('premis:objectCharacteristics/premis:objectCharacteristicsExtension/Mediainfo/File/track[@type="General"]/File_size', namespaces=metsrw.utils.NAMESPACES)
+    if r:
+        size = r[0].text
+    r = pobject.xpath('premis:objectCharacteristics/premis:fixity/premis:messageDigest', namespaces=metsrw.utils.NAMESPACES)
+    if r:
+        checksum = r[0].text
+    r = pobject.xpath('premis:objectCharacteristics/premis:fixity/premis:messageDigestAlgorithm', namespaces=metsrw.utils.NAMESPACES)
+    if r:
+        checksum_type = r[0].text
+
+    ret = (size, checksum, checksum_type)
+    logger.info('Archivematica AIP: size=%s, checksum=%s, checksum_type=%s', *ret)
+
+    return ret
+
+
+def main(shared_path, file_uuid, file_path, date, event_uuid):
+    try:
+        file_ = File.objects.get(uuid=file_uuid)
+    except File.DoesNotExist:
+        logger.exception('File with UUID %s cannot be found.', file_uuid)
+        return 1
+
+    # See if it's a Transfer and in particular a Archivematica AIP transfer.
+    # If so, try to extract the size, checksum and checksum function from the
+    # original METS document.
+    kw = {}
+    if file_.transfer and not file_.sip:
+        if file_.transfer.type == 'Archivematica AIP':
+            file_size, checksum, checksum_type = get_size_and_checksum_from_mets(shared_path, file_)
+            kw.update(fileSize=file_size, checksum=checksum, checksumType=checksum_type, add_event=False)
+
+    updateSizeAndChecksum(file_uuid, file_path, date, event_uuid, **kw)
+
+    return 0
+
+
 if __name__ == '__main__':
-    logger = get_script_logger("archivematica.mcp.client.updateSizeAndChecksum")
+    logger.info('Invoked as %s.', ' '.join(sys.argv))
 
-    parser = OptionParser()
-    parser.add_option("-i", "--fileUUID", action="store", dest="fileUUID", default="")
-    parser.add_option("-p", "--filePath", action="store", dest="filePath", default="")
-    parser.add_option("-d", "--date", action="store", dest="date", default="")
-    parser.add_option("-u", "--eventIdentifierUUID", action="store", dest="eventIdentifierUUID", default="")
-    (opts, args) = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sharedPath')
+    parser.add_argument('-i', '--fileUUID', type=lambda x: str(uuid.UUID(x)), dest='file_uuid')
+    parser.add_argument('-p', '--filePath', action='store', dest='file_path', default='')
+    parser.add_argument('-d', '--date', action='store', dest='date', default='')
+    parser.add_argument('-u', '--eventIdentifierUUID', type=lambda x: str(uuid.UUID(x)), dest='event_uuid')
+    args = parser.parse_args()
 
-    updateSizeAndChecksum(opts.fileUUID, opts.filePath, opts.date, opts.eventIdentifierUUID)
+    sys.exit(main(
+        args.sharedPath,
+        args.file_uuid,
+        args.file_path,
+        args.date,
+        args.event_uuid))

src/MCPClient/lib/clientScripts/verifyBAG.py
@@ -21,24 +21,30 @@ from __future__ import print_function
 
 import os
 import sys
+
 # archivematicaCommon
 from custom_handlers import get_script_logger
 from executeOrRunSubProcess import executeOrRun
 
-logger = get_script_logger("archivematica.mcp.client.verifyBAG")
+logger = get_script_logger('archivematica.mcp.client.verifyBAG')
+
+PRINT_SUBPROCESS_OUTPUT = True
+BAG = '/usr/share/bagit/bin/bag'
+BAG_INFO = 'bag-info.txt'
+
 
 def verify_bag(bag):
-    verificationCommands = [
+    verification_commands = [
         ["/usr/share/bagit/bin/bag", "verifyvalid", bag],  # Validity
         ["/usr/share/bagit/bin/bag", "verifycomplete", bag],  # Completness
         ["/usr/share/bagit/bin/bag", "verifypayloadmanifests", bag],  # Checksums in manifests
     ]
-    bagInfoPath = os.path.join(bag, "bag-info.txt")
-    if os.path.isfile(bagInfoPath):
-        for line in open(bagInfoPath, 'r'):
+    bag_info = os.path.join(bag, "bag-info.txt")
+    if os.path.isfile(bag_info):
+        for line in open(bag_info, 'r'):
             if line.startswith("Payload-Oxum"):
                 # Generate Payload-Oxum and check against Payload-Oxum in bag-info.txt.
-                verificationCommands.append(
+                verification_commands.append(
                     ["/usr/share/bagit/bin/bag", "checkpayloadoxum", bag]
                 )
                 break
@@ -46,21 +52,21 @@ def verify_bag(bag):
     for item in os.listdir(bag):
         if item.startswith("tagmanifest-") and item.endswith(".txt"):
             # Verify the checksums in all tag manifests.
-            verificationCommands.append(
+            verification_commands.append(
                 ["/usr/share/bagit/bin/bag", "verifytagmanifests", bag]
             )
             break
 
-    exitCode = 0
-    for command in verificationCommands:
+    exit_code = 0
+    for command in verification_commands:
         ret = executeOrRun("command", command)
         rc, _, _ = ret
         if rc != 0:
             print("Failed test: %s", command, file=sys.stderr)
-            exitCode = 1
+            exit_code = 1
         else:
             print("Passed test: %s", command)
-    return exitCode
+    return exit_code
 
 if __name__ == '__main__':
     bag = sys.argv[1]

src/archivematicaCommon/lib/fileOperations.py
@@ -36,18 +36,29 @@ from archivematicaFunctions import unicodeToStr, get_setting, get_file_checksum
 sys.path.append("/usr/share/archivematica/dashboard")
 from main.models import File, Transfer
 
-def updateSizeAndChecksum(fileUUID, filePath, date, eventIdentifierUUID):
-    fileSize = os.path.getsize(filePath)
-    checksumType = get_setting('checksum_type', 'sha256')
-    checksum = get_file_checksum(filePath, checksumType)
+def updateSizeAndChecksum(fileUUID, filePath, date, eventIdentifierUUID, fileSize=None, checksum=None, checksumType=None, add_event=True):
+    """
+    Update a File with its size, checksum and checksum type. These are
+    parameters that can be either generated or provided via keywords.
+
+    Finally, insert the corresponding Event. This behavior can be cancelled
+    using the boolean keyword 'add_event'.
+    """
+    if not fileSize:
+        fileSize = os.path.getsize(filePath)
+    if not checksumType:
+        checksumType = get_setting('checksum_type', 'sha256')
+    if not checksum:
+        checksum = get_file_checksum(filePath, checksumType)
 
     File.objects.filter(uuid=fileUUID).update(size=fileSize, checksum=checksum, checksumtype=checksumType)
 
-    insertIntoEvents(fileUUID=fileUUID,
-                     eventType='message digest calculation',
-                     eventDateTime=date,
-                     eventDetail='program="python"; module="hashlib.{}()"'.format(checksumType),
-                     eventOutcomeDetailNote=checksum)
+    if add_event:
+        insertIntoEvents(fileUUID=fileUUID,
+                         eventType='message digest calculation',
+                         eventDateTime=date,
+                         eventDetail='program="python"; module="hashlib.{}()"'.format(checksumType),
+                         eventOutcomeDetailNote=checksum)
 
 
 def addFileToTransfer(filePathRelativeToSIP, fileUUID, transferUUID, taskUUID, date, sourceType="ingestion", eventDetail="", use="original"):

commit 56dede7d7918ade1ebb8f0c12a6ed3e469f5ca3d

Fixes #10785: AIC creation failing at restrForComp
This commit fixes a regression introduced by
07ca4925560afeb09753b3cc9065c8f7521da0b9. That commit assumed that every
UUID passed into restructureForCompliance.py would match a Transfer
model. However, when an AIC is created, the UUID corresponds to a SIP
model. With the present commit, AIC creation should work as it did in
stable/1.5. However, future work will need to refactor AIC creation since it
results in a (non-fatal) failure at "Job: Removed bagged files" (and it
creates directories (e.g., objects/ logs/) that are not needed for an
AIC.

src/MCPClient/lib/clientScripts/restructureForCompliance.py
@@ -28,7 +28,7 @@ import shutil
 
 import django
 django.setup()
-from main.models import Transfer
+from main.models import Transfer, SIP
 
 from verifyBAG import verify_bag
 
@@ -132,10 +132,19 @@ if __name__ == '__main__':
     sip_path = sys.argv[1]
     sip_uuid = sys.argv[2]
 
-    transfer = Transfer.objects.get(uuid=sip_uuid)
-    logger.info('Transfer.type=%s', transfer.type)
+    transfer = None
+    sip = None
+    try:
+        transfer = Transfer.objects.get(uuid=sip_uuid)
+    except Transfer.DoesNotExist:
+        sip = SIP.objects.get(uuid=sip_uuid)
 
-    if transfer.type == 'Archivematica AIP':
+    if transfer:
+        logger.info('Transfer.type=%s', transfer.type)
+    else:
+        logger.info('SIP.sip_type=%s', sip.sip_type)
+
+    if transfer and transfer.type == 'Archivematica AIP':
         logger.info('Archivematica AIP detected, verifying bag...')
         exit_code = verify_bag(sip_path)
         if exit_code > 0: