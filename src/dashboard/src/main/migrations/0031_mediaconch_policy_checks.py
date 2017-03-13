44a5c2776660c15e487986ce1ea7aeb166cbc990
Joel Dunham <jrwdunham@gmail.com>
2017-03-13 14:14:57 -0700

    MediaConch-related migrations fixes
    - removed FPR-specific migrations (moved to archivematica-fpr-admin
      repo)
    - split migrations into those for validation (i.e., implementation
      checks) and those for policy checks

--- src/dashboard/src/main/migrations/0031_mediaconch_policy_checks.py
+++ src/dashboard/src/main/migrations/0031_mediaconch_policy_checks.py
@@ -1,81 +1,63 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals
-import os
 
 from django.db import migrations
 
 
-HERE = os.path.dirname(os.path.abspath(__file__))
-MIGR_DATA = os.path.join(os.path.dirname(HERE), 'migrations-data')
-VALIDATE_CMD_FNM = os.path.join(MIGR_DATA, 'mc_validate_cmd.py')
-POLICY_CHECK_CMD_FNM = os.path.join(MIGR_DATA, 'mc_policy_check_cmd.py')
-
-
 def data_migration(apps, schema_editor):
-    """Migrations for MediaConch integration.
+    """Workflow migrations for policy checks on files using MediaConch.
 
-    Changes workflow so that, at a high-level the following things happen:
+    Changes workflow so that, at a high-level the following micro-services are
+    created:
 
-    1. MediaConch is available for validation (in particular for validating
-       .mkv files)
-    2. Creates a "Validate Preservation Derivatives" micro-service (in
-       particular, so that MediaConch can be used for conformance-checking of
-       .mkv files created during "Normalize for preservation")
-    3. Creates a "Validate Access Derivatives" micro-service (in particular, so
-       that MediaConch can be used for conformance-checking of .mkv files
-       created during "Normalize for access")
+    1. "Policy Checks on Preservation Derivatives" micro-service
+    2. "Policy Checks on Access Derivatives" micro-service
+    3. "Policy Checks on Originals" micro-service
 
     Specifics:
 
-    a. MediaConch Tool (command-line program)
-    b. MediaConch Command (Python wrapper, "client script")
-    c. MediaConch-against-MKV-for-validate Rule
-    d. MediaConch-against-MKV-for-validatePreservationDerivative Rule
-    e. MediaConch-against-MKV-for-validateAccessDerivative Rule
-
-    f. "Validate Preservation Derivatives" chain link after "Normalize for
-       Preservation" chain link.
+    1. Policy Checks on Preservation Derivatives
 
        i.   Standard Tasks Config
-            - references ``validatePreservationDerivative_v0.0`` executable
+            - references ``policyCheckPreservationDerivative_v0.0`` executable
        ii.  Task Config
-            - configures ``validatePreservationDerivative`` as a "for each
-              file" type
-       iii. Chain Link ``vldt_prsrvtn_drvtv_cl``
-            - puts validate preservation derivative in the "Normalize" group
-            - sets the next chain link to the link that was the next link after
-              "Normalize for preservation".
-       iv.  Exit Codes
-            - "Validate preservation derivatives" continues on to the link that
-              was previously the next link after "Normalize for preservation",
-              no matter what the exit code (0, 1, or 2).
+            - configures ``policyCheckPreservationDerivative`` as a "for each file" type
+       iii. Chain Link ``prsrvtn_drvtv_policy_check_cl``
+            - puts policy check on access derivatives in the "Policy checks for
+              derivatives" group
+       iv.  Creates a choice point TaskConfig micro-service for letting the
+            user choose whether to perform policy checks on preservation derivatives.
 
-    g. "Validate Access Derivatives" chain link after "Normalize for Access"
-       chain link.
+    2. Policy Checks on Access Derivatives
 
        i.   Standard Tasks Config
-            - references ``validateAccessDerivative_v0.0`` executable
+            - references ``policyCheckAccessDerivative_v0.0`` executable
        ii.  Task Config
-            - configures ``validateAccessDerivative`` as a "for each file" type
-       iii. Chain Link ``vldt_ccss_drvtv_cl``
-            - puts validate access derivative in the "Normalize" group
-            - sets the next chain link to the link that was the next link after
-              "Normalize for access".
-       iv.  Exit Codes
-            - "Validate access derivatives" continues on to the link that
-              was previously the next link after "Normalize for access",
-              no matter what the exit code (0, 1, or 2).
+            - configures ``policyCheckPreservationDerivative`` as a "for each file" type
+       iii. Chain Link ``ccss_drvtv_policy_check_cl``
+            - puts policy check on access derivatives in the "Policy checks for
+              derivatives" group
+            - sets the next chain link to "Add final metadata > Move to
+              metadata reminder"
+       iv.  Creates a choice point TaskConfig micro-service for letting the
+            user choose whether to perform policy checks on access derivatives.
 
+    3. Policy Checks on Originals during Transfer
+
+       i.   Standard Tasks Config
+            - references ``policyCheckOriginal_v0.0`` executable
+       ii.  Task Config
+            - configures ``policyCheckOriginal`` as a "for each file" type
+       iii. Chain Link ``prsrvtn_drvtv_policy_check_cl``
+            - puts policy check on originals in the "Validation" group
+       iv.  Creates a choice point TaskConfig micro-service for letting the
+            user choose whether to perform policy checks on originals.
     """
 
     ###########################################################################
     # Model Classes
     ###########################################################################
 
-    FPTool = apps.get_model('fpr', 'FPTool')
-    FPCommand = apps.get_model('fpr', 'FPCommand')
-    FPRule = apps.get_model('fpr', 'FPRule')
-    FormatVersion = apps.get_model('fpr', 'FormatVersion')
     TaskType = apps.get_model('main', 'TaskType')
     TaskConfig = apps.get_model('main', 'TaskConfig')
     StandardTaskConfig = apps.get_model('main', 'StandardTaskConfig')
@@ -89,240 +71,9 @@ def data_migration(apps, schema_editor):
     # Get Existing Model Instances
     ###########################################################################
 
-    mkv_format = FormatVersion.objects.get(description='Generic MKV')
     for_each_file_type = TaskType.objects.get(description='for each file')
     user_choice_type = TaskType.objects.get(
         description='get user choice to proceed with')
-
-    # There are two chain links with the task config description 'Normalize for
-    # preservation'. However, all of them exit to the same next chain link.
-    nrmlz_prsrvtn_cl_1, nrmlz_prsrvtn_cl_2 = MicroServiceChainLink.objects\
-        .filter(currenttask__description='Normalize for preservation').all()
-    nrmlz_prsrvtn_next_link = nrmlz_prsrvtn_cl_1\
-        .exit_codes.first().nextmicroservicechainlink
-
-    # Similarly, there are two chain links with the task config description
-    # 'Normalize for access'. However, in this case, they exit to different
-    # next chain links.
-    nrmlz_ccss_cl_1, nrmlz_ccss_cl_2 = MicroServiceChainLink.objects\
-        .filter(currenttask__description='Normalize for access').all()
-    nrmlz_ccss_1_next_link = nrmlz_ccss_cl_1\
-        .exit_codes.first().nextmicroservicechainlink
-    nrmlz_ccss_2_next_link = nrmlz_ccss_cl_2\
-        .exit_codes.first().nextmicroservicechainlink
-
-    ###########################################################################
-    # Create MediaConch Tool and Command
-    ###########################################################################
-
-    # MediaConch Tool
-    mediaconch_tool_uuid = 'f79c56f1-2d42-440a-8a1f-f40888e24bca'
-    mediaconch_tool = FPTool.objects.create(
-        uuid=mediaconch_tool_uuid,
-        description='MediaConch',
-        version='16.12',
-        enabled=True,
-        slug='mediaconch-1612'
-    )
-
-    with open(VALIDATE_CMD_FNM) as filei:
-        mediaconch_command_script = filei.read()
-
-    # MediaConch Command
-    mediaconch_command_uuid = '287656fb-e58f-4967-bf72-0bae3bbb5ca8'
-    mediaconch_command = FPCommand.objects.create(
-        uuid=mediaconch_command_uuid,
-        tool=mediaconch_tool,
-        description='Validate using MediaConch',
-        command=mediaconch_command_script,
-        script_type='pythonScript',
-        command_usage='validation'
-    )
-
-    ###########################################################################
-    # MediaConch Rules
-    ###########################################################################
-
-    # MediaConch-against-MKV-for-validate Rule.
-    mediaconch_mkv_rule_uuid = 'a2fb0477-6cde-43f8-a1c9-49834913d588'
-    FPRule.objects.create(
-        uuid=mediaconch_mkv_rule_uuid,
-        purpose='validation',
-        command=mediaconch_command,
-        format=mkv_format
-    )
-
-    # MediaConch-against-MKV-for-validatePreservationDerivative Rule.
-    # Create the FPR rule that causes 'Validate using MediaConch' command to be
-    # used on for-preservation-derived 'Generic MKV' files in the "Validate
-    # Preservation Derivatives" micro-service.
-    vldt_prsrvtn_drvtv_rule_pk = '3fcbf5d2-c908-4ec4-b618-8c7dc0f4117e'
-    FPRule.objects.create(
-        uuid=vldt_prsrvtn_drvtv_rule_pk,
-        purpose='validatePreservationDerivative',
-        command=mediaconch_command,
-        format=mkv_format
-    )
-
-    # MediaConch-against-MKV-for-validateAccessDerivative Rule.
-    # Create the FPR rule that causes 'Validate using MediaConch' command to be
-    # used on for-access-derived 'Generic MKV' files in the "Validate
-    # Access Derivatives" micro-service.
-    vldt_ccss_drvtv_rule_pk = '0ada4f48-d8a6-4762-8a20-c04cb4e58676'
-    FPRule.objects.create(
-        uuid=vldt_ccss_drvtv_rule_pk,
-        purpose='validateAccessDerivative',
-        command=mediaconch_command,
-        format=mkv_format
-    )
-
-    ###########################################################################
-    # Validate PRESERVATION Derivatives CHAIN LINK, etc.
-    ###########################################################################
-
-    # Validate Preservation Derivatives Standard Task Config.
-    vldt_prsrvtn_drvtv_stc_pk = 'f8bc7b43-8bd4-4db8-88dc-d6f55732fb63'
-    StandardTaskConfig.objects.create(
-        id=vldt_prsrvtn_drvtv_stc_pk,
-        execute='validatePreservationDerivative_v0.0',
-        arguments=('"%relativeLocation%" "%fileUUID%" "%SIPUUID%"'
-                   ' "%sharedPath%"'),
-        filter_subdir='objects/'
-    )
-
-    # Validate Preservation Derivatives Task Config.
-    vldt_prsrvtn_drvtv_tc_pk = 'b6479474-159d-47aa-a10f-40495cb0e273'
-    vldt_prsrvtn_drvtv_tc = TaskConfig.objects.create(
-        id=vldt_prsrvtn_drvtv_tc_pk,
-        tasktype=for_each_file_type,
-        tasktypepkreference=vldt_prsrvtn_drvtv_stc_pk,
-        description='Validate preservation derivatives'
-    )
-
-    # Validate Preservation Derivatives Chain Link.
-    vldt_prsrvtn_drvtv_cl_pk = '5b0042a2-2244-475c-85d5-41e4b11e65d6'
-    vldt_prsrvtn_drvtv_cl = MicroServiceChainLink.objects.create(
-        id=vldt_prsrvtn_drvtv_cl_pk,
-        currenttask=vldt_prsrvtn_drvtv_tc,
-        defaultnextchainlink=nrmlz_prsrvtn_next_link,
-        microservicegroup='Normalize'
-    )
-
-
-    # Fix default next links for "Normalize for Preservation" links
-    nrmlz_prsrvtn_cl_1.defaultnextchainlink = vldt_prsrvtn_drvtv_cl
-    nrmlz_prsrvtn_cl_2.defaultnextchainlink = vldt_prsrvtn_drvtv_cl
-
-    # Update the six chain link exit code rows for the 'Normalize for
-    # preservation' chain links so that they exit to the 'Validate preservation
-    # derivatives' chain link.
-    MicroServiceChainLinkExitCode.objects\
-        .filter(microservicechainlink__in=[nrmlz_prsrvtn_cl_1,
-                                           nrmlz_prsrvtn_cl_2])\
-        .update(nextmicroservicechainlink=vldt_prsrvtn_drvtv_cl)
-
-    # Create three new chain link exit code rows that cause the Validate
-    # Preservation Derivatives chain link to exit to whatever chain link that
-    # Normalize for Preservation used to exit to.
-    for pk, exit_code, exit_message in (
-            ('f574f94f-c431-4442-a554-ac0934ccac93', 0,
-             'Completed successfully'),
-            ('d922a98b-2d65-4d75-bae0-9e8a446cb289', 1, 'Failed')):
-        MicroServiceChainLinkExitCode.objects.create(
-            id=pk,
-            microservicechainlink=vldt_prsrvtn_drvtv_cl,
-            exitcode=exit_code,
-            exitmessage=exit_message,
-            nextmicroservicechainlink=nrmlz_prsrvtn_next_link
-        )
-
-    ###########################################################################
-    # Validate ACCESS Derivatives CHAIN LINK, etc.
-    ###########################################################################
-
-    # Validate Access Derivatives Standard Task Config.
-    vldt_ccss_drvtv_stc_pk = '52e7912e-2ce9-4192-9ba4-19a75b2a2807'
-    StandardTaskConfig.objects.create(
-        id=vldt_ccss_drvtv_stc_pk,
-        execute='validateAccessDerivative_v0.0',
-        arguments=('"%relativeLocation%" "%fileUUID%" "%SIPUUID%"'
-                   ' "%sharedPath%"'),
-        filter_subdir='DIP/objects/'
-    )
-
-    # Validate Access Derivatives Task Config.
-    vldt_ccss_drvtv_tc_pk = 'b597753f-0b36-484f-ae78-4ae95951fd90'
-    vldt_ccss_drvtv_tc = TaskConfig.objects.create(
-        id=vldt_ccss_drvtv_tc_pk,
-        tasktype=for_each_file_type,
-        tasktypepkreference=vldt_ccss_drvtv_stc_pk,
-        description='Validate access derivatives'
-    )
-
-    # Validate Access Derivatives Chain Link # 1.
-    vldt_ccss_drvtv_cl_1_pk = '286bbb36-6a38-41d5-bf7a-a8ba58aa71ce'
-    vldt_ccss_drvtv_cl_1 = MicroServiceChainLink.objects.create(
-        id=vldt_ccss_drvtv_cl_1_pk,
-        currenttask=vldt_ccss_drvtv_tc,
-        defaultnextchainlink=nrmlz_ccss_1_next_link,
-        microservicegroup='Normalize'
-    )
-
-    # Validate Access Derivatives Chain Link # 2.
-    vldt_ccss_drvtv_cl_2_pk = 'a7c18fee-c8c1-4713-ba74-9705c45efbce'
-    vldt_ccss_drvtv_cl_2 = MicroServiceChainLink.objects.create(
-        id=vldt_ccss_drvtv_cl_2_pk,
-        currenttask=vldt_ccss_drvtv_tc,
-        defaultnextchainlink=nrmlz_ccss_2_next_link,
-        microservicegroup='Normalize'
-    )
-
-    # Fix default next links for "Normalize for Access" links
-    nrmlz_ccss_cl_1.defaultnextchainlink = vldt_ccss_drvtv_cl_1
-    nrmlz_ccss_cl_2.defaultnextchainlink = vldt_ccss_drvtv_cl_2
-
-    # Update the three chain link exit code rows for the FIRST 'Normalize for
-    # access' chain link so that they exit to the FIRST 'Validate access
-    # derivatives' chain link.
-    MicroServiceChainLinkExitCode.objects\
-        .filter(microservicechainlink=nrmlz_ccss_cl_1)\
-        .update(nextmicroservicechainlink=vldt_ccss_drvtv_cl_1)
-
-    # Update the three chain link exit code rows for the SECOND 'Normalize for
-    # access' chain link so that they exit to the SECOND 'Validate access
-    # derivatives' chain link.
-    MicroServiceChainLinkExitCode.objects\
-        .filter(microservicechainlink=nrmlz_ccss_cl_2)\
-        .update(nextmicroservicechainlink=vldt_ccss_drvtv_cl_2)
-
-    # Create three new MSCL exit code rows that cause the Validate
-    # Access Derivatives CL 1 to exit to whatever Normalize for Access 1 used
-    # to exit to.
-    for pk, exit_code, exit_message in (
-            ('9bbaafd6-9954-4f1f-972a-4f7eb0a60de7', 0,
-             'Completed successfully'),
-            ('de1dabdd-93ca-4f3b-accf-b9096aa494ba', 1, 'Failed')):
-        MicroServiceChainLinkExitCode.objects.create(
-            id=pk,
-            microservicechainlink=vldt_ccss_drvtv_cl_1,
-            exitcode=exit_code,
-            exitmessage=exit_message,
-            nextmicroservicechainlink=nrmlz_ccss_1_next_link
-        )
-
-    # Create three new MSCL exit code rows that cause the Validate
-    # Access Derivatives CL 2 to exit to whatever Normalize for Access 2 used
-    # to exit to.
-    for pk, exit_code in (
-            ('09df34f7-31ff-4107-82e7-1db36351acd3', 0),
-            ('0e92980d-2545-42f6-9d62-b506ac2ceecb', 1),
-            ('41a2ab1e-6804-4228-9f6c-ae6259839348', 2)):
-        MicroServiceChainLinkExitCode.objects.create(
-            id=pk,
-            microservicechainlink=vldt_ccss_drvtv_cl_2,
-            exitcode=exit_code,
-            nextmicroservicechainlink=nrmlz_ccss_2_next_link
-        )
 
     ###########################################################################
     # Policy Check for Derivative CHAIN LINKs, etc.
@@ -562,45 +313,6 @@ def data_migration(apps, schema_editor):
         )
 
     ###########################################################################
-    # Create MediaConch Command for Policy Checking
-    ###########################################################################
-
-    with open(POLICY_CHECK_CMD_FNM) as filei:
-        mediaconch_policy_check_command_script = filei.read()
-
-    # MediaConch Command
-    # NOTE: this command is disabled by default because it references a
-    # dummy/non-existent policy file.
-    mediaconch_policy_check_command_uuid = \
-        '9ef290f7-5320-4d69-821a-3156fc184b4e'
-    mediaconch_policy_check_command = FPCommand.objects.create(
-        uuid=mediaconch_policy_check_command_uuid,
-        tool=mediaconch_tool,
-        description=('Check against policy PLACEHOLDER_FOR_POLICY_FILE_NAME'
-                     ' using MediaConch'),
-        command=mediaconch_policy_check_command_script,
-        script_type='pythonScript',
-        command_usage='validation',
-        enabled=False
-    )
-
-    # MediaConch-against-MKV-for-policyCheckingPreservationFile Rule.
-    # Create the FPR rule that causes 'Check against policy using MediaConch'
-    # command to be used on 'Generic MKV' files intended for preservation in
-    # the "Policy check" micro-service.
-    # NOTE: this rule is disabled by default because it references a disabled
-    # command that, in turn, references a non-existent MediaConch policy file.
-    policy_check_preservation_rule_pk = 'aaaf34ef-c00f-4bb9-85c1-01c0ad5f3a8c'
-    FPRule.objects.create(
-        uuid=policy_check_preservation_rule_pk,
-        purpose='checkingPresDerivativePolicy',
-        command=mediaconch_policy_check_command,
-        format=mkv_format,
-        enabled=False
-    )
-
-
-    ###########################################################################
     # Policy Checking on Originals during Transfer
     ###########################################################################
 
@@ -649,7 +361,6 @@ def data_migration(apps, schema_editor):
             exitmessage=exit_message,
             nextmicroservicechainlink=move_examine_contents_cl
         )
-
 
     ###########################################################################
     # BEGIN Micro-service that asks "Do you want to do policy checks on
@@ -732,7 +443,7 @@ def data_migration(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0029_backlog_removal_event'),
+        ('main', '0030_mediaconch_validation'),
     ]
 
     operations = [
