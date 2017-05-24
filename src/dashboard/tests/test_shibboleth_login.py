a2805957a35dfe5ca01c16c5372988e33978014c
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2017-05-24 09:34:23 +0100

    Update Shib headers to match what nginx sends

--- src/dashboard/tests/test_shibboleth_login.py
+++ src/dashboard/tests/test_shibboleth_login.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.test import TestCase
 
@@ -14,10 +13,10 @@ class TestShibbolethLogin(TestCase):
 
     def test_auto_creates_user(self):
         shib_headers = {
-            'HTTP_X_SHIB_USER': 'testuser',
-            'HTTP_X_SHIB_GIVEN_NAME': 'Test',
-            'HTTP_X_SHIB_SN': 'User',
-            'HTTP_X_SHIB_MAIL': 'test@example.com'
+            'HTTP_EPPN': 'testuser',
+            'HTTP_GIVENNAME': 'Test',
+            'HTTP_SN': 'User',
+            'HTTP_MAIL': 'test@example.com'
         }
 
         response = self.client.get('/transfer/', **shib_headers)
@@ -33,10 +32,10 @@ class TestShibbolethLogin(TestCase):
     def test_uses_existing_user(self):
         user = User.objects.create(username='testuser')
         shib_headers = {
-            'HTTP_X_SHIB_USER': 'testuser',
-            'HTTP_X_SHIB_GIVEN_NAME': 'Test',
-            'HTTP_X_SHIB_SN': 'User',
-            'HTTP_X_SHIB_MAIL': 'test@example.com'
+            'HTTP_EPPN': 'testuser',
+            'HTTP_GIVENNAME': 'Test',
+            'HTTP_SN': 'User',
+            'HTTP_MAIL': 'test@example.com'
         }
 
         response = self.client.get('/transfer/', **shib_headers)
@@ -47,10 +46,10 @@ class TestShibbolethLogin(TestCase):
     def test_long_username(self):
         long_email = 'person-with-very-long-name@long-institution-name.ac.uk'
         shib_headers = {
-            'HTTP_X_SHIB_USER': long_email,
-            'HTTP_X_SHIB_GIVEN_NAME': 'Test',
-            'HTTP_X_SHIB_SN': 'User',
-            'HTTP_X_SHIB_MAIL': 'test@example.com'
+            'HTTP_EPPN': long_email,
+            'HTTP_GIVENNAME': 'Test',
+            'HTTP_SN': 'User',
+            'HTTP_MAIL': 'test@example.com'
         }
 
         response = self.client.get('/transfer/', **shib_headers)
