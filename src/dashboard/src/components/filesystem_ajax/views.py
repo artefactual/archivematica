a0aa94ec823c6b0f092bc0f049907dd572fb4ea3
Joel Dunham <jrwdunham@gmail.com>
2017-06-13 08:39:51 -0700

    Allow UTF8-encoded transfer directory names
    UTF8-encodes Location paths when creating the source and destination in
    ``filesystem_ajax/views.py::copy_from_transfer_sources``. This allows transfers
    to be started on UTF-8-encoded directory names.

--- src/dashboard/src/components/filesystem_ajax/views.py
+++ src/dashboard/src/components/filesystem_ajax/views.py
@@ -827,11 +827,18 @@ def copy_from_transfer_sources(paths, relative_destination):
             logger.warning('Location %s is not associated with this pipeline.', location)
             return True, _('Location %(location)s is not associated with this pipeline') % {'location': location}
 
-        source = path.replace(files[location]['location']['path'], '', 1).lstrip('/')
+        # ``path`` will be a UTF-8 bytestring but the replacement pattern path
+        # from ``files`` will be a Unicode object. Therefore, the latter must
+        # be UTF-8 encoded prior. Same reasoning applies to ``destination``
+        # below. This allows transfers to be started on UTF-8-encoded directory
+        # names.
+        source = path.replace(files[location]['location']['path'].encode('utf8'), '', 1).lstrip('/')
         # Use the last segment of the path for the destination - basename for a
         # file, or the last folder if not. Keep the trailing / for folders.
         last_segment = os.path.basename(source.rstrip('/')) + '/' if source.endswith('/') else os.path.basename(source)
-        destination = os.path.join(processing_location['path'], relative_destination, last_segment).replace('%sharedPath%', '')
+        destination = os.path.join(processing_location['path'].encode('utf8'),
+                                   relative_destination,
+                                   last_segment).replace('%sharedPath%', '')
         files[location]['files'].append({'source': source, 'destination': destination})
         logger.debug('source: %s, destination: %s', source, destination)
 
