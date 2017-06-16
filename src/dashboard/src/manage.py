5f47397d2ca43f85bafab8f4e6a830f656472266
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2017-06-16 17:36:58 +0000

    Dashboard: set up Elasticsearch client in manage.py

--- src/dashboard/src/manage.py
+++ src/dashboard/src/manage.py
@@ -5,6 +5,10 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
 
+    # Set up Elasticsearch client
+    import elasticSearchFunctions
+    elasticSearchFunctions.setup_reading_from_client_conf()
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
