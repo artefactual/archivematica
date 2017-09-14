e1fb1b5983f2a6da676924b7e22cd866e603d5a6
Miguel Angel Medinilla <mamedin@gmail.com>
2017-09-14 18:18:40 +0200

    Antivirus detection fixed
    It fixes the issue [#730] (https://github.com/artefactual/archivematica/issues/730)
    and Jisc issue [#68](https://github.com/JiscRDSS/rdss-archivematica/issues/68)

--- src/MCPClient/lib/clientScripts/archivematicaClamscan.py
+++ src/MCPClient/lib/clientScripts/archivematicaClamscan.py
@@ -108,7 +108,7 @@ def main(fileUUID, target, date):
     else:
         record_event(fileUUID, version, date, outcome='Fail')
         logger.info('Clamd state=%s - %s', state, details)
-
+        return 1
 
 if __name__ == '__main__':
     fileUUID = sys.argv[1]
