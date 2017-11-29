0b3ee00ec9cdce1ad3d3777768666edee4ef9d03
Joel Dunham <jrwdunham@gmail.com>
2017-11-29 15:20:48 -0800

    Fix launchSubProcess tests
    - Now that the `capture_output` kwarg defaults to `False`, the tests had to be
      modified to account for that.
    - Also fixed some formatting/linter issues in the test file.

--- src/archivematicaCommon/tests/test_execute_functions.py
+++ src/archivematicaCommon/tests/test_execute_functions.py
@@ -1,15 +1,18 @@
 # -*- coding: UTF-8 -*-
-import pytest
 
 import executeOrRunSubProcess as execsub
 
-"""Tests behaviour of capture_output when executing sub processes."""
+
 def test_capture_output():
-        # Test that stdout and stderr are captured by default
-        ret, std_out, std_err = execsub.launchSubProcess(['ls' , '/tmp'])
-        assert std_out is not '' or std_err is not ''
-        # Test that stdout and stderr are not captured when disabled
-        ret, std_out, std_err = execsub.launchSubProcess(['ls', '/tmp'],
-                                                         capture_output=False)
-        assert std_out is ''
-        assert std_err is''
+    """Tests behaviour of capture_output when executing sub processes."""
+
+    # Test that stdout and stderr are not captured by default
+    ret, std_out, std_err = execsub.launchSubProcess(['ls', '/tmp'])
+    assert std_out is ''
+    assert std_err is''
+
+    # Test that stdout and stderr are captured when `capture_output` is
+    # enabled.
+    ret, std_out, std_err = execsub.launchSubProcess(
+        ['ls', '/tmp'], capture_output=True)
+    assert std_out is not '' or std_err is not ''
