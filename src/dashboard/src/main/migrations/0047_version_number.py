7a10307d3ff2d9c1cb86dd82fb8ac64ed07e0b50
Jesús García Crespo <jesus@sevein.com>
2018-01-16 12:50:32 -0800

    Update version to 1.7
    This is in addition to 1f9adf8fae46211459fe910b81e6c18e18ad853b.

---
+++ src/dashboard/src/main/migrations/0047_version_number.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def data_migration(apps, schema_editor):
+    Agent = apps.get_model('main', 'Agent')
+    Agent.objects \
+        .filter(identifiertype='preservation system', name='Archivematica') \
+        .update(identifiervalue='Archivematica-1.7')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0046_optional_normative_structmap'),
+    ]
+
+    operations = [
+        migrations.RunPython(data_migration)
+    ]
