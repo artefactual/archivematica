artefactual/archivematica

commit 4922187899d947674faa3e197c029d24a8ff9be1

Stop using django-annoying

src/dashboard/src/components/api/views.py
@@ -30,7 +30,6 @@ import django.http
 from django.conf import settings as django_settings
 
 # External dependencies, alphabetical
-from annoying.functions import get_object_or_None
 from tastypie.authentication import ApiKeyAuthentication, MultiAuthentication, SessionAuthentication
 
 # This project, alphabetical
@@ -164,10 +163,16 @@ def status(request, unit_uuid, unit_type):
 
     # Get info about unit
     if unit_type == 'unitTransfer':
-        unit = get_object_or_None(models.Transfer, uuid=unit_uuid)
+        try:
+            unit = models.Transfer(uuid=unit_uuid)
+        except models.Transfer.DoesNotExist:
+            unit = None
         response['type'] = 'transfer'
     elif unit_type == 'unitSIP':
-        unit = get_object_or_None(models.SIP, uuid=unit_uuid)
+        try:
+            unit = models.SIP(uuid=unit_uuid)
+        except models.SIP.DoesNotExist:
+            unit = None
         response['type'] = 'SIP'
 
     if unit is None:
@@ -655,33 +660,33 @@ def path_metadata(request):
     path = request.GET.get('path', '') if request.method == 'GET' else request.POST.get('path', '')
 
     # Get current metadata, if any
-    file_lod = get_object_or_None(models.SIPArrange, arrange_path=path, sip_created=False)
-    if file_lod is None:
-        # Try with trailing / to see if it's a directory
-        file_lod = get_object_or_None(models.SIPArrange, arrange_path=path + '/', sip_created=False)
+    files = models.SIPArrange.objects.filter(
+        arrange_path__in=(path, path + '/'),
+        sip_created=False)
+    if not files:
+        raise django.http.Http404
+    file_lod = files.first()
 
     # Return current metadata, if requested
     if request.method == 'GET':
-        level_of_description = file_lod.level_of_description if file_lod is not None else ''
-
+        level_of_description = file_lod.level_of_description
         return helpers.json_response({
             "level_of_description": level_of_description
         })
 
     # Add/update metadata, if requested
     if request.method == 'POST':
-        level_of_description_id = request.POST.get('level_of_description', '')
-
-        level_of_description = get_object_or_None(models.LevelOfDescription, id=level_of_description_id)
-
         file_lod.relative_location = path
-        file_lod.level_of_description = level_of_description.name if level_of_description is not None else ''
+        try:
+            file_lod.level_of_description = \
+                models.LevelOfDescription.objects.get(
+                    pk=request.POST['level_of_description']).name
+        except (KeyError, models.LevelOfDescription.DoesNotExist):
+            file_lod.level_of_description = ''
         file_lod.save()
-
         body = {
             "success": True,
         }
-
         return helpers.json_response(body, status_code=201)
 
 

src/MCPClient/lib/clientScripts/normalize.py
@@ -20,7 +20,6 @@ django.setup()
 # dashboard
 from fpr.models import FPRule
 from main.models import Derivation, FileFormatVersion, File, FileID
-from annoying.functions import get_object_or_None
 
 # archivematicaCommon
 from custom_handlers import get_script_logger
@@ -333,10 +332,10 @@ def main(opts):
         return SUCCESS
 
     do_fallback = False
-    format_id = get_object_or_None(
-        FileFormatVersion,
-        file_uuid=opts.file_uuid
-    )
+    try:
+        format_id = FileFormatVersion.objects.get(file_uuid=opts.file_uuid)
+    except FileFormatVersion.DoesNotExist:
+        format_id = None
 
     # Look up the normalization command in the FPR
     if format_id:

src/MCPClient/lib/clientScripts/storeAIP.py
@@ -21,7 +21,7 @@
 # @subpackage archivematicaClientScript
 # @author Joseph Perry <joseph@artefactual.com>
 from __future__ import print_function
-from annoying.functions import get_object_or_None
+
 import argparse
 import os
 import sys
@@ -125,8 +125,13 @@ def store_aip(aip_destination_uri, aip_path, sip_uuid, sip_name, sip_type):
             print('Noting DIP UUID {} related to AIP so relationship can be created when AIP is stored.'.format(uuid))
     else:
         uuid = sip_uuid
-        related_package = get_object_or_None(UnitVariable, unituuid=sip_uuid, variable='relatedPackage')
-        related_package_uuid = related_package.variablevalue if related_package is not None else None
+        try:
+            related_package = UnitVariable.objects.get(
+                unituuid=sip_uuid, variable='relatedPackage')
+        except UnitVariable.DoesNotExist:
+            pass
+        else:
+            related_package_uuid = related_package.variablevalue
 
     # If AIP is a directory, calculate size recursively
     if os.path.isdir(aip_path):

src/archivematicaCommon/lib/utilities/FPRClient/client.py
@@ -6,8 +6,6 @@ import os
 
 from .getFromRestAPI import each_record, FPRConnectionError
 
-from annoying.functions import get_object_or_None
-
 # Set up Django settings
 if 'DJANGO_SETTINGS_MODULE' not in os.environ:
     os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.common'
@@ -47,13 +45,17 @@ class FPRClient(object):
             self.maxLastUpdate = fields['lastmodified']
 
         # If an fields with this UUID exists, update enabled
-        obj = get_object_or_None(model, uuid=fields['uuid'])
-        if obj:
-            print('Object already in DB:', get_object_or_None(model, uuid=fields['uuid']))
+        try:
+            obj = model.objects.get(uuid=fields['uuid'])
+        except model.DoesNotExist:
+            pass
+        else:
+            print('Object already in DB:', obj)
             if not hasattr(model, 'replaces') and hasattr(model, 'enabled'):
                 obj.enabled = fields['enabled']
                 obj.save()
             return
+
         # Otherwise, new fields, need to add to database
 
         # TastyPie doesn't like fields named format, so they're all fmt
@@ -75,10 +77,10 @@ class FPRClient(object):
                 valid_fields[field + u"_id"] = uuid
         # Insert FormatGroup for Formats if not exist
         if model == models.Format:
-            if not get_object_or_None(
-                    models.FormatGroup,
-                    uuid=fields['group']['uuid']):
-                models.FormatGroup.objects.create(**fields['group'])
+            models.FormatGroup.objects.get_or_create(
+                uuid=fields['group']['uuid'],
+                defaults=fields['group'],
+            )
             valid_fields['group_id'] = fields['group']['uuid']
             del valid_fields['group']
 

commit 6c1ffdf02e21f017be0944644ed0f303d4424243

Fix typo introduced in 4922187899d947674faa3e197c029d24a8ff9be1

src/dashboard/src/components/api/views.py
@@ -164,13 +164,13 @@ def status(request, unit_uuid, unit_type):
     # Get info about unit
     if unit_type == 'unitTransfer':
         try:
-            unit = models.Transfer(uuid=unit_uuid)
+            unit = models.Transfer.objects.get(uuid=unit_uuid)
         except models.Transfer.DoesNotExist:
             unit = None
         response['type'] = 'transfer'
     elif unit_type == 'unitSIP':
         try:
-            unit = models.SIP(uuid=unit_uuid)
+            unit = models.SIP.objects.get(uuid=unit_uuid)
         except models.SIP.DoesNotExist:
             unit = None
         response['type'] = 'SIP'