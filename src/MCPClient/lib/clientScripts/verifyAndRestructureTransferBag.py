685ad01ed78d0b7b049762af742f377509ba5102
Hector Akamine <hakamine@artefactual.com>
2018-03-23 09:18:58 -0400

    MoMA: exclude hidden files in bag checks

--- src/MCPClient/lib/clientScripts/verifyAndRestructureTransferBag.py
+++ src/MCPClient/lib/clientScripts/verifyAndRestructureTransferBag.py
@@ -43,20 +43,20 @@ verificationCommandsOutputs = []
 def verifyBag(bag):
     global exitCode
     verificationCommands = []
-    verificationCommands.append("/usr/share/bagit/bin/bag verifyvalid \"%s\"" % (bag)) #Verifies the validity of a bag.
-    verificationCommands.append("/usr/share/bagit/bin/bag verifycomplete \"%s\"" % (bag)) #Verifies the completeness of a bag.
-    verificationCommands.append("/usr/share/bagit/bin/bag verifypayloadmanifests \"%s\"" % (bag)) #Verifies the checksums in all payload manifests.
+    verificationCommands.append("/usr/share/bagit/bin/bag verifyvalid --excludehiddenfiles \"%s\"" % (bag)) #Verifies the validity of a bag.
+    verificationCommands.append("/usr/share/bagit/bin/bag verifycomplete --excludehiddenfiles \"%s\"" % (bag)) #Verifies the completeness of a bag.
+    verificationCommands.append("/usr/share/bagit/bin/bag verifypayloadmanifests --excludehiddenfiles \"%s\"" % (bag)) #Verifies the checksums in all payload manifests.
     
     bagInfoPath = os.path.join(bag, "bag-info.txt")
     if os.path.isfile(bagInfoPath):
         for line in open(bagInfoPath,'r'):
             if line.startswith("Payload-Oxum"):
-                verificationCommands.append("/usr/share/bagit/bin/bag checkpayloadoxum \"%s\"" % (bag)) #Generates Payload-Oxum and checks against Payload-Oxum in bag-info.txt.
+                verificationCommands.append("/usr/share/bagit/bin/bag checkpayloadoxum --excludehiddenfiles \"%s\"" % (bag)) #Generates Payload-Oxum and checks against Payload-Oxum in bag-info.txt.
                 break
     
     for item in os.listdir(bag):
         if item.startswith("tagmanifest-") and item.endswith(".txt"):        
-            verificationCommands.append("/usr/share/bagit/bin/bag verifytagmanifests \"%s\"" % (bag)) #Verifies the checksums in all tag manifests.
+            verificationCommands.append("/usr/share/bagit/bin/bag verifytagmanifests --excludehiddenfiles \"%s\"" % (bag)) #Verifies the checksums in all tag manifests.
             break
 
     for command in verificationCommands:
