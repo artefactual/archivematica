040c8f002bc4db04702fb8a3aee46c901ce5c1ae
Jesús García Crespo <jesus@sevein.com>
2018-07-29 18:47:25 -0700

    Migration: fix unit variable pull link
    Set `defaultExitMessage` to `2` (`Job.STATUS_COMPLETED_SUCCESSFULLY`) so the
    job is marked as successful when `linkTaskManagerUnitVariableLinkPull`
    completes.
    
    This is connected to #1090.

---
+++ src/dashboard/src/main/migrations/0058_fix_unit_variable_pull_link.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""Migration to update "Resume after normalization file identification tool
+selected." chain link to exit with the successful status."""
+
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+# Can't use apps.get_model for this model as we need to access class attributes.
+from main.models import Job
+
+
+# Link: "Resume after normalization file identification tool selected."
+mscl_id = '83484326-7be7-4f9f-b252-94553cd42370'
+
+
+def data_migration_up(apps, schema_editor):
+    mscl = apps.get_model('main', 'MicroServiceChainLink')
+
+    mscl.objects.filter(pk=mscl_id).update(
+        defaultexitmessage=Job.STATUS_COMPLETED_SUCCESSFULLY)
+
+
+def data_migration_down(apps, schema_editor):
+    mscl = apps.get_model('main', 'MicroServiceChainLink')
+
+    mscl.objects.filter(pk=mscl_id).update(
+        defaultexitmessage=Job.STATUS_FAILED)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0057_7zip_no_compression'),
+    ]
+
+    operations = [
+        migrations.RunPython(data_migration_up, data_migration_down),
+    ]
