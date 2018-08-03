e209c8eafb0855964d542206079b36eac5e66cc3
Jesús García Crespo <jesus@sevein.com>
2018-08-03 14:06:49 -0700

    Dashboard: add execute_unit method to MCPServer client
    This new method simplifies the approval of job(s) awaiting for approval
    when the user only knows the unit ID.

--- src/dashboard/src/contrib/mcp/client.py
+++ src/dashboard/src/contrib/mcp/client.py
@@ -15,14 +15,25 @@
 # You should have received a copy of the GNU General Public License
 # along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
 
-import gearman
 import cPickle
 
 from django.conf import settings
+import gearman
+
+from main.models import Job
 
 
 class RPCError(Exception):
     pass
+
+
+class NoJobFoundError(Exception):
+    def __init__(self, *args, **kwargs):
+        try:
+            message = args[0]
+        except IndexError:
+            message = "No job was found"
+        super(NoJobFoundError, self).__init__(message)
 
 
 class MCPClient:
@@ -39,6 +50,24 @@ class MCPClient:
         gm_client.submit_job("approveJob", cPickle.dumps(data), None)
         gm_client.shutdown()
         return
+
+    def execute_unit(self, unit_id, choice, mscl_id=None, uid=None):
+        """Execute the jobs awaiting for approval associated to a given unit.
+
+        Use ``mscl_id`` to pass the ID of the chain link to restrict the
+        execution to a single microservice.
+        """
+        kwargs = {
+            'currentstep': Job.STATUS_AWAITING_DECISION,
+            'sipuuid': unit_id,
+        }
+        if mscl_id is not None:
+            kwargs['microservicechainlink_id'] = mscl_id
+        jobs = Job.objects.filter(**kwargs)
+        if len(jobs) < 1:
+            raise NoJobFoundError()
+        for item in jobs:
+            self.execute(item.pk, choice, uid)
 
     def list(self):
         gm_client = gearman.GearmanClient([self.server])
