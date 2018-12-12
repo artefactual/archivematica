3177e181a9a8354026242ba31434badeba9613f4
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2018-12-12 16:58:31 +0100

    Update the Archivematica agent version to 1.8

---
+++ src/dashboard/src/main/migrations/0064_version_number.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Migrate the Archivematica agent version string to Archivematica-1.8."""
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def data_migration_down(apps, _):
+    """Undo the migration if requested."""
+    agent = apps.get_model('main', 'Agent')
+    agent.objects \
+        .filter(identifiertype='preservation system', name='Archivematica') \
+        .update(identifiervalue='Archivematica-1.7')
+
+
+def data_migration_up(apps, _):
+    """Update the application agent version in the database."""
+    agent = apps.get_model('main', 'Agent')
+    agent.objects \
+        .filter(identifiertype='preservation system', name='Archivematica') \
+        .update(identifiervalue='Archivematica-1.8')
+
+
+class Migration(migrations.Migration):
+    """Run the migration to update the Archivematica agent version string."""
+    dependencies = [
+        ('main', '0063_update_idtools'),
+    ]
+
+    operations = [
+        migrations.RunPython(data_migration_up, data_migration_down)
+    ]
