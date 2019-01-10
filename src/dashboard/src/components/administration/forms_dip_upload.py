cb7e4fc9bb3adb40efec446965a659445b083374
Jesús García Crespo <jesus@sevein.com>
2019-01-09 17:09:21 -0800

    Update agentarchives to v0.5.0
    This is a port of https://github.com/artefactual/archivematica/pull/1328 for
    `stable/1.7.x`. It brings more flexibility for the user to determine the
    connection details used in the ArchivesSpace client.

--- src/dashboard/src/components/administration/forms_dip_upload.py
+++ src/dashboard/src/components/administration/forms_dip_upload.py
@@ -25,8 +25,8 @@ EAD_SHOW_CHOICES = [
 
 
 class ArchivesSpaceConfigForm(forms.Form):
-    host = forms.CharField(label=_('ArchivesSpace host'), help_text=_('Do not include http:// or www. Example: aspace.test.org'))
-    port = forms.IntegerField(label=_('ArchivesSpace backend port'), help_text=_('Example: 8089'), initial=8089)
+    host = forms.CharField(label=_('ArchivesSpace host or base URL'), help_text=_('Examples: "http://space.test.org:8089", "space.test.org"'))
+    port = forms.IntegerField(label=_('ArchivesSpace backend port'), help_text=_('Ignored when the host value is a valid URL. Example: 8089'), initial=8089)
     user = forms.CharField(label=_('ArchivesSpace administrative user'), help_text=_('Example: admin'))
     passwd = forms.CharField(required=False, label=_('ArchivesSpace administrative user password'), help_text=_('Password for user set above. Re-enter this password every time changes are made.'))
     restrictions = forms.ChoiceField(choices=PREMIS_CHOICES, label=_('Restrictions apply'), initial='yes')
