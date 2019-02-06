c7161216d07f94fcada49e8305e2c684f5024165
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2019-02-05 19:26:33 -0600

    Dashboard: fix metadata views of SIPs in Ingest tab
    - Manual normalization event detail
    - Add metadata files
    
    Also added basic browser tests for both.

--- src/dashboard/tests/test_ingest.py
+++ src/dashboard/tests/test_ingest.py
@@ -1,5 +1,6 @@
 import os
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 import mock
@@ -14,7 +15,7 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class TestIngest(TestCase):
-    fixture_files = ['test_user.json']
+    fixture_files = ['test_user.json', 'sip.json', 'jobs-sip-complete.json']
     fixtures = [os.path.join(THIS_DIR, 'fixtures', p) for p in fixture_files]
 
     def setUp(self):
@@ -54,3 +55,37 @@ class TestIngest(TestCase):
         })
         with pytest.raises(ArchivesSpaceError):
             client = get_as_system_client()
+
+    def test_normalization_event_detail_view(self):
+        """Test the 'Manual normalization event detail' view of a SIP"""
+        sip_uuid = '4060ee97-9c3f-4822-afaf-ebdf838284c3'
+        url = reverse(
+            'components.ingest.views.ingest_metadata_event_detail',
+            args=[sip_uuid],
+        )
+        response = self.client.get(url)
+        assert response.status_code == 200
+        title = ''.join([
+            '<h1>'
+            'Normalization Event Detail<br />',
+            '<small>test</small>',
+            '</h1>',
+        ])
+        assert title in response.content
+
+    def test_add_metadata_files_view(self):
+        """Test the 'Add metadata files' view of a SIP"""
+        sip_uuid = '4060ee97-9c3f-4822-afaf-ebdf838284c3'
+        url = reverse(
+            'components.ingest.views.ingest_metadata_add_files',
+            args=[sip_uuid],
+        )
+        response = self.client.get(url)
+        assert response.status_code == 200
+        title = '\n    '.join([
+            '<h1>',
+            '  Add metadata files<br />',
+            '  <small>test</small>',
+            '</h1>',
+        ])
+        assert title in response.content
