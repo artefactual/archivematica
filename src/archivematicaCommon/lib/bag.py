de07c97c3a40a485d33cad93fe34fe6469da05f7
Jesús García Crespo <jesus@sevein.com>
2019-03-19 10:02:57 -0700

    bag: add is_bag function

--- src/archivematicaCommon/lib/bag.py
+++ src/archivematicaCommon/lib/bag.py
@@ -6,6 +6,20 @@ import sys
 from bagit import Bag, BagError
 
 
+def is_bag(path, printfn=print):
+    """Determine whether the directory contains a BagIt package.
+
+    The constructor of ``Bag`` is fast enough but we may prefer to optimize
+    later.
+    """
+    try:
+        Bag(path)
+    except BagError as err:
+        printfn("Error opening BagIt package:", err, file=sys.stderr)
+        return False
+    return True
+
+
 def is_valid(path, completeness_only=False, printfn=print):
     """Return whether a BagIt package is valid given its ``path``."""
     try:
