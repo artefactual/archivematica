2c0c477202bbff31fc15bbd5cda4cd8a3bca8830
Jesús García Crespo <jesus@sevein.com>
2019-03-20 08:05:51 -0700

    Bump version to 1.10.0
    The "preservation system" agent is still a database object, this is the usual
    migration that bumps its version (see #383).

---
+++ src/dashboard/src/main/migrations/0068_version_number.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""Migrate the Archivematica agent version string to Archivematica-1.9."""
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def data_migration_down(apps, _):
+    """Undo the migration if requested."""
+    agent = apps.get_model("main", "Agent")
+    agent.objects.filter(
+        identifiertype="preservation system", name="Archivematica"
+    ).update(identifiervalue="Archivematica-1.9")
+
+
+def data_migration_up(apps, _):
+    """Update the application agent version in the database."""
+    agent = apps.get_model("main", "Agent")
+    agent.objects.filter(
+        identifiertype="preservation system", name="Archivematica"
+    ).update(identifiervalue="Archivematica-1.10")
+
+
+class Migration(migrations.Migration):
+    """Run the migration to update the Archivematica agent version string."""
+
+    dependencies = [("main", "0067_delete_workflow_models")]
+
+    operations = [migrations.RunPython(data_migration_up, data_migration_down)]
