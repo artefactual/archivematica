67d56acb85ef3cec4569670dc6a3c38253de6a4c
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-22 12:09:39 +0000

    Use fixtures to tidy up the tests

--- src/MCPClient/tests/test_move_or_merge.py
+++ src/MCPClient/tests/test_move_or_merge.py
@@ -2,52 +2,63 @@
 
 import pytest
 
-from .move_or_merge import move_or_merge
+from move_or_merge import move_or_merge
 
 
-def test_move_or_merge_when_dst_doesnt_exist(tmpdir):
-    src = tmpdir.join("src.txt")
-    dst = tmpdir.join("dst.txt")
-
-    src.write("hello world")
-
-    move_or_merge(src=src, dst=dst)
-    assert not src.exists()
-    assert dst.exists()
-    assert dst.read() == "hello world"
+@pytest.fixture
+def src_txt(tmpdir):
+    return tmpdir.join("src.txt")
 
 
-def test_okay_if_dst_exists_and_is_same(tmpdir):
-    src = tmpdir.join("src.txt")
-    dst = tmpdir.join("dst.txt")
-
-    src.write("hello world")
-    dst.write("hello world")
-
-    move_or_merge(src=src, dst=dst)
-    assert not src.exists()
-    assert dst.exists()
-    assert dst.read() == "hello world"
+@pytest.fixture
+def dst_txt(tmpdir):
+    return tmpdir.join("dst.txt")
 
 
-def test_error_if_dst_exists_and_is_different(tmpdir):
-    src = tmpdir.join("src.txt")
-    dst = tmpdir.join("dst.txt")
+@pytest.fixture
+def src_dir(tmpdir):
+    return tmpdir.mkdir("src")
 
-    src.write("hello world")
-    dst.write("we come in peace")
+
+@pytest.fixture
+def dst_dir(tmpdir):
+    return tmpdir.mkdir("dst")
+
+
+def test_move_or_merge_when_dst_doesnt_exist(src_txt, dst_txt):
+    src_txt.write("hello world")
+
+    move_or_merge(src=str(src_txt), dst=str(dst_txt))
+    assert not src_txt.exists()
+    assert dst_txt.exists()
+    assert dst_txt.read() == "hello world"
+
+
+def test_okay_if_dst_exists_and_is_same(src_txt, dst_txt):
+    src_txt.write("hello world")
+    dst_txt.write("hello world")
+
+    move_or_merge(src=str(src_txt), dst=str(dst_txt))
+    assert not src_txt.exists()
+    assert dst_txt.exists()
+    assert dst_txt.read() == "hello world"
+
+
+def test_error_if_dst_exists_and_is_different(src_txt, dst_txt):
+    src_txt.write("hello world")
+    dst_txt.write("we come in peace")
 
     with pytest.raises(RuntimeError, match="dst exists and is different"):
-        move_or_merge(src=src, dst=dst)
+        move_or_merge(src=str(src_txt), dst=str(dst_txt))
 
     # Check the original file wasn't deleted
-    assert src.exists()
-    assert dst.exists()
+    assert src_txt.exists()
+    assert dst_txt.exists()
 
 
-def test_moves_contents_of_directory(tmpdir):
+def test_moves_contents_of_directory_when_dst_does_not_exist(tmpdir):
     src_dir = tmpdir.mkdir("src")
-    dst_dir = tmpdir.mkdir("dst")
+    dst_dir = tmpdir.join("dst")
 
     src = src_dir.join("file.txt")
     dst = dst_dir.join("file.txt")
@@ -59,10 +70,18 @@ def test_moves_contents_of_directory(tmpdir):
     assert dst.read() == "hello world"
 
 
-def test_moves_nested_directory(tmpdir):
-    src_dir = tmpdir.mkdir("src")
-    dst_dir = tmpdir.mkdir("dst")
+def test_moves_contents_of_directory(src_dir, dst_dir):
+    src = src_dir.join("file.txt")
+    dst = dst_dir.join("file.txt")
+    src.write("hello world")
 
+    move_or_merge(src=str(src_dir), dst=str(dst_dir))
+    assert not src.exists()
+    assert dst.exists()
+    assert dst.read() == "hello world"
+
+
+def test_moves_nested_directory(src_dir, dst_dir):
     src_nested = src_dir.mkdir("nested")
     dst_nested = dst_dir.join("nested")
 
@@ -76,10 +95,7 @@ def test_moves_nested_directory(tmpdir):
     assert dst.read() == "hello world"
 
 
-def test_merges_nested_directory(tmpdir):
-    src_dir = tmpdir.mkdir("src")
-    dst_dir = tmpdir.mkdir("dst")
-
+def test_merges_nested_directory(src_dir, dst_dir):
     src_nested = src_dir.mkdir("nested")
 
     # Unlike the previous test, we create the "nested" directory upfront,
@@ -96,10 +112,7 @@ def test_merges_nested_directory(tmpdir):
     assert dst.read() == "hello world"
 
 
-def test_merges_nested_directory_with_existing_file(tmpdir):
-    src_dir = tmpdir.mkdir("src")
-    dst_dir = tmpdir.mkdir("dst")
-
+def test_merges_nested_directory_with_existing_file(src_dir, dst_dir):
     src_nested = src_dir.mkdir("nested")
     dst_nested = dst_dir.mkdir("nested")
 
@@ -114,10 +127,7 @@ def test_merges_nested_directory_with_existing_file(tmpdir):
     assert dst.read() == "hello world"
 
 
-def test_merges_nested_directory_with_mismatched_existing_file(tmpdir):
-    src_dir = tmpdir.mkdir("src")
-    dst_dir = tmpdir.mkdir("dst")
-
+def test_merges_nested_directory_with_mismatched_existing_file(src_dir, dst_dir):
     src_nested = src_dir.mkdir("nested")
     dst_nested = dst_dir.mkdir("nested")
 
@@ -130,10 +140,7 @@ def test_merges_nested_directory_with_mismatched_existing_file(tmpdir):
         move_or_merge(src=str(src_dir), dst=str(dst_dir))
 
 
-def test_ignores_existing_files_in_dst(tmpdir):
-    src_dir = tmpdir.mkdir("src")
-    dst_dir = tmpdir.mkdir("dst")
-
+def test_ignores_existing_files_in_dst(src_dir, dst_dir):
     dst_existing = dst_dir.join("philosophy.txt")
     dst_existing.write("i think therefore i am")
 
