a27742478c8b472c72b80b927215fe488ec61830
Cole Maclean <hi@colemaclean.dev>
2019-10-09 15:36:44 -0700

    Handle import from MCPClient

--- src/dashboard/src/components/accounts/signals.py
+++ src/dashboard/src/components/accounts/signals.py
@@ -10,5 +10,7 @@ def ldap_populate_user(sender, user, ldap_user, **kwargs):
         generate_api_key(user)
 
 
-if settings.LDAP_AUTHENTICATION:
+# This code is imported from MCPClient, which has no
+# LDAP_AUTHENTICATION setting :(
+if getattr(settings, "LDAP_AUTHENTICATION", False):
     populate_user.connect(ldap_populate_user)
