cd79682316203218fd9b9b4e33c41a58dbbba9c3
Hillel Arnold <helrond@hotmail.com>
2019-12-20 22:05:04 -0500

    formatting again

--- src/dashboard/src/main/migrations/0075_userprofile_system_emails.py
+++ src/dashboard/src/main/migrations/0075_userprofile_system_emails.py
@@ -6,14 +6,12 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ("main", "0074_version_number"),
-    ]
+    dependencies = [("main", "0074_version_number")]
 
     operations = [
         migrations.AddField(
             model_name="userprofile",
             name="system_emails",
             field=models.BooleanField(default=False),
-        ),
+        )
     ]
