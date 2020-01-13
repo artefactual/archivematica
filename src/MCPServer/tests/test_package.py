28c57cca0f2b1c3a53cbd306a172512014d2931b
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-01-13 09:07:30 -0500

    Add tests for Package.files()
    Tests simulate the interplay between the filesystem and database
    when there are objects in both locations that need to be looked
    after.

--- src/MCPServer/tests/test_package.py
+++ src/MCPServer/tests/test_package.py
@@ -123,6 +123,107 @@ def test_transfer_get_or_create_from_db_path_with_uuid(tmp_path):
         )
 
 
+@pytest.mark.django_db(transaction=True)
+def test_reload_file_list(tmp_path):
+
+    # Create a transfer that will be updated through time to simulate
+    # Archivematica's processing.
+    transfer_uuid = uuid.uuid4()
+    transfer_path = tmp_path / "test-transfer-{}".format(transfer_uuid)
+    transfer = Transfer.get_or_create_from_db_by_path(str(transfer_path))
+
+    # Add files to the transfer to simulate a transfer existing on disk.
+    transfer_path.mkdir()
+    objects_path = transfer_path / "objects"
+    objects_path.mkdir()
+    first_file = objects_path / "file.txt"
+    first_file.touch()
+
+    kwargs = {
+        "uuid": uuid.uuid4(),
+        "currentlocation": "".join(
+            [transfer.REPLACEMENT_PATH_STRING, "/objects/file.txt"]
+        ),
+        "filegrpuse": "original",
+        "transfer_id": transfer_uuid,
+    }
+    models.File.objects.create(**kwargs)
+    assert models.File.objects.filter(transfer_id=str(transfer_uuid)).count() == 1
+
+    # Add a new file to the file-system, e.g. to simulate normalization for
+    # preservation adding a new object.
+    new_file = objects_path / "new_file.txt"
+    new_file.touch()
+
+    # One file will be returned from the database  with a UUID, another from
+    # the filesystem without a UUID.
+    for file_count, file_info in enumerate(transfer.files(None, None, "/objects"), 1):
+        assert "%fileUUID%" in file_info
+        assert "%fileGrpUse%" in file_info
+        assert "%relativeLocation%" in file_info
+        if file_info.get("%fileUUID%") != "None":
+            continue
+        assert file_info.get("%relativeLocation%") == str(new_file)
+        file_path = "".join(
+            [
+                transfer.REPLACEMENT_PATH_STRING,
+                "/objects",
+                file_info.get("%relativeLocation%").split("objects")[1],
+            ]
+        )
+        kwargs = {
+            "uuid": uuid.uuid4(),
+            "currentlocation": file_path,
+            "filegrpuse": "original",
+            "transfer_id": transfer_uuid,
+        }
+        models.File.objects.create(**kwargs)
+    assert (
+        file_count == 2
+    ), "Database and file objects were not returned by the generator"
+    assert models.File.objects.filter(transfer_id=str(transfer_uuid)).count() == 2
+
+    # Simulate an additional file object being added later on in the transfer
+    # in a sub directory of the objects folder, e.g. transcribe contents.
+    sub_dir = objects_path / "subdir"
+    sub_dir.mkdir()
+    new_file = sub_dir / "another_new_file.txt"
+    new_file.touch()
+    for file_count, file_info in enumerate(transfer.files(None, None, "/objects"), 1):
+        file_path = "".join(
+            [
+                transfer.REPLACEMENT_PATH_STRING,
+                "/objects",
+                file_info.get("%relativeLocation%").split("objects")[1],
+            ]
+        )
+        if file_info.get("%fileUUID%") != "None":
+            continue
+        kwargs = {
+            "uuid": uuid.uuid4(),
+            "currentlocation": file_path,
+            "filegrpuse": "original",
+            "transfer_id": transfer_uuid,
+        }
+        models.File.objects.create(**kwargs)
+    assert (
+        file_count == 3
+    ), "Database and file objects were not returned by the generator"
+    assert models.File.objects.filter(transfer_id=str(transfer_uuid)).count() == 3
+
+    # Now the database is updated, we will still have the same file count, but
+    # all objects will be returned from the database and we will have uuids.
+    for file_count, file_info in enumerate(transfer.files(None, None, "/objects"), 1):
+        if file_info.get("%fileUUID%") == "None":
+            print(file_info)
+            assert False, "Non-database entries returned from package.files()"
+    assert file_count == 3
+
+    # Clean up state and ensure test doesn't interfere with other transfers
+    # expected to be in the database, e.g. in test_queues.py.
+    models.File.objects.filter(transfer_id=str(transfer_uuid)).delete()
+
+
 class TestPadDestinationFilePath:
     def test_zipfile_is_not_padded_if_does_not_exist(self, tmp_path):
         transfer_path = tmp_path / "transfer.zip"
