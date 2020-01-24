0b7a7086fcbdc9a89df1056b558e4329ee6bbbf7
Ashley <ashley.blewer@gmail.com>
2020-01-24 09:04:33 -0500

    Update to PRONOM v96 (#1542)

---
+++ src/dashboard/src/fpr/migrations/0032_pronom_96.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+
+from django.core.management import call_command
+from django.db import migrations
+
+
+def data_migration(apps, schema_editor):
+    fixture_file = os.path.join(os.path.dirname(__file__), "pronom_96.json")
+    call_command("loaddata", fixture_file, app_label="fpr")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("fpr", "0031_update_normalization_rules")]
+
+    operations = [migrations.RunPython(data_migration)]
