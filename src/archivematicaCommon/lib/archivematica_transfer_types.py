573eef4755ed7f1b910fda80a30e4c6aee542711
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-03-01 20:59:00 -0500

    Make transfer-based server code more generic

---
+++ src/archivematicaCommon/lib/archivematica_transfer_types.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+"""
+Archivematica transfer types.
+
+Module collects together information related to the validation and initiation
+of Archivematica transfers.
+
+Where feasible this module can be imported to be used in different classes
+calling it in Python. Places where this isn't feasible are as follows:
+
+ - Listing of transfer types plus file-type transfer control:
+     * src/dashboard/frontend/app/browse/browse.controller.js
+
+ - Enable the transfer type to be selected in the browser drop-down:
+     * src/dashboard/frontend/app/front_page/transfer_browser.html
+
+ - Disable the visibility for a transfer name in the browser:
+     * src/dashboard/frontend/app/header/header.controller.js
+
+ - A dummy name is generated for a transfer in the UI:
+     * src/dashboard/frontend/app/services/transfer_browser_transfer.service.js
+
+Transfers have different characteristics such as whether a transfer can be
+selected by directory, or by file, e.g. standard transfer vs. zipped bags.
+
+This module should try and capture these characteristics where possible.
+"""
+from __future__ import absolute_import, unicode_literals
+
+TRANSFER_STANDARD = "standard"
+TRANSFER_ZIPFILE = "zipfile"
+TRANSFER_UNZIPPED_BAG = "unzipped bag"
+TRANSFER_ZIPPED_BAG = "zipped bag"
+TRANSFER_DSPACE = "dspace"
+TRANSFER_MAILDIR = "maildir"
+TRANSFER_TRIM = "TRIM"
+TRANSFER_DATAVERSE = "dataverse"
+
+TRANSFER_DISk_IMAGE = "disk image"
+
+WATCHED_STANDARD = "standardTransfer"
+WATCHED_ZIPFILE = "zippedDirectory"
+WATCHED_UNZIPPED_BAG = "baggitDirectory"
+WATCHED_ZIPPED_BAG = "baggitZippedDirectory"
+WATCHED_DSPACE = "Dspace"
+WATCHED_MAILDIR = "maildir"
+WATCHED_TRIM = "TRIM"
+WATCHED_DATAVERSE = "dataverseTransfer"
+
+# TRANSFER_DISK_IMAGE is treated differently by src/MCPServer/lib/server/packages.py.
+TRANSFER_TYPES = (
+    TRANSFER_STANDARD,
+    TRANSFER_ZIPFILE,
+    TRANSFER_UNZIPPED_BAG,
+    TRANSFER_ZIPPED_BAG,
+    TRANSFER_DSPACE,
+    TRANSFER_MAILDIR,
+    TRANSFER_TRIM,
+    TRANSFER_DATAVERSE,
+)
+
+ARCHIVE_ZIP = ".zip"
+ARCHIVE_TGZ = ".tgz"
+ARCHIVE_TAR_GZ = ".tar.gz"
+
+ARCHIVE_TYPES = (ARCHIVE_ZIP, ARCHIVE_TGZ, ARCHIVE_TAR_GZ)
