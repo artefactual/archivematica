a71976c64ac05a1865e5de7a3ffc06419748ccc0
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-03-02 21:03:41 -0500

    Modify archivematica_transfer_type tests following CR

--- src/archivematicaCommon/tests/test_archivematica_transfer_types.py
+++ src/archivematicaCommon/tests/test_archivematica_transfer_types.py
@@ -21,17 +21,18 @@ def test_retrieve_watched_dirs():
     dirs = amtypes.retrieve_watched_dirs()
     assert len(list(dirs)) == len(EXPECTED_DIRS)
     for dir_ in dirs:
-        if dir_ not in EXPECTED_DIRS:
-            assert (
-                False
-            ), "Unexpected watched directory in transfer watched directories: {}".format(
-                dir_
-            )
+        assert (
+            dir_ in EXPECTED_DIRS
+        ), "Unexpected watched directory in transfer watched directories: {}".format(
+            dir_
+        )
 
 
 def test_watched_directory():
-    for expected in EXPECTED_DIRS:
-        assert amtypes.retrieve_watched_directory(expected[0]) == expected[1]
+    for transfer_type, watched_directory_name in EXPECTED_DIRS:
+        assert (
+            amtypes.retrieve_watched_directory(transfer_type) == watched_directory_name
+        )
     amtypes.retrieve_watched_directory(
         "unknown transfer type"
     ) == amtypes.WATCHED_STANDARD
