88d086bee42000db855e9613101cd5981249a1fc
Tim Walsh <tim@bitarchivist.net>
2020-04-06 15:12:20 -0400

    Add processing config selector and API endpoint
    This work adds a dropdown component to the "Start transfer" button that
    enables users to select which processing configuration to use with a
    transfer. In addition, this commit:
    - Adds new API endpoint to list processing configurations
    - Adds new info alert when a transfer has been started from the GUI
    - Changes the base layout.html template to use HTML5 doctype

--- src/dashboard/src/components/api/views.py
+++ src/dashboard/src/components/api/views.py
@@ -806,6 +806,17 @@ def processing_configuration(request, name):
         return django.http.HttpResponse(content, content_type="text/xml")
 
 
+@_api_endpoint(expected_methods=["GET"])
+def processing_configurations(request):
+    """Return list of names of available processing configurations."""
+    config_dir = helpers.processing_config_path()
+    CONFIG_FILE_SUFFIX = "ProcessingMCP.xml"
+    processing_configs = sorted(
+        [filename[: -len(CONFIG_FILE_SUFFIX)] for filename in os.listdir(config_dir)]
+    )
+    return helpers.json_response({"processing_configurations": processing_configs})
+
+
 @_api_endpoint(expected_methods=["GET", "POST"])
 def package(request):
     """Package resource handler."""
