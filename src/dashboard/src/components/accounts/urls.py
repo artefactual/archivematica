1a3dbcf1fbb64bfb107ac0f120e67fdc97310da9
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2020-04-06 15:21:48 -0600

    Use LoginView instead of login

--- src/dashboard/src/components/accounts/urls.py
+++ src/dashboard/src/components/accounts/urls.py
@@ -35,8 +35,9 @@ urlpatterns = [
 urlpatterns += [
     url(
         r"login/$",
-        django.contrib.auth.views.login,
-        {"template_name": "accounts/login.html"},
+        django.contrib.auth.views.LoginView.as_view(
+            template_name="accounts/login.html"
+        ),
         name="login",
     ),
     url(r"logout/$", django.contrib.auth.views.logout_then_login, name="logout"),
