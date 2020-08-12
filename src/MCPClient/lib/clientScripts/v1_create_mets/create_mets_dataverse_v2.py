5ff568a8d0566a49e4518075fde606c95074013d
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-08-12 08:16:12 -0400

    Remove Python shebangs and in-script licenses
    Their inclusion is additional noise for the developer at this stage
    and source control maintains the history of the hard-work put in
    by folk.
    
    Co-authored-by: Joseph Perry <joseph@artefactual.com>

--- src/MCPClient/lib/clientScripts/v1_create_mets/create_mets_dataverse_v2.py
+++ src/MCPClient/lib/clientScripts/v1_create_mets/create_mets_dataverse_v2.py
@@ -1,24 +1,9 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# This file is part of Archivematica.
-#
-# Copyright 2010-2018 Artefactual Systems Inc. <http://artefactual.com>
-#
-# Archivematica is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Archivematica is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Archivematica.    If not, see <http://www.gnu.org/licenses/>.
 
-"""Maps Dataverse specific elements into the AIP METS file generated on ingest.
+"""Create METS Dataverse
+
+Maps Dataverse specific elements into the AIP METS file generated on
+ingest.
 """
 
 from __future__ import print_function
