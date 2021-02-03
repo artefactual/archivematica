d4e4c56e9f8ca0b93404f975999a9b5c06572e71
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-02-03 11:09:43 -0600

    Add settings for using MySQL test databases

---
+++ src/dashboard/src/settings/testmysql.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+from __future__ import absolute_import
+
+from .test import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "DASHBOARDTEST",
+        "USER": "archivematica",
+        "PASSWORD": "demo",
+        "HOST": "mysql",
+        "PORT": "3306",
+        "CONN_MAX_AGE": 600,
+    }
+}
