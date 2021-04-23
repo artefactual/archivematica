e47bca6ba41d08efa63cb564820317d0f25509d6
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-04-23 15:30:01 -0600

    Remove repr tests for TranslationLabel

--- src/MCPServer/tests/test_translation.py
+++ src/MCPServer/tests/test_translation.py
@@ -2,19 +2,12 @@
 
 from __future__ import unicode_literals
 
-import six
-
 from server.translation import UNKNOWN_TRANSLATION_LABEL, TranslationLabel
 
 
 def test_translation_label(mocker):
     mocker.patch("server.translation.FALLBACK_LANG", "en")
     tr = TranslationLabel({"en": "cat", "es": "gato"})
-    # TODO: revise this
-    if six.PY2:
-        assert repr(tr) == "TranslationLabel({u'en': u'cat', u'es': u'gato'})"
-    else:
-        assert repr(tr) == "TranslationLabel({'en': 'cat', 'es': 'gato'})"
     assert str(tr) == "cat"
     assert tr["es"] == "gato"
     assert tr["unexistent-lang-code"] == "cat"
@@ -31,13 +24,3 @@ def test_translation_label_with_prepared_codes(mocker):
     assert tr.get_label(lang="en") == "dog"
     assert tr.get_label(lang="pt-br") == "cão"
     assert tr.get_label(lang="pt_BR") == "cão"
-
-
-def test_translation_label_string(mocker):
-    mocker.patch("server.translation.FALLBACK_LANG", "en")
-    tr = TranslationLabel("cat")
-    # TODO: revise this
-    if six.PY2:
-        assert repr(tr) == "TranslationLabel({u'en': u'cat'})"
-    else:
-        assert repr(tr) == "TranslationLabel({'en': 'cat'})"
