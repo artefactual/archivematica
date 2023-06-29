cf4e38cf9e0951fea37e4a12db4c0701407c9497
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2023-06-29 01:08:07 +0000

    Use local XML schemas for XML validation
    This implements a custom XML catalog that points to local XML schema
    files to prevent accessing the loc.gov URLs during XML validation in
    the MCPClient.

---
+++ src/MCPClient/tests/conftest.py
@@ -0,0 +1,18 @@
+import pathlib
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def set_xml_catalog_files(monkeypatch):
+    """Use local XML schemas for validation."""
+    monkeypatch.setenv(
+        "XML_CATALOG_FILES",
+        str(
+            pathlib.Path(__file__).parent.parent
+            / "lib"
+            / "assets"
+            / "catalog"
+            / "catalog.xml"
+        ),
+    )
