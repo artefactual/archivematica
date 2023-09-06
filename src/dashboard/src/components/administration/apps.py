cf355103659117d833a43ce3ecf0dfe0cb5c620a
Dhwani Patel <dpatel@artefactual.com>
2023-09-06 14:20:27 -0600

    Remove django warnings in development environment

---
+++ src/dashboard/src/components/administration/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdministrationAppConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
+    name = "components.administration"
---
+++ src/dashboard/src/fpr/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FPRAppConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
+    name = "fpr"
