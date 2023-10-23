abebc74021820af3fa5552057703a95207e480ba
Jesús García Crespo <jesus@sevein.com>
2023-10-23 10:07:55 +0000

    Delete unused @ comments

--- src/MCPClient/lib/clientScripts/bag_with_empty_directories.py
+++ src/MCPClient/lib/clientScripts/bag_with_empty_directories.py
@@ -15,9 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
-# @package Archivematica
-# @subpackage archivematicaClientScript
-# @author Joseph Perry <joseph@artefactual.com>
 import argparse
 import multiprocessing
 import os
--- src/archivematicaCommon/lib/elasticSearchFunctions.py
+++ src/archivematicaCommon/lib/elasticSearchFunctions.py
@@ -14,9 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
-# @package Archivematica
-# @subpackage archivematicaCommon
-# @author Mike Cantelon <mike@artefactual.com>
 import calendar
 import copy
 import datetime
