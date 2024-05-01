2c5eb483f4f4802f55472403abcd270ff09c5d22
Dhwani Patel <dpatel@artefactual.com>
2024-05-01 14:46:03 -0600

    Remove UUID_REGEX from archival_storage/urls.py

--- src/dashboard/src/components/archival_storage/urls.py
+++ src/dashboard/src/components/archival_storage/urls.py
@@ -15,42 +15,37 @@
 # You should have received a copy of the GNU General Public License
 # along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
 from components.archival_storage import views
-from django.conf import settings
 from django.urls import path
 from django.urls import re_path
 
 
 app_name = "archival_storage"
 urlpatterns = [
-    re_path(
-        r"^(?P<uuid>" + settings.UUID_REGEX + ")/$", views.view_aip, name="view_aip"
-    ),
-    re_path(
-        r"^download/aip/file/(?P<uuid>" + settings.UUID_REGEX + ")/$",
+    path("<uuid:uuid>/", views.view_aip, name="view_aip"),
+    path(
+        "download/aip/file/<uuid:uuid>/",
         views.aip_file_download,
         name="aip_file_download",
     ),
-    re_path(
-        r"^download/aip/(?P<uuid>" + settings.UUID_REGEX + ")/mets_download/$",
+    path(
+        "download/aip/<uuid:uuid>/mets_download/",
         views.aip_mets_file_download,
         name="aip_mets_file_download",
     ),
-    re_path(
-        r"^download/aip/(?P<uuid>" + settings.UUID_REGEX + ")/pointer_file/$",
+    path(
+        "download/aip/<uuid:uuid>/pointer_file/",
         views.aip_pointer_file_download,
         name="aip_pointer_file_download",
     ),
-    re_path(
-        r"^download/aip/(?P<uuid>" + settings.UUID_REGEX + ")/$",
+    path(
+        "download/aip/<uuid:uuid>/",
         views.aip_download,
         name="aip_download",
     ),
     re_path(r"^search/json/file/(?P<document_id_modified>\w+)/$", views.file_json),
     path("search/create_aic/", views.create_aic, name="create_aic"),
     path("search/", views.search, name="archival_storage_search"),
-    re_path(
-        r"^thumbnail/(?P<fileuuid>" + settings.UUID_REGEX + ")/$", views.send_thumbnail
-    ),
+    path("thumbnail/<uuid:fileuuid>/", views.send_thumbnail),
     re_path(r"^save_state/(?P<table>[-\w]+)/$", views.save_state, name="save_state"),
     re_path(r"^load_state/(?P<table>[-\w]+)/$", views.load_state, name="load_state"),
     path("", views.execute, name="archival_storage_index"),
