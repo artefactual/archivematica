727b2db5148036ec5aa6202f1d2d61f87c8b6d05
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2024-05-08 21:45:39 +0200

    Add failing test for failure reports index

--- tests/dashboard/components/administration/test_administration.py
+++ tests/dashboard/components/administration/test_administration.py
@@ -1,13 +1,19 @@
+import uuid
+
 import pytest
 from components import helpers
 from django.urls import reverse
 from main.models import Report
 
 
+@pytest.fixture
 @pytest.mark.django_db
-def test_admin_set_language(admin_client):
-    helpers.set_setting("dashboard_uuid", "test-uuid")
+def dashboard_uuid():
+    helpers.set_setting("dashboard_uuid", str(uuid.uuid4()))
 
+
+@pytest.mark.django_db
+def test_admin_set_language(dashboard_uuid, admin_client):
     response = admin_client.get(reverse("administration:admin_set_language"))
     assert response.status_code == 200
 
@@ -17,8 +23,7 @@ def test_admin_set_language(admin_client):
 
 
 @pytest.mark.django_db
-def test_failure_report_delete(admin_client):
-    helpers.set_setting("dashboard_uuid", "test-uuid")
+def test_failure_report_delete(dashboard_uuid, admin_client):
     report = Report.objects.create(content="my report")
 
     response = admin_client.post(
@@ -30,3 +35,15 @@ def test_failure_report_delete(admin_client):
 
     assert "No reports found." in response.content.decode()
     assert Report.objects.count() == 0
+
+
+@pytest.mark.django_db
+def test_failure_report(dashboard_uuid, admin_client):
+    report = Report.objects.create(content="my report")
+
+    response = admin_client.get(reverse("administration:reports_failures_index"))
+    assert response.status_code == 200
+
+    content = response.content.decode()
+    assert "<h3>Failure report</h3>" in content
+    assert reverse("administration:failure_report", args=[report.pk]) in content
