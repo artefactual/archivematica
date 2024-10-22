be74df81e6f9ccd0c8cf1a3cf1a385d89e441e9c
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2024-10-22 22:01:26 +0000

    Add tests for edit user view

--- tests/dashboard/components/accounts/test_views.py
+++ tests/dashboard/components/accounts/test_views.py
@@ -1,10 +1,20 @@
+import hmac
+import uuid
+from hashlib import sha1
+from typing import Type
+from unittest import mock
 from urllib.parse import parse_qs
 from urllib.parse import urlparse
 
 import pytest
 import pytest_django
+from components import helpers
 from components.accounts.views import get_oidc_logout_url
+from django.contrib.auth.models import User
+from django.test import Client
 from django.test import RequestFactory
+from django.urls import reverse
+from tastypie.models import ApiKey
 
 
 def test_get_oidc_logout_url_fails_if_token_is_not_set(rf: RequestFactory) -> None:
@@ -42,3 +52,135 @@ def test_get_oidc_logout_url_returns_logout_url(
     assert set(query_dict) == {"id_token_hint", "post_logout_redirect_uri"}
     assert query_dict["id_token_hint"] == [token]
     assert query_dict["post_logout_redirect_uri"] == ["http://testserver/"]
+
+
+@pytest.fixture
+def dashboard_uuid() -> None:
+    helpers.set_setting("dashboard_uuid", str(uuid.uuid4()))
+
+
+@pytest.fixture
+def non_administrative_user(django_user_model: Type[User]) -> User:
+    return django_user_model.objects.create_user(
+        username="test",
+        password="test",
+        first_name="Foo",
+        last_name="Bar",
+        email="foobar@example.com",
+    )
+
+
+@pytest.mark.django_db
+def test_edit_user_view_denies_access_to_non_admin_users_editing_others(
+    dashboard_uuid: None,
+    non_administrative_user: User,
+    admin_user: User,
+    client: Client,
+) -> None:
+    client.force_login(non_administrative_user)
+
+    response = client.get(
+        reverse("accounts:edit", kwargs={"id": admin_user.id}), follow=True
+    )
+    assert response.status_code == 200
+
+    content = response.content.decode()
+    assert "Forbidden" in content
+    assert "You do not have enough access privileges for this operation" in content
+
+
+@pytest.fixture
+def non_administrative_user_apikey(non_administrative_user: User) -> ApiKey:
+    return ApiKey.objects.create(user=non_administrative_user)
+
+
+@pytest.mark.django_db
+def test_edit_user_view_renders_user_profile_fields(
+    dashboard_uuid: None,
+    non_administrative_user: User,
+    non_administrative_user_apikey: ApiKey,
+    admin_client: Client,
+) -> None:
+    response = admin_client.get(
+        reverse("accounts:edit", kwargs={"id": non_administrative_user.id}), follow=True
+    )
+    assert response.status_code == 200
+
+    content = response.content.decode()
+    assert f"Edit user {non_administrative_user.username}" in content
+    assert f'name="username" value="{non_administrative_user.username}"' in content
+    assert f'name="first_name" value="{non_administrative_user.first_name}"' in content
+    assert f'name="last_name" value="{non_administrative_user.last_name}"' in content
+    assert f'name="email" value="{non_administrative_user.email}"' in content
+    assert f"<code>{non_administrative_user_apikey.key}</code>" in content
+
+
+@pytest.mark.django_db
+def test_edit_user_view_updates_user_profile_fields(
+    dashboard_uuid: None,
+    non_administrative_user: User,
+    admin_client: Client,
+) -> None:
+    new_username = "newusername"
+    new_password = "newpassword"
+    new_first_name = "bar"
+    new_last_name = "foo"
+    new_email = "newemail@example.com"
+    data = {
+        "username": new_username,
+        "password": new_password,
+        "password_confirmation": new_password,
+        "first_name": new_first_name,
+        "last_name": new_last_name,
+        "email": new_email,
+    }
+
+    response = admin_client.post(
+        reverse("accounts:edit", kwargs={"id": non_administrative_user.id}),
+        data,
+        follow=True,
+    )
+    assert response.status_code == 200
+
+    content = response.content.decode()
+    assert "Saved" in content
+    assert (
+        f'<a href="{reverse("accounts:edit", kwargs={"id": non_administrative_user.id})}">{new_username}</a>'
+        in content
+    )
+    assert f"<td>{new_first_name} {new_last_name}</td>" in content
+    assert f"<td>{new_email}</td>" in content
+
+    non_administrative_user.refresh_from_db()
+    assert non_administrative_user.check_password(new_password)
+
+
+@pytest.mark.django_db
+def test_edit_user_view_regenerates_api_key(
+    dashboard_uuid: None,
+    non_administrative_user: User,
+    non_administrative_user_apikey: ApiKey,
+    admin_client: Client,
+) -> None:
+    data = {
+        "username": non_administrative_user.username,
+        "first_name": non_administrative_user.first_name,
+        "last_name": non_administrative_user.last_name,
+        "email": non_administrative_user.email,
+        "regenerate_api_key": True,
+    }
+    expected_uuid = uuid.uuid4()
+    expected_key = hmac.new(expected_uuid.bytes, digestmod=sha1).hexdigest()
+
+    with mock.patch("uuid.uuid4", return_value=expected_uuid):
+        response = admin_client.post(
+            reverse("accounts:edit", kwargs={"id": non_administrative_user.id}),
+            data,
+            follow=True,
+        )
+    assert response.status_code == 200
+
+    assert "Saved" in response.content.decode()
+
+    non_administrative_user_apikey.refresh_from_db()
+    assert non_administrative_user_apikey.key == expected_key
