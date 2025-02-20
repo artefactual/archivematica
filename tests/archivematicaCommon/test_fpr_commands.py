02156d3414d22ca0a5b0f1721611eae1ed40d8e4
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2025-02-20 16:41:26 +0000

    Add test for the 7z extraction event detail command

---
+++ tests/archivematicaCommon/test_fpr_commands.py
@@ -0,0 +1,27 @@
+import re
+
+import pytest
+from executeOrRunSubProcess import executeOrRun
+from fpr.models import FPCommand
+
+
+@pytest.mark.django_db
+def test_7z_extraction_event_detail_command_returns_7z_version() -> None:
+    expected_program = "7z"
+    # The event detail command extracts different lines depending on the 7z version.
+    # Older versions report the version on a line starting with "p7zip Version"
+    # while more recent versions use a line starting with "7-Zip".
+    expected_version_pattern = r"(^p7zip Version|^7-Zip)"
+    filters = {
+        "command_usage": "event_detail",
+        "description": "Get event detail text for 7z extraction",
+    }
+    command = FPCommand.active.get(**filters)
+
+    _, output, _ = executeOrRun(command.script_type, command.command)
+
+    match = re.search(r'program="(?P<program>.*?)"; version="(?P<version>.*?)"', output)
+    assert match is not None
+    result = match.groupdict()
+    assert result["program"] == expected_program
+    assert re.search(expected_version_pattern, result["version"]) is not None
