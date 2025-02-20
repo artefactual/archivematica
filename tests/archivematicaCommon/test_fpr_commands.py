c359a1427cf85a508a77a2aea02f107e06f18157
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2025-02-20 17:26:07 +0000

    Parametrize tests cases

--- tests/archivematicaCommon/test_fpr_commands.py
+++ tests/archivematicaCommon/test_fpr_commands.py
@@ -1,59 +1,52 @@
 import re
+from typing import TypedDict
 
 import pytest
 from executeOrRunSubProcess import executeOrRun
 from fpr.models import FPCommand
 
 
-@pytest.mark.django_db
-def test_7z_extraction_event_detail_command_returns_7z_version() -> None:
-    expected_program = "7z"
-    # The event detail command extracts different lines depending on the 7z version.
-    # Older versions report the version on a line starting with "p7zip Version"
-    # while more recent versions use a line starting with "7-Zip".
-    expected_version_pattern = r"(^p7zip Version|^7-Zip)"
-    filters = {
-        "command_usage": "event_detail",
-        "description": "Get event detail text for 7z extraction",
-    }
-    command = FPCommand.active.get(**filters)
-
-    _, output, _ = executeOrRun(command.script_type, command.command)
-
-    match = re.search(r'program="(?P<program>.*?)"; version="(?P<version>.*?)"', output)
-    assert match is not None
-    result = match.groupdict()
-    assert result["program"] == expected_program
-    assert re.search(expected_version_pattern, result["version"]) is not None
+class QueryFilters(TypedDict):
+    command_usage: str
+    description: str
 
 
 @pytest.mark.django_db
-def test_convert_event_detail_command_returns_convert_version() -> None:
-    expected_program = "convert"
-    expected_version_pattern = r"^Version: ImageMagick"
-    filters = {
-        "command_usage": "event_detail",
-        "description": "convert event detail",
-    }
-    command = FPCommand.active.get(**filters)
-
-    _, output, _ = executeOrRun(command.script_type, command.command)
-
-    match = re.search(r'program="(?P<program>.*?)"; version="(?P<version>.*?)"', output)
-    assert match is not None
-    result = match.groupdict()
-    assert result["program"] == expected_program
-    assert re.search(expected_version_pattern, result["version"]) is not None
-
-
-@pytest.mark.django_db
-def test_ffmpeg_extraction_event_detail_command_returns_ffmpeg_version() -> None:
-    expected_program = "ffmpeg"
-    expected_version_pattern = r"^ffmpeg version"
-    filters = {
-        "command_usage": "event_detail",
-        "description": "Get event detail text for ffmpeg extraction",
-    }
+@pytest.mark.parametrize(
+    "expected_program,expected_version_pattern,filters",
+    [
+        (
+            "7z",
+            # The event detail command extracts different lines depending on the 7z version.
+            # Older versions report the version on a line starting with "p7zip Version"
+            # while more recent versions use a line starting with "7-Zip".
+            r"(^p7zip Version|^7-Zip)",
+            {
+                "command_usage": "event_detail",
+                "description": "Get event detail text for 7z extraction",
+            },
+        ),
+        (
+            "convert",
+            "^Version: ImageMagick",
+            {
+                "command_usage": "event_detail",
+                "description": "convert event detail",
+            },
+        ),
+        (
+            "ffmpeg",
+            r"^ffmpeg version",
+            {
+                "command_usage": "event_detail",
+                "description": "Get event detail text for ffmpeg extraction",
+            },
+        ),
+    ],
+)
+def test_event_detail_command_returns_tool_version(
+    expected_program: str, expected_version_pattern: str, filters: QueryFilters
+) -> None:
     command = FPCommand.active.get(**filters)
 
     _, output, _ = executeOrRun(command.script_type, command.command)
