4903c4472246a6f4cec7d4dc39c1d9bd57891d25
Santi Collazo <santi@collazo.eu>
2025-08-20 11:35:54 +0200

    Set DEFAULT_AUTO_FIELD
    Removes models.W042 warning when running manage.py commands

--- src/archivematica/dashboard/settings/base.py
+++ src/archivematica/dashboard/settings/base.py
@@ -753,3 +753,6 @@ if PROMETHEUS_ENABLED:
 
 # Apply email settings
 globals().update(email_settings.get_settings(config))
+
+# Remove models.W042 warning
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
