5254dc1a2f9fe8b56a4094551fe859668bab29a2
Steve Breker <sbreker@artefactual.com>
2025-09-04 17:37:46 -0700

    Add secure CSP directives with Report Only
    Add secure CSP directives with REPORT-ONLY. This will cause CSP
    enforcement failures to be logged in the browser console. Inline scripts
    that triggered the failure will still run so the Dashboard functions
    normally.

--- src/archivematica/dashboard/settings/components/csp.py
+++ src/archivematica/dashboard/settings/components/csp.py
@@ -2,6 +2,7 @@ from csp.constants import NONE
 from csp.constants import SELF
 from csp.constants import UNSAFE_EVAL
 from csp.constants import UNSAFE_INLINE
+from csp.constants import NONCE
 
 CONTENT_SECURITY_POLICY = {
     "DIRECTIVES": {
@@ -15,4 +16,25 @@ CONTENT_SECURITY_POLICY = {
         # for /status
         "connect-src": [SELF],
     }
+}
+
+
+CONTENT_SECURITY_POLICY_REPORT_ONLY = {
+    "DIRECTIVES": {
+        "default-src": [NONE],
+        "script-src": [SELF, NONCE],
+        "style-src": [SELF, NONCE],
+        "img-src": [SELF, "data:"],
+        "font-src": [SELF, "data:"],
+        # for preview file pane in the appraisal tab
+        "frame-src": [SELF],
+        # for /status
+        "connect-src": [SELF],
+        # Newly added directives for testing
+        "form-action": [SELF],
+        "frame-ancestors": [SELF],
+
+        # "upgrade-insecure-requests": True,
+        # "report-uri": "/csp-report/",
+    },
 }
