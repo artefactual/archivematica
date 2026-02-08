968e2cff5d46a06004238b9c7832458601f8f087
Jesús García Crespo <jesus@sevein.com>
2026-02-08 09:36:05 +0100

    Remove unused .preview-help-text js behaviour

--- src/archivematica/dashboard/media/js/misc.js
+++ src/archivematica/dashboard/media/js/misc.js
@@ -82,33 +82,6 @@ function getCookie(c_name) {
 $(document).ready(
   function()
     {
-      $('.preview-help-text')
-
-        // Preview text
-        .children('.preview')
-          .show()
-          .children('a')
-            .click(function(event)
-              {
-                event.preventDefault();
-                $(this).closest('.preview').hide();
-                $(this).closest('.preview-help-text').children('.content').show();
-              })
-          .end()
-        .end()
-
-        // Content
-        .children('.content')
-          .hide()
-          .append(' <a href="#">(collapse)</a>')
-          .children('a')
-            .click(function(event)
-              {
-                event.preventDefault();
-                $(this).closest('.content').hide();
-                $(this).closest('.preview-help-text').children('.preview').show();
-              });
-
       // Set up the button which allows users to copy the API key to the clipboard.
       $('#copy-api-key-button').tooltip().click(function () {
         var button = $(this);
