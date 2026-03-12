02f2bbe13607cf6e0165e48e0aab8f2d4c79b4ec
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2026-03-11 20:55:29 -0600

    Add local nonce-enforced CSP test overrides

---
+++ hack/etc/csp/dashboard-csp-nonce-enforce.py
@@ -0,0 +1,28 @@
+import os
+
+from csp.constants import NONCE
+from csp.constants import NONE
+from csp.constants import REPORT_SAMPLE
+from csp.constants import SELF
+from csp.constants import STRICT_DYNAMIC
+
+REPORT_URI = os.environ.get(
+    "CSP_REPORT_URI",
+    "http://nginx/api/csp-report/",
+)
+
+CONTENT_SECURITY_POLICY = {
+    "DIRECTIVES": {
+        "default-src": [NONE],
+        "script-src": [NONCE, STRICT_DYNAMIC, REPORT_SAMPLE],
+        "style-src": [SELF],
+        "img-src": [SELF, "data:"],
+        "font-src": [SELF, "data:"],
+        "connect-src": [SELF],
+        "object-src": [NONE],
+        "base-uri": [SELF],
+        "form-action": [SELF],
+        "frame-ancestors": [NONE],
+        "report-uri": REPORT_URI,
+    }
+}
