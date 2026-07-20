abb2407986fe1f47997c62048d462a3f568faa5c
Douglas Cerna <douglascerna@yahoo.com>
2026-07-20 10:56:59 -0600

    Update to PRONOM v124
    * Fix get_fpr_changes comparisons
    
    Ignore database-local primary keys and timestamps when comparing FPR
    dumps. Detect semantically identical records with different UUIDs and
    reject new entries that reference drifted UUIDs.
    
    * Omit PKs from get_fpr_changes output
    
    Remove database primary keys from generated FPR migration fixtures.

---
+++ src/archivematica/dashboard/fpr/migrations/0054_pronom_124.py
@@ -0,0 +1,14 @@
+import pathlib
+
+from django.core.management import call_command
+from django.db import migrations
+
+
+def data_migration_up(apps, schema_editor):
+    fixture_file = pathlib.Path(__file__).parent / "pronom_124.json"
+    call_command("loaddata", fixture_file, app_label="fpr")
+
+
+class Migration(migrations.Migration):
+    dependencies = [("fpr", "0053_update_jhove_tool")]
+    operations = [migrations.RunPython(data_migration_up, migrations.RunPython.noop)]
